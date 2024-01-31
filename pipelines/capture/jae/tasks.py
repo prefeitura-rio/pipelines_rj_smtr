541c9d222d413a42144ee6813b9878663a95339f
Rafael <rafael.c.pinheiro96@gmail.com>
2024-01-31 16:28:29 -0300

    ajuste jae

--- pipelines/capture/jae/tasks.py
+++ pipelines/capture/jae/tasks.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Tasks for pipelines.capture.jae"""
+from datetime import datetime
 
 from pipelines.capture.jae.constants import constants
 from pipelines.utils.capture.db import DBExtractor
@@ -23,8 +24,13 @@ def create_extractor_jae(
     engine = database_details["engine"]
     host = database_details["host"]
 
-    start = incremental_info.start_value.stftime("%Y-%m-%d %H:%M:%S")
-    end = incremental_info.end_value.stftime("%Y-%m-%d %H:%M:%S")
+    start = incremental_info.start_value
+    end = incremental_info.end_value
+    if isinstance(datetime, start):
+        start = start.stftime("%Y-%m-%d %H:%M:%S")
+
+    if isinstance(datetime, end):
+        end = end.stftime("%Y-%m-%d %H:%M:%S")
     query = render_template(
         template_string=extract_params["query"],
         execution_mode=incremental_info.execution_mode,
