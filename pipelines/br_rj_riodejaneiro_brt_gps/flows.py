da3794d9671bccf161ef9e1123817d4925e3a1e5
hellcassius <caiorogerio.santos@gmail.com>
2024-02-02 09:43:13 -0300

    trigger cd

--- pipelines/br_rj_riodejaneiro_brt_gps/flows.py
+++ pipelines/br_rj_riodejaneiro_brt_gps/flows.py
@@ -118,6 +118,7 @@ with Flow(
             wait=RUN,
             mode=MODE,
         )
+        #trigger cd
 
 materialize_brt.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
 materialize_brt.run_config = KubernetesRun(
