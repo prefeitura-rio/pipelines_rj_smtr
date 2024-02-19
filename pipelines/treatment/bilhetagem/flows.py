259f5443c459fb6b7d7fb9add98a9e8b9affacb1
Rafael <rafael.c.pinheiro96@gmail.com>
2024-02-19 18:29:20 -0300

    teste subflow

--- pipelines/treatment/bilhetagem/flows.py
+++ pipelines/treatment/bilhetagem/flows.py
@@ -2,23 +2,39 @@
 """Flows de tratamento da bilhetagem"""
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefeitura_rio.pipelines_utils.custom import Flow
 
 from pipelines.capture.jae.constants import constants as jae_capture_constants
 from pipelines.capture.jae.flows import JAE_AUXILIAR_CAPTURE
 from pipelines.constants import constants
-from pipelines.tasks import run_subflow
+
+# from pipelines.tasks import run_subflow
+
 
 with Flow("Bilhetagem - Tratamento") as bilhetagem_tratamento:
 
-    params_auxiliar = [p for p in jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value]
-    AUXILIAR_CAPTURE = run_subflow(
+    # AUXILIAR_CAPTURE = run_subflow(
+    #     flow_name=JAE_AUXILIAR_CAPTURE.name,
+    #     parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value,
+    #     maximum_parallelism=3,
+    # )
+
+    # AUXILIAR_CAPTURE.name = "run_captura_auxiliar_jae"
+
+    runs_captura = create_flow_run.map(
         flow_name=JAE_AUXILIAR_CAPTURE.name,
-        parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value,
-        maximum_parallelism=3,
+        project_name="staging",
+        parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value[0],
+        labels=[constants.RJ_SMTR_AGENT_LABEL.value],
     )
 
-    AUXILIAR_CAPTURE.name = "run_captura_auxiliar_jae"
+    wait_captura_true = wait_for_flow_run.map(
+        runs_captura,
+        stream_states=True,
+        stream_logs=True,
+        raise_final_state=True,
+    )
 
 
 bilhetagem_tratamento.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
