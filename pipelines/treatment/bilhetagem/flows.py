60b20347ec03b2a2f29fa5c333476a310b82e03d
Rafael <rafael.c.pinheiro96@gmail.com>
2024-02-19 14:30:45 -0300

    teste bilhetagem

--- pipelines/treatment/bilhetagem/flows.py
+++ pipelines/treatment/bilhetagem/flows.py
@@ -3,15 +3,15 @@
 
 from prefeitura_rio.pipelines_utils.custom import Flow
 
-from pipelines.capture.jae.constants import constants as jae_capture_constants
-from pipelines.capture.jae.flows import JAE_AUXILIAR_CAPTURE
-from pipelines.tasks import run_flow_mapped
+# from pipelines.capture.jae.constants import constants as jae_capture_constants
+# from pipelines.capture.jae.flows import JAE_AUXILIAR_CAPTURE
+# from pipelines.tasks import run_flow_mapped
+from pipelines.tasks import flow_log
 
 with Flow("Bilhetagem - Tratamento") as bilhetagem_tratamento:
-    AUXILIAR_CAPTURE = run_flow_mapped(
-        flow_name=JAE_AUXILIAR_CAPTURE.name,
-        parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value,
-        maximum_parallelism=3,
-    )
-
-    AUXILIAR_CAPTURE.name = "run_captura_auxiliar"
+    # AUXILIAR_CAPTURE = run_flow_mapped(
+    #     flow_name=JAE_AUXILIAR_CAPTURE.name,
+    #     parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value,
+    #     maximum_parallelism=3,
+    # )
+    flow_log("hello world")
