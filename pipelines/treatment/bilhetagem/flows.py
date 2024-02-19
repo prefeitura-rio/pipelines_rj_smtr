806a632ba2f6c2c57bbcab80fd9715c5709724ae
Rafael <rafael.c.pinheiro96@gmail.com>
2024-02-19 18:33:28 -0300

    adicionar state handlers

--- pipelines/treatment/bilhetagem/flows.py
+++ pipelines/treatment/bilhetagem/flows.py
@@ -2,39 +2,26 @@
 """Flows de tratamento da bilhetagem"""
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
-from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefeitura_rio.pipelines_utils.custom import Flow
+from prefeitura_rio.pipelines_utils.state_handlers import (
+    handler_inject_bd_credentials,
+    handler_skip_if_running,
+)
 
 from pipelines.capture.jae.constants import constants as jae_capture_constants
 from pipelines.capture.jae.flows import JAE_AUXILIAR_CAPTURE
 from pipelines.constants import constants
-
-# from pipelines.tasks import run_subflow
-
+from pipelines.tasks import run_subflow
 
 with Flow("Bilhetagem - Tratamento") as bilhetagem_tratamento:
 
-    # AUXILIAR_CAPTURE = run_subflow(
-    #     flow_name=JAE_AUXILIAR_CAPTURE.name,
-    #     parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value,
-    #     maximum_parallelism=3,
-    # )
-
-    # AUXILIAR_CAPTURE.name = "run_captura_auxiliar_jae"
-
-    runs_captura = create_flow_run(
+    AUXILIAR_CAPTURE = run_subflow(
         flow_name=JAE_AUXILIAR_CAPTURE.name,
-        project_name="staging",
-        parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value[0],
-        labels=[constants.RJ_SMTR_AGENT_LABEL.value],
+        parameters=jae_capture_constants.AUXILIAR_TABLE_CAPTURE_PARAMS.value,
+        maximum_parallelism=3,
     )
 
-    wait_captura_true = wait_for_flow_run(
-        runs_captura,
-        stream_states=True,
-        stream_logs=True,
-        raise_final_state=True,
-    )
+    AUXILIAR_CAPTURE.name = "run_captura_auxiliar_jae"
 
 
 bilhetagem_tratamento.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
@@ -42,3 +29,5 @@ bilhetagem_tratamento.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[constants.RJ_SMTR_AGENT_LABEL.value],
 )
+
+bilhetagem_tratamento.state_handlers = [handler_inject_bd_credentials, handler_skip_if_running]
