70be4e0d39baa265999d55021513c31fb463f6bf
Rafael <rafael.c.pinheiro96@gmail.com>
2024-02-21 16:03:57 -0300

    corrige extração gcs

--- pipelines/utils/capture/gcs.py
+++ pipelines/utils/capture/gcs.py
@@ -26,12 +26,12 @@ class GCSExtractor(DataExtractor):
         folder: str,
         filename: str,
         save_filepath: str,
-        bucket_name: str = None,
+        bucket_names: dict[str] = None,
     ) -> None:
         super().__init__(save_filepath=save_filepath)
         filetype = get_filetype(filepath=save_filepath)
         self.complete_filename = f"{filename}.{filetype}"
-        self.storage = Storage(env=env, dataset_id=folder, bucket_name=bucket_name)
+        self.storage = Storage(env=env, dataset_id=folder, bucket_names=bucket_names)
 
     def _get_data(self) -> str:
         """Baixa o arquivo como string
