21a415eafab254e6492fa61c92b4fd8b1bfd6c15
hellcassius <caiorogerio.santos@gmail.com>
2024-03-13 14:51:58 -0300

    add report tasks

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -31,7 +31,7 @@ class constants(Enum):  # pylint: disable=c0103
 
     # WEBHOOK #
     CRITICAL_SECRET_PATH = "critical_webhook"
-
+    WEBHOOKS_SECRET_PATH = 'webhooks'
     # RETRY POLICY #
     MAX_TIMEOUT_SECONDS = 60
     MAX_RETRIES = 3
@@ -46,6 +46,11 @@ class constants(Enum):  # pylint: disable=c0103
     MODE_FULL = "full"
     MODE_INCR = "incr"
     FLOW_RUN_URL_PATTERN = "https://pipelines.dados.rio/smtr/flow-run/{run_id}"
+
+    GLITCH_API_ENDPOINT = 'https://glitch.dados.rio/api/0/projects/smtr/pipelines/issues/'
+    GLITCH_URL = 'https://glitch.dados.rio/'
+    GLITCH_AUTH = 'glitch_auth'
+    GLITCH_WEBHOOK = 'glitch'
 
     # GPS STPL #
     # GPS_STPL_API_BASE_URL = "http://zn4.m2mcontrol.com.br/api/integracao/veiculos"
--- pipelines/glitchtip/tasks.py
+++ pipelines/glitchtip/tasks.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+import requests
 
 from prefect import task
 from prefeitura_rio.pipelines_utils.logging import log
 
 from pipelines.constants import constants
-from pipelines.utils.backup.utils import log_critical, map_dict_keys
-
+from pipelines.utils.secret import get_secret
 # EMD Imports #
 
 
@@ -21,3 +21,56 @@ def test_raise_errors(datetime: datetime):
         raise ValueError(f"{datetime} % 3 is equal to zero")
     else:
         return datetime.minute / 0
+    
+@task
+def glitch_api_get(query='is:unresolved'):
+    base_url = constants.GLITCH_API_ENDPOINT.value
+
+    headers = get_secret(secret_path=constants.GLITCH_AUTH.value)
+    print(headers)
+    if query:
+        base_url += f'?query={query}'
+    print(base_url)
+    issues = requests.get(base_url, headers=headers)
+
+    return issues.json()
+
+@task
+def format_glitch_issue_messages(issues):
+    messages = []
+    issue_count = len(issues)
+    base_message = f"**Issues não resolvidos: {issue_count}**"
+    messages.append(base_message)
+    for issue in issues:
+        msg = f"""**Fonte: {issue['culprit']}**
+**Erro**: {issue['title']}
+**Event Count:** {issue['count']}
+**Criado:** {issue['firstSeen'].split('.')[0]}
+**Link:** {constants.GLITCH_URL.value}/smtr/issues/{issue['id']}
+"""
+        messages.append({'name':f"Issue {issue['id']}", 'value':msg})
+    
+    return messages
+
+@task
+def send_issue_report(messages):
+    timestamp = datetime.now().isoformat()
+    webhook = get_secret(
+        secret_path=constants.WEBHOOKS_SECRET_PATH.value, 
+        secret_name=constants.GLITCH_WEBHOOK.value
+    )['glitch']
+    headers = {"Content-Type": "application/json"}
+    message = {
+            "content": messages[0],
+            "embeds": [{
+                "color": 16515072,
+                "timestamp": timestamp,
+                "author": {'name':"Glitch Tip Issues", 'url':constants.GLITCH_URL.value},
+                "fields": messages[1:]
+            }]
+            }
+    response = requests.post(url=webhook, headers=headers, json=message)
+
+    log(response.text)
+    log(response.status_code)
+
