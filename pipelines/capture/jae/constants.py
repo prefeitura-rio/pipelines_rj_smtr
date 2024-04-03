77aa778c2cbf379ff7ce22700e82a1e15f245dc9
Rafael <rafael.c.pinheiro96@gmail.com>
2024-04-03 09:09:59 -0300

    ajuste janela incremental riocard

--- pipelines/capture/jae/constants.py
+++ pipelines/capture/jae/constants.py
@@ -53,7 +53,7 @@ class constants(Enum):
         "table_id": "transacao",
         "raw_filetype": JAE_RAW_FILETYPE,
         "incremental_capture_strategy": DatetimeIncremental(
-            max_incremental_window={"hours": 2}, first_value="2024-02-15 00:00:00"
+            max_incremental_window={"hours": 3}, first_value="2024-02-15 00:00:00"
         ).to_dict(),
         "data_extractor_params": {
             "database": "transacao_db",
@@ -74,7 +74,7 @@ class constants(Enum):
         "table_id": "transacao_riocard",
         "raw_filetype": JAE_RAW_FILETYPE,
         "incremental_capture_strategy": DatetimeIncremental(
-            max_incremental_window={"hours": 3}, first_value="2024-04-01 00:00:00"
+            max_incremental_window={"hours": 2}, first_value="2024-04-01 00:00:00"
         ).to_dict(),
         "data_extractor_params": {
             "database": "transacao_db",
