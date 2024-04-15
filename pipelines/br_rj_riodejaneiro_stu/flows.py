dd7d290b00dce288d99424d352728d7de3d735b5
hellcassius <caiorogerio.santos@gmail.com>
2024-04-15 10:21:40 -0300

    add stu flows

---
+++ pipelines/br_rj_riodejaneiro_stu/flows.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Flows for br_rj_riodejaneiro_stu
+"""
+
+from copy import deepcopy
+
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+from prefect.utilities.edges import unmapped
+from prefect import Parameter
+
+
+# EMD Imports #
+
+from pipelines.constants import constants as emd_constants
+from prefeitura_rio.pipelines_utils.custom import Flow
+from prefeitura_rio.pipelines_utils.state_handlers import handler_inject_bd_credentials, handler_initialize_sentry
+from pipelines.utils.backup.tasks import (
+    rename_current_flow_run_now_time,
+    get_current_flow_labels,
+    get_current_timestamp
+)
+
+
+from pipelines.utils.utils import set_default_parameters
+
+# SMTR Imports #
+
+from pipelines.capture.templates.flows import create_default_capture_flow
+
+from pipelines.br_rj_riodejaneiro_stu.tasks import (
+    get_stu_raw_blobs,
+    read_stu_raw_file,
+    create_final_stu_dataframe,
+    save_stu_dataframes,
+)
+
+from pipelines.constants import constants
+
+stu_captura_subflow = create_default_capture_flow(
+    flow_name="SMTR: STU - Captura (subflow)", 
+    agent_label=emd_constants.RJ_SMTR_AGENT_LABEL.value,
+    overwrite_flow_params=constants.STU_GENERAL_CAPTURE_PARAMS.value
+)
+
+
+with Flow(
+    "SMTR: STU - Captura",
+    code_owners=["rodrigo", "rafaelpinheiro"],
+) as stu_captura:
+    # SETUP
+    data_versao_stu = Parameter("data_versao_stu", required=True)
+
+    timestamp = get_current_timestamp()
+
+    rename_flow_run = rename_current_flow_run_now_time(
+        prefix=stu_captura.name + " ",
+        now_time=timestamp,
+    )
+
+    LABELS = get_current_flow_labels()
+
+    # JOIN INDIVIDUAL FILES
+    raw_files = get_stu_raw_blobs(data_versao_stu=data_versao_stu)
+
+    raw_dfs = read_stu_raw_file.map(blob=raw_files)
+
+    df_pf, df_pj = create_final_stu_dataframe(dfs=raw_dfs)
+
+    SAVE_TABLE_FILES = save_stu_dataframes(df_pf=df_pf, df_pj=df_pj)
+
+    # CAPTURE
+    stu_capture_parameters = [
+        {"timestamp": data_versao_stu, **d}
+        for d in constants.STU_TABLE_CAPTURE_PARAMS.value
+    ]
+
+    run_captura = create_flow_run.map(
+        flow_name=unmapped(stu_captura_subflow.name),
+        project_name=unmapped(emd_constants.PREFECT_DEFAULT_PROJECT.value),
+        parameters=stu_capture_parameters,
+        labels=unmapped(LABELS),
+    )
+
+    run_captura.set_upstream(SAVE_TABLE_FILES)
+
+    wait_captura_true = wait_for_flow_run.map(
+        run_captura,
+        stream_states=unmapped(True),
+        stream_logs=unmapped(True),
+        raise_final_state=unmapped(True),
+    )
+
+stu_captura.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
+stu_captura.run_config = KubernetesRun(
+    image=emd_constants.DOCKER_IMAGE.value,
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
+)
+stu_captura.state_handlers = [handler_inject_bd_credentials, handler_initialize_sentry]
