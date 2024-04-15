128c287d6d2652dcf0d3de1437bc459408ed979c
hellcassius <caiorogerio.santos@gmail.com>
2024-04-15 10:13:27 -0300

    add stpl flows

---
+++ pipelines/br_rj_riodejaneiro_stpl_gps/flows.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Flows for br_rj_riodejaneiro_stpl_gps
+"""
+
+from prefect import Parameter
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+
+# EMD Imports #
+
+from pipelines.constants import constants as emd_constants
+from prefeitura_rio.pipelines_utils.custom import Flow
+
+# SMTR Imports #
+
+from pipelines.constants import constants
+
+from pipelines.schedules import (
+    every_minute,
+    # every_hour,
+)
+from pipelines.utils.backup.tasks import (
+    create_date_hour_partition,
+    create_local_partition_path,
+    get_current_timestamp,
+    get_raw,
+    parse_timestamp_to_string,
+    rename_current_flow_run_now_time,
+    save_raw_local,
+    save_treated_local,
+    upload_logs_to_bq,
+    bq_upload,
+)
+
+from pipelines.br_rj_riodejaneiro_stpl_gps.tasks import (
+    pre_treatment_br_rj_riodejaneiro_stpl_gps,
+)
+
+
+with Flow(
+    "SMTR: GPS STPL - Captura",
+    code_owners=["caio", "fernanda", "boris", "rodrigo"],
+) as captura_stpl:
+    # DEFAULT PARAMETERS #
+    dataset_id = Parameter(
+        "dataset_id", default=constants.GPS_STPL_RAW_DATASET_ID.value
+    )
+    table_id = Parameter("table_id", default=constants.GPS_STPL_RAW_TABLE_ID.value)
+    url = Parameter("url", default=constants.GPS_STPL_API_BASE_URL.value)
+    secret_path = Parameter(
+        "secret_path", default=constants.GPS_STPL_API_SECRET_PATH.value
+    )
+
+    # SETUP #
+    timestamp = get_current_timestamp()
+
+    rename_flow_run = rename_current_flow_run_now_time(
+        prefix=captura_stpl.name + " - ", now_time=timestamp
+    )
+
+    partitions = create_date_hour_partition(timestamp)
+
+    filename = parse_timestamp_to_string(timestamp)
+
+    filepath = create_local_partition_path(
+        dataset_id=constants.GPS_STPL_RAW_DATASET_ID.value,
+        table_id=constants.GPS_STPL_RAW_TABLE_ID.value,
+        filename=filename,
+        partitions=partitions,
+    )
+
+    raw_status = get_raw(url=url, headers=constants.GPS_STPL_API_SECRET_PATH.value)
+
+    raw_filepath = save_raw_local(status=raw_status, file_path=filepath)
+
+    # TREAT
+    treated_status = pre_treatment_br_rj_riodejaneiro_stpl_gps(
+        status_dict=raw_status, timestamp=timestamp
+    )
+
+    treated_filepath = save_treated_local(status=treated_status, file_path=filepath)
+
+    # LOAD
+    error = bq_upload(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        filepath=treated_filepath,
+        raw_filepath=raw_filepath,
+        partitions=partitions,
+        status=treated_status,
+    )
+    upload_logs_to_bq(
+        dataset_id=dataset_id,
+        parent_table_id=table_id,
+        timestamp=timestamp,
+        error=error,
+    )
+    # FLOW
+    # ? Congelado para evitar o TriggerFailed, permitindo testar as demais tasks
+    captura_stpl.set_dependencies(task=partitions, upstream_tasks=[rename_flow_run])
+
+
+captura_stpl.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
+captura_stpl.run_config = KubernetesRun(
+    image=emd_constants.DOCKER_IMAGE.value,
+    labels=[emd_constants.RJ_SMTR_AGENT_LABEL.value],
+)
+# Captura descontinuada (sem dados), avaliar quando voltar
+# captura_stpl.schedule = every_minute
