ebe6b408392f9ec1f2e038ee5b0ed26e4bf9f669
hellcassius <caiorogerio.santos@gmail.com>
2024-04-19 08:47:51 -0300

    add import to flows

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -7,3 +7,10 @@ from pipelines.capture.jae.flows import *  # noqa
 from pipelines.capture.templates.flows import *  # noqa
 from pipelines.exemplo import *  # noqa
 from pipelines.treatment.bilhetagem.flows import *  # noqa
+from pipelines.br_rj_riodejaneiro_diretorios.flows import *
+from pipelines.br_rj_riodejaneiro_gtfs.flows import *
+from pipelines.br_rj_riodejaneiro_onibus_gps.flows import *
+from pipelines.br_rj_riodejaneiro_rdo.flows import *
+from pipelines.br_rj_riodejaneiro_stpl_gps.flows import *
+from pipelines.br_rj_riodejaneiro_stu.flows import *
+
