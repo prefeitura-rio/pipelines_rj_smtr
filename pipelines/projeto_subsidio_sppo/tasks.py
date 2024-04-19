b700bda8a48f4af08288c7acf2391b55a73a913b
hellcassius <caiorogerio.santos@gmail.com>
2024-04-19 15:29:36 -0300

    fix get_secret import

--- pipelines/projeto_subsidio_sppo/tasks.py
+++ pipelines/projeto_subsidio_sppo/tasks.py
@@ -14,7 +14,8 @@ from pipelines.utils.backup.tasks import (
     perform_check,
     perform_checks_for_table,
 )
-from pipelines.utils.utils import get_vault_secret, log
+from pipelines.utils.utils import log
+from pipelines.utils.secret import get_secret
 
 
 @task
@@ -87,9 +88,7 @@ def subsidio_data_quality_check(
         else f'{params["start_date"]} a {params["end_date"]}'
     )
 
-    webhook_url = get_vault_secret(secret_path=smtr_constants.SUBSIDIO_SPPO_SECRET_PATH.value)[
-        "data"
-    ]["discord_data_check_webhook"]
+    webhook_url = get_secret(secret_path=smtr_constants.SUBSIDIO_SPPO_SECRET_PATH.value)["discord_data_check_webhook"]
 
     test_check = all(table["status"] for sublist in checks.values() for table in sublist)
 
