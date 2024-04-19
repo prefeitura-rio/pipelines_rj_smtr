abede7a5786492a2d678af1b41ed2c97b9d0568c
hellcassius <caiorogerio.santos@gmail.com>
2024-04-19 10:34:48 -0300

    add veiculo to flows

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -13,3 +13,4 @@ from pipelines.capture.jae.flows import *  # noqa
 from pipelines.capture.templates.flows import *  # noqa
 from pipelines.exemplo import *  # noqa
 from pipelines.treatment.bilhetagem.flows import *  # noqa
+from pipelines.veiculo.flows import * #noqa
