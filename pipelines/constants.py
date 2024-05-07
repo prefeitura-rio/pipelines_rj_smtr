fdbd3f0aaee12a71130cfdd99836222d18d0e25b
hellcassius <caiorogerio.santos@gmail.com>
2024-05-07 15:46:37 -0300

    add save local to pre_treatment_infracao

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -620,6 +620,21 @@ class constants(Enum):  # pylint: disable=c0103
 
     # INFRAÇÃO
     SPPO_INFRACAO_URL = "https://siurblab.rio.rj.gov.br/SMTR/Multas/multas.txt"
+
+    SPPO_INFRACAO_COLUMNS = [
+        "permissao",
+        "modo",
+        "placa",
+        "id_auto_infracao",
+        "data_infracao",
+        "valor",
+        "id_infracao",
+        "infracao",
+        "status",
+        "data_pagamento",
+        "servico",
+    ]
+
     SPPO_INFRACAO_MAPPING_KEYS = {
         "permissao": "permissao",
         "modal": "modo",
