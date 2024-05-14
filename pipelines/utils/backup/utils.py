7128d9f589a9ffd1c72ecff7c5ac13e1355de01b
Rafael <rafael.c.pinheiro96@gmail.com>
2024-05-14 16:58:50 -0300

    ajusta read_raw_data

--- pipelines/utils/backup/utils.py
+++ pipelines/utils/backup/utils.py
@@ -799,30 +799,30 @@ def get_datetime_range(
     return {"start": start, "end": end}
 
 
-def read_raw_data(filepath: str, csv_args: dict = None) -> tuple[str, pd.DataFrame]:
+def read_raw_data(filepath: str, reader_args: dict = None) -> tuple[str, pd.DataFrame]:
     """
     Read raw data from file
 
     Args:
         filepath (str): filepath to read
-        csv_args (dict): arguments to pass to pandas.read_csv
+        reader_args (dict): arguments to pass to pandas.read_csv or read_json
 
     Returns:
         tuple[str, pd.DataFrame]: error and data
     """
     error = None
     data = None
+    if reader_args is None:
+        reader_args = {}
     try:
         file_type = filepath.split(".")[-1]
 
         if file_type == "json":
-            data = pd.read_json(filepath)
+            data = pd.read_json(filepath, **reader_args)
 
             # data = json.loads(data)
         elif file_type in ("txt", "csv"):
-            if csv_args is None:
-                csv_args = {}
-            data = pd.read_csv(filepath, **csv_args)
+            data = pd.read_csv(filepath, **reader_args)
         else:
             error = "Unsupported raw file extension. Supported only: json, csv and txt"
 
