c0246fb82cc1eadfb2a5673b9aa466cda988bca6
Rafael <rafael.c.pinheiro96@gmail.com>
2024-05-14 20:28:06 -0300

    adiciona filtros auxiliar jae

--- pipelines/constants.py
+++ pipelines/constants.py
@@ -538,7 +538,9 @@ and createdDate lt {end})",
                         *
                     FROM
                         LINHA
-
+                    WHERE
+                        DT_INCLUSAO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": ["CD_LINHA"],  # id column to nest data on
@@ -559,7 +561,9 @@ and createdDate lt {end})",
                         TIPO_MODAL m
                     ON
                         o.CD_TIPO_MODAL = m.CD_TIPO_MODAL
-
+                    WHERE
+                        DT_INCLUSAO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": ["CD_OPERADORA_TRANSPORTE"],  # id column to nest data on
@@ -575,7 +579,9 @@ and createdDate lt {end})",
                         c.*
                     FROM
                         CLIENTE c
-
+                    WHERE
+                        DT_CADASTRO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": ["CD_CLIENTE"],  # id column to nest data on
@@ -598,7 +604,9 @@ and createdDate lt {end})",
                         CLIENTE c
                     ON
                         p.CD_CLIENTE = c.CD_CLIENTE
-
+                    WHERE
+                        c.DT_CADASTRO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": ["CD_CLIENTE"],  # id column to nest data on
@@ -620,7 +628,9 @@ and createdDate lt {end})",
                         tipo_gratuidade t
                     ON
                         g.id_tipo_gratuidade = t.id
-
+                    WHERE
+                        g.data_inclusao BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": ["id"],  # id column to nest data on
@@ -637,7 +647,9 @@ and createdDate lt {end})",
                         *
                     FROM
                         CONSORCIO
-
+                    WHERE
+                        DT_INCLUSAO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": ["CD_CONSORCIO"],  # id column to nest data on
@@ -653,7 +665,9 @@ and createdDate lt {end})",
                           *
                       FROM
                           percentual_rateio_integracao
-
+                      WHERE
+                          dt_inclusao BETWEEN '{start}'
+                          AND '{end}'
                   """,
             },
             "primary_key": ["id"],  # id column to nest data on
@@ -678,15 +692,16 @@ and createdDate lt {end})",
                         OPERADORA_TRANSPORTE o
                     ON
                         o.CD_CLIENTE = c.CD_CLIENTE
-
+                    WHERE
+                        {update}
                 """,
-                # "get_updates": [
-                #     "c.cd_cliente",
-                #     "c.cd_agencia",
-                #     "c.cd_tipo_conta",
-                #     "c.nr_banco",
-                #     "c.nr_conta",
-                # ],
+                "get_updates": [
+                    "c.cd_cliente",
+                    "c.cd_agencia",
+                    "c.cd_tipo_conta",
+                    "c.nr_banco",
+                    "c.nr_conta",
+                ],
             },
             "primary_key": ["CD_CLIENTE"],  # id column to nest data on
             "interval_minutes": BILHETAGEM_TRATAMENTO_INTERVAL,
@@ -702,7 +717,9 @@ and createdDate lt {end})",
                         *
                     FROM
                         CONTATO_PESSOA_JURIDICA
-
+                    WHERE
+                        DT_INCLUSAO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": [
@@ -722,7 +739,11 @@ and createdDate lt {end})",
                         *
                     FROM
                         SERVICO_MOTORISTA
-
+                    WHERE
+                        DT_ABERTURA BETWEEN '{start}'
+                        AND '{end}'
+                        OR DT_FECHAMENTO BETWEEN '{start}'
+                        AND '{end}'
                 """,
             },
             "primary_key": [
