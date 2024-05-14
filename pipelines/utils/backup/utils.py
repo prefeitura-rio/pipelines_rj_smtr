c5272723bb68bbb4fed13185636ba2d74ebf5b58
Rafael <rafael.c.pinheiro96@gmail.com>
2024-05-14 17:29:51 -0300

    ajusta funções captura

--- pipelines/utils/backup/utils.py
+++ pipelines/utils/backup/utils.py
@@ -11,6 +11,7 @@ import traceback
 import zipfile
 from datetime import date, datetime, timedelta
 from ftplib import FTP
+from functools import partial
 from pathlib import Path
 from typing import Any, List, Union
 
@@ -22,8 +23,10 @@ import psycopg2.extras
 import pymysql
 import pytz
 import requests
-from basedosdados import Table
+from basedosdados import Storage, Table
+from basedosdados.upload.datatypes import Datatype
 from google.api_core.exceptions import BadRequest
+from google.cloud import bigquery
 from google.cloud.storage.blob import Blob
 from prefect.schedules.clocks import IntervalClock
 
@@ -76,27 +79,137 @@ def log_critical(message: str, secret_path: str = constants.CRITICAL_SECRET_PATH
     return send_discord_message(message=message, webhook_url=url)
 
 
-def create_or_append_table(dataset_id: str, table_id: str, path: str, partitions: str = None):
+def create_bq_table_schema(
+    data_sample_path: Union[str, Path],
+) -> list[bigquery.SchemaField]:
+    """
+    Create the bq schema based on the structure of data_sample_path.
+
+    Args:
+        data_sample_path (str, Path): Data sample path to auto complete columns names
+
+    Returns:
+        list[bigquery.SchemaField]: The table schema
+    """
+
+    data_sample_path = Path(data_sample_path)
+
+    if data_sample_path.is_dir():
+        data_sample_path = [
+            f for f in data_sample_path.glob("**/*") if f.is_file() and f.suffix == ".csv"
+        ][0]
+
+    columns = Datatype(source_format="csv").header(data_sample_path=data_sample_path)
+
+    schema = []
+    for col in columns:
+        schema.append(bigquery.SchemaField(name=col, field_type="STRING", description=None))
+    return schema
+
+
+def create_bq_external_table(table_obj: Table, path: str, bucket_name: str):
+    """Creates an BigQuery External table based on sample data
+
+    Args:
+        table_obj (Table): BD Table object
+        path (str): Table data local path
+        bucket_name (str, Optional): The bucket name where the data is located
+    """
+
+    Storage(
+        dataset_id=table_obj.dataset_id,
+        table_id=table_obj.table_id,
+        bucket_name=bucket_name,
+    ).upload(
+        path=path,
+        mode="staging",
+        if_exists="replace",
+    )
+
+    bq_table = bigquery.Table(table_obj.table_full_name["staging"])
+    project_name = table_obj.client["bigquery_prod"].project
+    table_full_name = table_obj.table_full_name["prod"].replace(
+        project_name, f"{project_name}.{bucket_name}", 1
+    )
+    bq_table.description = f"staging table for `{table_full_name}`"
+
+    bq_table.external_data_configuration = Datatype(
+        dataset_id=table_obj.dataset_id,
+        table_id=table_obj.table_id,
+        schema=create_bq_table_schema(
+            data_sample_path=path,
+        ),
+        mode="staging",
+        bucket_name=bucket_name,
+        partitioned=True,
+        biglake_connection_id=None,
+    ).external_config
+
+    table_obj.client["bigquery_staging"].create_table(bq_table)
+
+
+def create_or_append_table(
+    dataset_id: str,
+    table_id: str,
+    path: str,
+    partitions: str = None,
+    bucket_name: str = None,
+):
     """Conditionally create table or append data to its relative GCS folder.
 
     Args:
         dataset_id (str): target dataset_id on BigQuery
         table_id (str): target table_id on BigQuery
         path (str): Path to .csv data file
+        partitions (str): partition string.
+        bucket_name (str, Optional): The bucket name to save the data.
     """
-    tb_obj = Table(table_id=table_id, dataset_id=dataset_id)
-    if not tb_obj.table_exists("staging"):
-        log("Table does not exist in STAGING, creating table...")
-        dirpath = path.split(partitions)[0]
-        tb_obj.create(
+    table_arguments = {"table_id": table_id, "dataset_id": dataset_id}
+    if bucket_name is not None:
+        table_arguments["bucket_name"] = bucket_name
+
+    tb_obj = Table(**table_arguments)
+    dirpath = path.split(partitions)[0]
+
+    if bucket_name is not None:
+        create_func = partial(
+            create_bq_external_table,
+            table_obj=tb_obj,
+            path=dirpath,
+            bucket_name=bucket_name,
+        )
+
+        append_func = partial(
+            Storage(dataset_id=dataset_id, table_id=table_id, bucket_name=bucket_name).upload,
+            path=path,
+            mode="staging",
+            if_exists="replace",
+            partitions=partitions,
+        )
+
+    else:
+        create_func = partial(
+            tb_obj.create,
             path=dirpath,
             if_table_exists="pass",
             if_storage_data_exists="replace",
         )
+
+        append_func = partial(
+            tb_obj.append,
+            filepath=path,
+            if_exists="replace",
+            timeout=600,
+            partitions=partitions,
+        )
+
+    if not tb_obj.table_exists("staging"):
+        log("Table does not exist in STAGING, creating table...")
+        create_func()
         log("Table created in STAGING")
     else:
         log("Table already exists in STAGING, appending to it...")
-        tb_obj.append(filepath=path, if_exists="replace", timeout=600, partitions=partitions)
+        append_func()
         log("Appended to table on STAGING successfully.")
 
 
