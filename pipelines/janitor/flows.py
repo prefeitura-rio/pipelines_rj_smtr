6235786aeb86e17920dd142a17d21416a147d9a9
hellcassius <caiorogerio.santos@gmail.com>
2024-05-28 17:09:21 -0300

    remove code_owners

--- pipelines/janitor/flows.py
+++ pipelines/janitor/flows.py
@@ -14,13 +14,11 @@ from pipelines.janitor.tasks import (
 )
 
 with Flow(
-    "SMTR: Desagendamento de runs arquivadas", code_owners=["caio"]
+    "SMTR: Desagendamento de runs arquivadas",
 ) as janitor_flow:
     client = get_prefect_client()
     flows = query_active_flow_names(prefect_client=client)
-    archived_flow_runs = query_not_active_flows.map(
-        flows=flows, prefect_client=unmapped(client)
-    )
+    archived_flow_runs = query_not_active_flows.map(flows=flows, prefect_client=unmapped(client))
     cancel_flows.map(flows=archived_flow_runs, prefect_client=unmapped(client))
 
 janitor_flow.storage = GCS(emd_constants.GCS_FLOWS_BUCKET.value)
--- pipelines/janitor/tasks.py
+++ pipelines/janitor/tasks.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from typing import Dict, List
+# from typing import Dict, List
 import traceback
 from datetime import datetime
 
@@ -184,9 +184,7 @@ query($flow_name: String, $offset: Int){
         for flow_run in flow["flow_runs"]:
             if flow["flow_runs"]:
                 archived_flow_runs.append(flow_run)
-            log(
-                f"Got flow_run {flow_run['id']}, scheduled: {flow_run['scheduled_start_time']}"
-            )
+            log(f"Got flow_run {flow_run['id']}, scheduled: {flow_run['scheduled_start_time']}")
 
     if archived_flow_runs:
         log(f"O Flow {flow_name} possui runs a serem canceladas")
@@ -221,9 +219,7 @@ def cancel_flows(flows, prefect_client: Client = None) -> None:
 
     for flow in flows:
         try:
-            response = prefect_client.graphql(
-                query=query, variables=dict(flow_id=flow["id"])
-            )
+            response = prefect_client.graphql(query=query, variables=dict(flow_id=flow["id"]))
             # state: str = response["data"]["cancel_flow_run"]["state"]
             log(response)
             log(f">>>>>>>>>> Flow run {flow['id']} arquivada")
