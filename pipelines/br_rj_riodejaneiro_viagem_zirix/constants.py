83d95d87b09b38923892ea8c61c796f09e8499bb
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-06-19 18:16:53 -0300

    adiciona zirix na task create_request_params

--- pipelines/br_rj_riodejaneiro_viagem_zirix/constants.py
+++ pipelines/br_rj_riodejaneiro_viagem_zirix/constants.py
@@ -5,16 +5,16 @@ Constant values for rj_smtr br_rj_riodejaneiro_viagem_zirix
 
 from enum import Enum
 
+from pipelines.constants import constants as smtr_constants
+
 
 class constants(Enum):  # pylint: disable=c0103
     """
     Constant values for rj_smtr br_rj_riodejaneiro_viagem_zirix
     """
 
-    VIAGEM_ZIRIX_RAW_DATASET_ID = "br_rj_riodejaneiro_viagem_zirix"
-
     VIAGEM_CAPTURE_PARAMETERS = {
-        "dataset_id": VIAGEM_ZIRIX_RAW_DATASET_ID,
+        "dataset_id": smtr_constants.VIAGEM_ZIRIX_RAW_DATASET_ID.value,
         "table_id": "viagem_informada",
         "partition_date_only": False,
         "extract_params": {"delay_minutes": 5},
