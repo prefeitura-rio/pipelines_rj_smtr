3448670e3d6786c9ecd97f47bee719a4c0ff7363
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-06-24 17:43:16 -0300

    adiciona alias no modelo sppo_veiculo_dia para teste

--- queries/models/veiculo/sppo_veiculo_dia.sql
+++ queries/models/veiculo/sppo_veiculo_dia.sql
@@ -5,6 +5,7 @@
         partition_by={"field": "data", "data_type": "date", "granularity": "day"},
         unique_key=["data", "id_veiculo"],
         incremental_strategy="insert_overwrite",
+        alias='sppo_veiculo_dia_teste',
     )
 }}
 
