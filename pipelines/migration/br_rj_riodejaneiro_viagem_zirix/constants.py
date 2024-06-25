6aa65ff7779224cc4492dc307da632338cd92c31
Rafael <rafael.c.pinheiro96@gmail.com>
2024-06-25 16:29:59 -0300

    cria flow materializacao de viagem da zirix

--- pipelines/migration/br_rj_riodejaneiro_viagem_zirix/constants.py
+++ pipelines/migration/br_rj_riodejaneiro_viagem_zirix/constants.py
@@ -22,3 +22,19 @@ class constants(Enum):  # pylint: disable=c0103
         "interval_minutes": 10,
         "source_type": "api-json",
     }
+
+    VIAGEM_MATERIALIZACAO_PARAMS = {
+        "dataset_id": smtr_constants.VIAGEM_ZIRIX_RAW_DATASET_ID.value,
+        "table_id": "viagem_informada",
+        "upstream": True,
+        "dbt_vars": {
+            "date_range": {
+                "table_run_datetime_column_name": "data",
+                "delay_hours": 1,
+            },
+            "version": {},
+        },
+        "source_dataset_ids": [smtr_constants.VIAGEM_ZIRIX_RAW_DATASET_ID.value],
+        "source_table_ids": [VIAGEM_CAPTURE_PARAMETERS["table_id"]],
+        "capture_intervals_minutes": [VIAGEM_CAPTURE_PARAMETERS["interval_minutes"]],
+    }
