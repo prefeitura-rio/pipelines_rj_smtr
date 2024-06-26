e74805c31c09e303155fd7bee79f9a206fcefee3
Rafael <rafael.c.pinheiro96@gmail.com>
2024-06-26 11:19:37 -0300

    altera logica incremental e adiciona colunas

--- queries/models/br_rj_riodejaneiro_viagem_zirix/viagem_informada.sql
+++ queries/models/br_rj_riodejaneiro_viagem_zirix/viagem_informada.sql
@@ -10,25 +10,91 @@
   )
 }}
 
+{% set incremental_filter %}
+  DATE(data) BETWEEN DATE("{{var('date_range_start')}}") AND DATE("{{var('date_range_end')}}")
+{% endset %}
+
+{% set staging_viagem_informada = ref('staging_viagem_informada') %}
+{% if execute %}
+  {% if is_incremental() %}
+    {% set partitions_query %}
+      SELECT DISTINCT
+        CONCAT("'", DATE(data_viagem), "'") AS data_viagem
+      FROM
+        {{ staging_viagem_informada }}
+      WHERE
+        {{ incremental_filter }}
+    {% endset %}
+
+    {% set partitions = run_query(partitions_query)[0].values() %}
+  {% endif %}
+{% endif %}
+
+WITH staging_data AS (
+  SELECT
+    data_viagem AS data,
+    datetime_partida,
+    datetime_chegada,
+    datetime_processamento,
+    timestamp_captura AS datetime_captura,
+    id_veiculo,
+    trip_id,
+    route_id,
+    shape_id,
+    servico,
+    CASE
+      WHEN sentido = 'I' THEN 'Ida'
+      WHEN sentido = 'V' THEN 'Volta'
+      ELSE sentido
+    END AS sentido,
+    id_viagem
+  FROM
+    {{ staging_viagem_informada }}
+  {% if is_incremental() %}
+    WHERE
+      {{ incremental_filter }}
+  {% endif %}
+),
+complete_partitions AS (
+  SELECT
+    *,
+    0 AS priority
+  FROM
+    staging_data
+
+  {% if is_incremental() and partitions|length > 0 %}
+    UNION ALL
+
+    SELECT
+      * EXCEPT(versao, datetime_ultima_atualizacao),
+      1 AS priority
+    FROM
+      {{ this }}
+    WHERE
+      data IN ({{ partitions|join(', ') }})
+  {% endif %}
+),
+deduplicado AS (
+  SELECT
+    * EXCEPT(rn, priority)
+  FROM
+    (
+      SELECT
+        *,
+        ROW_NUMBER() OVER(PARTITION BY id_viagem ORDER BY datetime_captura DESC, priority) AS rn
+      FROM
+        complete_partitions
+    )
+  WHERE
+    rn = 1
+)
 SELECT
-  data_viagem AS data,
-  datetime_partida,
-  datetime_chegada,
-  datetime_processamento,
-  id_veiculo,
-  trip_id,
-  route_id,
-  shape_id,
-  servico,
-  CASE
-    WHEN sentido = 'I' THEN 'Ida'
-    WHEN sentido = 'V' THEN 'Volta'
-    ELSE sentido
-  END AS sentido,
-  '{{ var("version") }}' AS versao
+  *,
+  '{{ var("version") }}' AS versao,
+  CURRENT_DATETIME("America/Sao_Paulo") as datetime_ultima_atualizacao
 FROM
-  {{ ref("staging_viagem_informada") }}
+  {{ staging_viagem_informada }}
 {% if is_incremental() %}
   WHERE
-    data BETWEEN DATE("{{var('date_range_start')}}") AND DATE("{{var('date_range_end')}}")
+    {{ incremental_filter }}
 {% endif %}
