490badc2314e6ce6463ad1a66ca0bd2ae77c9781
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-07-17 14:18:12 -0300

    Cria tabelas de validação da Jaé com verificação no GTFS / Remodela tabela cadastro.servicos (#98)
    * cria validação da jae com o gtfs
    
    * validacao ordem pagamento staging
    
    * adiciona operadoras de fretamento
    
    * altera aux_servicos_gtfs para ephemeral
    
    * altera filtro incremental da integracao_invalida
    
    * altera filtro incremental da transacao_invalida
    
    * corrige aux_transacao_ordem
    
    * corrige validacao ordem_pagamento por servico
    
    * cria modelo ordem_pagamento_dia_invalida
    
    * cria validacao ordem_pagamento final
    
    * altera schema validacao_dados_jae
    
    * agenda pipeline de materializacao das validacoes
    
    * corrige query viagem_informada
    
    * add changelog
    
    * adiciona prefixo data nas colunas de vigencia
    
    * corrige nome das colunas de vigencia
    
    * altera schema da tabela de servicos
    
    * add data dbt
    
    * corrige typo
    
    * altera cast para safe_cast
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ queries/models/validacao_dados_jae/integracao_invalida.sql
@@ -0,0 +1,85 @@
+{{
+  config(
+    incremental_strategy="insert_overwrite",
+    partition_by={
+      "field": "data",
+      "data_type": "date",
+      "granularity": "day"
+    },
+  )
+}}
+
+{% set integracao_table = ref('integracao') %}
+{% if execute %}
+  {% if is_incremental() %}
+
+    {% set partitions_query %}
+      SELECT
+        CONCAT("'", PARSE_DATE("%Y%m%d", partition_id), "'") AS data
+      FROM
+        `{{ integracao_table.database }}.{{ integracao_table.schema }}.INFORMATION_SCHEMA.PARTITIONS`
+      WHERE
+        table_name = "{{ integracao_table.identifier }}"
+        AND partition_id != "__NULL__"
+        AND DATE(last_modified_time, "America/Sao_Paulo") = DATE_SUB(DATE("{{var('run_date')}}"), INTERVAL 1 DAY)
+    {% endset %}
+
+    {{ log("Running query: \n"~partitions_query, info=True) }}
+    {% set partitions = run_query(partitions_query) %}
+
+    {% set partition_list = partitions.columns[0].values() %}
+    {{ log("integracao partitions: \n"~partition_list, info=True) }}
+  {% endif %}
+{% endif %}
+
+WITH sequencias_validas AS (
+  SELECT
+    id_matriz_integracao,
+    STRING_AGG(modo, ', ' ORDER BY sequencia_integracao) AS modos
+  FROM
+    {{ ref("matriz_integracao") }}
+  GROUP BY
+    id_matriz_integracao
+),
+integracao_agg AS (
+  SELECT
+    DATE(datetime_processamento_integracao) AS data,
+    id_integracao,
+    STRING_AGG(modo, ', ' ORDER BY sequencia_integracao) AS modos,
+    MIN(datetime_transacao) AS datetime_primeira_transacao,
+    MAX(datetime_transacao) AS datetime_ultima_transacao,
+    MIN(intervalo_integracao) AS menor_intervalo
+  FROM
+    {{ ref("integracao") }}
+  WHERE
+    {% if is_incremental() %}
+      {% if partition_list|length > 0 %}
+        data IN ({{ partition_list|join(', ') }})
+      {% else %}
+        data = "2000-01-01"
+      {% endif %}
+    {% endif %}
+  GROUP BY
+    1,
+    2
+),
+indicadores AS (
+  SELECT
+    data,
+    id_integracao,
+    modos,
+    modos NOT IN (SELECT DISTINCT modos FROM sequencias_validas) AS indicador_fora_matriz,
+    TIMESTAMP_DIFF(datetime_ultima_transacao, datetime_primeira_transacao, MINUTE) > 180 AS indicador_tempo_integracao_invalido,
+    menor_intervalo < 5 AS indicador_intervalo_transacao_suspeito
+  FROM
+    integracao_agg
+)
+SELECT
+  *,
+  '{{ var("version") }}' as versao
+FROM
+  indicadores
+WHERE
+  indicador_fora_matriz = TRUE
+  OR indicador_tempo_integracao_invalido = TRUE
+  OR indicador_intervalo_transacao_suspeito = TRUE
