9667a3e33de3261598b874e0d5a51a067c45eea0
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-08-19 17:46:34 -0300

    altera ref subsidio_parametros

--- queries/models/dashboard_subsidio_sppo/viagens_remuneradas.sql
+++ queries/models/dashboard_subsidio_sppo/viagens_remuneradas.sql
@@ -69,8 +69,8 @@ WITH
     subsidio_km,
     MAX(subsidio_km) OVER (PARTITION BY data_inicio, data_fim) AS subsidio_km_teto
   FROM
-    -- {{ ref("subsidio_valor_km_tipo_viagem") }}
-    rj-smtr-staging.dashboard_subsidio_sppo_staging.subsidio_valor_km_tipo_viagem
+    {{ ref("subsidio_valor_km_tipo_viagem") }}
+    -- rj-smtr-staging.dashboard_subsidio_sppo_staging.subsidio_valor_km_tipo_viagem
 ),
 -- 3. Viagens com quantidades de transações
   viagem_transacao AS (
