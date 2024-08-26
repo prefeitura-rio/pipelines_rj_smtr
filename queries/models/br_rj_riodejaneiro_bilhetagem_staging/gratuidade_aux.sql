e193f01d0411494655aed64da149c87bb247220d
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-08-26 16:42:46 -0300

    HOTFIX - Ajusta filtro incremental `passageiros_hora` e `passageiros_tile_hora` (#166)
    * altera para ephemeral
    
    * adiciona filtre de timestamp_captura
    
    * separa materializacao transacao e passageiros_hora
    
    * corrige table_id
    
    * otimiza filtro de particao
    
    * add dependencia transacao
    
    * add captura endereco e linha_op_transporte
    
    * cria modelo
    
    * add coluna geo_point_transacao
    
    * add particoes integracao
    
    * add linha_consorcio_operadora_transporte/endereco
    
    * add changelog
    
    * corrige referencia
    
    * registra flows
    
    * ajusta filtro incremental
    
    * registra flows
    
    * corrige gratuidade
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- queries/models/br_rj_riodejaneiro_bilhetagem_staging/gratuidade_aux.sql
+++ queries/models/br_rj_riodejaneiro_bilhetagem_staging/gratuidade_aux.sql
@@ -1,6 +1,6 @@
 {{
   config(
-    materialized="incremental",
+    materialized="table",
     incremental_strategy="merge",
     unique_key="id_gratuidade",
     partition_by={
@@ -18,27 +18,6 @@
 
 {% set staging_gratuidade = ref('staging_gratuidade') %}
 
-{% set incremental_filter %}
-    DATE(data) BETWEEN DATE("{{var('date_range_start')}}") AND DATE("{{var('date_range_end')}}")
-    AND timestamp_captura BETWEEN DATETIME("{{var('date_range_start')}}") AND DATETIME("{{var('date_range_end')}}")
-{% endset %}
-
-{% if execute %}
-    {% if is_incremental() %}
-        {% set partitions_query %}
-            SELECT DISTINCT
-                CAST(CAST(cd_cliente AS FLOAT64) AS INT64) AS cd_cliente
-            FROM
-                {{ staging_gratuidade }}
-            WHERE
-                {{ incremental_filter }}
-        {% endset %}
-
-        {% set partitions = run_query(partitions_query) %}
-
-        {% set partition_list = partitions.columns[0].values() %}
-    {% endif %}
-{% endif %}
 
 WITH gratuidade_complete_partitions AS (
     SELECT
@@ -49,25 +28,6 @@ WITH gratuidade_complete_partitions AS (
         timestamp_captura
     FROM
         {{ staging_gratuidade }}
-    {% if is_incremental() -%}
-        WHERE
-        {{ incremental_filter }}
-
-        {% if partition_list|length > 0 -%}
-            UNION ALL
-
-            SELECT
-                id_cliente,
-                id_gratuidade,
-                tipo_gratuidade,
-                data_inicio_validade,
-                timestamp_captura
-            FROM
-                {{ this }}
-            WHERE
-                id_cliente IN ({{ partition_list|join(', ') }})
-        {%- endif %}
-    {%- endif %}
 ),
 gratuidade_deduplicada AS (
     SELECT
