36655b33b0fc96869731273814fa7a723364ae4c
hellcassius <caiorogerio.santos@gmail.com>
2024-08-29 13:14:30 -0300

    add new behavior to replace docker tag, fix wait for fedora image

--- .github/workflows/scripts/replace_docker_tag.py
+++ .github/workflows/scripts/replace_docker_tag.py
@@ -11,16 +11,17 @@ from typing import List
 FILE_PATH = Path("./pipelines/constants.py")
 REPLACE_TAG = "AUTO_REPLACE_DOCKER_TAG"
 REPLACE_IMAGE = "AUTO_REPLACE_DOCKER_IMAGE"
-
+REPLACE_FEDORA_IMAGE = "AUTO_REPLACE_FEDORA_IMAGE"
+REPLACE_FEDORA_TAG = "AUTO_REPLACE_FEDORA_TAG"
 
 def get_name_version_from_args() -> List[str]:
     """
     Returns the version from the command line arguments.
     """
-    if len(argv) != 3:
-        print("Usage: replace_docker_tag.py <image_name> <version>")
+    if len(argv) != 4:
+        print("Usage: replace_docker_tag.py <None/Fedora> <image_name> <version>")
         exit(1)
-    return argv[1], argv[2]
+    return argv[1], argv[2], argv[3]
 
 
 def replace_in_text(orig_text: str, find_text: str, replace_text: str) -> str:
@@ -30,18 +31,25 @@ def replace_in_text(orig_text: str, find_text: str, replace_text: str) -> str:
     return orig_text.replace(find_text, replace_text)
 
 
-def update_file(file_path: Path, image_name: str, version: str) -> None:
+def update_file(file_path: Path,image_name: str, version: str, mode:str=None) -> None:
     """
     Updates the `DOCKER_TAG` variable in the `constants.py` file.
     """
     with file_path.open("r") as file:
         text = file.read()
-    text = replace_in_text(text, REPLACE_TAG, version)
-    text = replace_in_text(text, REPLACE_IMAGE, image_name)
+    if mode.lower() == 'fedora':
+        replace_tag = REPLACE_FEDORA_TAG
+        replace_image = REPLACE_FEDORA_IMAGE
+    else:
+        replace_tag=REPLACE_TAG
+        replace_image=REPLACE_IMAGE
+        
+    text = replace_in_text(text, replace_tag, version)
+    text = replace_in_text(text, replace_image, image_name)
     with file_path.open("w") as file:
         file.write(text)
 
 
 if __name__ == "__main__":
-    image_name, version = get_name_version_from_args()
-    update_file(FILE_PATH, image_name, version)
+    mode, image_name, version = get_name_version_from_args()
+    update_file(FILE_PATH, image_name, version, mode=mode)
