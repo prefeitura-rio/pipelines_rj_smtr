8753caaffef92b94d066b6b33f0d0720b854d775
hellcassius <caiorogerio.santos@gmail.com>
2024-09-03 15:01:43 -0300

    better setup

--- pipelines/serpro/tasks.py
+++ pipelines/serpro/tasks.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from time import sleep
-from pathlib import Path
 from prefect import task
 import os
 
-from pipelines.utils.secret import get_secret   
+from pipelines.utils.secret import get_secret  
+from pipelines.utils.utils import log 
 
 @task
 def wait_sleeping(interval_seconds: int = 3600, wait=None):
@@ -13,6 +13,8 @@ def wait_sleeping(interval_seconds: int = 3600, wait=None):
 @task
 def setup_serpro(secret_path:str='radar_serpro'):
     data = get_secret(secret_path=secret_path)['setup.sh']
+    log('Got Secret')
+    os.popen('touch setup.sh')
     with open('setup.sh','w') as f:
         f.write(data)
     return os.popen("sh setup.sh")
