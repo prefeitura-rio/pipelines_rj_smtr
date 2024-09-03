e828065e1aceae06446b6b6f3c771383938c8254
vtr363 <victor.m.rocha.vr@gmail.com>
2024-09-03 12:26:21 -0300

    cria tabela de autuaçoes com dados da citran

---
+++ queries/models/infracao/autuacoes.sql
@@ -0,0 +1,103 @@
+-- models/smtr_autuacoes.sql
+
+{{ config(
+    materialized='incremental',
+    partition_by={
+      "field":"data",
+      "data_type":"date",
+      "granularity": "day"
+    },
+    unique_key='id_autuacao',
+    incremental_strategy='insert_overwrite'
+) }}
+
+WITH citran AS (
+
+    select 
+        DATE(data)  data,
+        id_auto_infracao,
+        TIMESTAMP(concat(data,' ',hora,':00')) AS datetime_autuacao,
+        data_limite_defesa_previa,
+        data_limite_recurso,
+        NULL AS descricao_situacao_autuacao,
+        status_infracao,
+        codigo_enquadramento,
+        tipificacao_resumida,
+        pontuacao,
+        NULL AS gravidade,
+        NULL AS amparo_legal,
+        tipo_veiculo,
+        descricao_veiculo,
+        NULL AS placa_veiculo,
+        NULL AS ano_fabricacao_veiculo,
+        NULL AS ano_modelo_veiculo,
+        NULL AS cor_veiculo,
+        especie_veiculo,
+        NULL AS uf_infrator,
+        NULL AS uf_principal_condutor,
+        uf_proprietario,
+        cep_proprietario,
+        valor_infracao,
+        valor_pago,
+        data_pagamento,
+        id_autuador,
+        descricao_autuador,
+        id_municipio_autuacao,
+        descricao_municipio,
+        "RJ" AS uf_autuacao,
+        NULL AS cep_autuacao, -- não padronizado na citran
+        NULL AS tile_autuacao,
+        processo_defesa_autuacao,
+        recurso_penalidade_multa,
+        processo_troca_real_infrator,
+        status_sne,
+        "CITRAN" AS fonte
+    FROM {{ ref('autuacoes_citran') }}
+    {% if is_incremental() %}
+        WHERE
+            data BETWEEN DATE("{var('date_range_start')}") AND DATE("{var('date_range_end')}")
+    {% endif %}
+)
+
+SELECT 
+    data,
+    GENERATE_UUID() AS id_autuacao,
+    id_auto_infracao,
+    datetime_autuacao,
+    data_limite_defesa_previa,
+    data_limite_recurso,
+    descricao_situacao_autuacao,
+    status_infracao,
+    codigo_enquadramento,
+    tipificacao_resumida,
+    pontuacao,
+    gravidade,
+    amparo_legal,
+    tipo_veiculo,
+    descricao_veiculo,
+    placa_veiculo,
+    ano_fabricacao_veiculo,
+    ano_modelo_veiculo,
+    cor_veiculo,
+    especie_veiculo,
+    uf_infrator,
+    uf_principal_condutor,
+    uf_proprietario,
+    cep_proprietario,
+    valor_infracao,
+    valor_pago,
+    data_pagamento,
+    id_autuador,
+    descricao_autuador,
+    id_municipio_autuacao,
+    descricao_municipio,
+    uf_autuacao, 
+    cep_autuacao,
+    tile_autuacao,
+    processo_defesa_autuacao,
+    recurso_penalidade_multa,
+    processo_troca_real_infrator,
+    status_sne,
+    fonte
+FROM
+    citran
--- queries/models/infracao/staging/autuacoes_citran.sql
+++ queries/models/infracao/staging/autuacoes_citran.sql
@@ -6,9 +6,9 @@
 
 
 SELECT
-  Cod__Detran,
   data,
   SAFE_CAST(JSON_VALUE(content,'$.Hora') AS STRING) hora,
+  Cod__Detran as id_auto_infracao, -- coluna oculta
   IF(JSON_VALUE(content, '$.DtLimDP') != '', SAFE_CAST(PARSE_DATE('%d/%m/%Y', JSON_VALUE(content,'$.DtLimDP')) AS STRING), NULL) data_limite_defesa_previa, -- data
   IF(JSON_VALUE(content, '$.DtLimR') != '', SAFE_CAST(PARSE_DATE('%d/%m/%Y', JSON_VALUE(content,'$.DtLimR')) AS STRING), NULL) data_limite_recurso, -- data
   SAFE_CAST(JSON_VALUE(content,'$.Situacao Atual') AS STRING) situacao_atual, 
@@ -18,17 +18,21 @@ SELECT
   SAFE_CAST(JSON_VALUE(content,'$.Po') AS STRING) pontuacao, 
   SAFE_CAST(JSON_VALUE(content,'$.Tipo') AS STRING) tipo_veiculo, 
   SAFE_CAST(JSON_VALUE(content,'$.Marca') AS STRING) descricao_veiculo, 
-  SAFE_CAST(JSON_VALUE(content,'$.Esp') AS STRING)especie_veiculo, 
+  SAFE_CAST(JSON_VALUE(content,'$.Esp') AS STRING) especie_veiculo, 
   SAFE_CAST(JSON_VALUE(content,'$.CDUF') AS STRING) uf_proprietario, 
-  SAFE_CAST(JSON_VALUE(content,'$.Cep') AS STRING) cep_proprietario,
-  SAFE_CAST(JSON_VALUE(content,'$.Ufir') AS NUMERIC) valor_pagamento, 
+  SAFE_CAST(JSON_VALUE(content,'$.Cep') AS STRING) cep_proprietario, -- coluna oculta
+  SAFE_CAST(JSON_VALUE(content,'$.Ufir') AS NUMERIC) valor_infracao, 
   SAFE_CAST(JSON_VALUE(content,'$.VlPagto') AS NUMERIC) valor_pago, 
   IF(JSON_VALUE(content, '$.DtPagto') != '', SAFE_CAST(PARSE_DATE('%d/%m/%Y', JSON_VALUE(content,'$.DtPagto')) AS STRING), NULL) data_pagamento, -- data
-  SAFE_CAST(JSON_VALUE(content,'$.Orgao') AS STRING) orgao, 
-  SAFE_CAST(JSON_VALUE(content,'$.LocInf') AS STRING) local_autuacao, 
-  SAFE_CAST(JSON_VALUE(content,'$.ProAutu') AS STRING) processo_defesa, -- Rever nome da coluna
-  SAFE_CAST(JSON_VALUE(content,'$.NotifPen') AS STRING) penalidade_primeira_instancia, -- Rever nome  dacoluna
-  SAFE_CAST(JSON_VALUE(content,'$.ProcRI') AS STRING) processo_troca_real_infrator,  -- Rever nome  dacoluna
+  "260010" AS id_autuador,
+  SAFE_CAST(JSON_VALUE(content,'$.Orgao') AS STRING) descricao_autuador, 
+  "6001" AS id_municipio_autuacao,
+  "RIO DE JANEIRO" AS descricao_municipio,
+  SAFE_CAST(JSON_VALUE(content,'$.LocInf') AS STRING) local_autuacao, -- coluna oculta
+  SAFE_CAST(JSON_VALUE(content,'$.ProAutu') AS STRING) processo_defesa_autuacao, 
+  SAFE_CAST(JSON_VALUE(content,'$.NotifPen') AS STRING) recurso_penalidade_multa,
+  SAFE_CAST(JSON_VALUE(content,'$.ProcRI') AS STRING) processo_troca_real_infrator, 
+  FALSE AS status_sne, 
 FROM
   {{ source('infracao_staging','autuacoes_citran') }} as t
 
