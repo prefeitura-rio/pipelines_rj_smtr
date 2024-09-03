f8ea0c905f8226e408944b68eb7ddddc4e1448ae
hellcassius <caiorogerio.santos@gmail.com>
2024-09-03 17:49:25 -0300

    add requirements, add JDBC class

--- pipelines/serpro/tasks.py
+++ pipelines/serpro/tasks.py
@@ -4,9 +4,7 @@ from time import sleep
 
 from prefect import task
 
-from pipelines.utils.secret import get_secret
-from pipelines.utils.utils import log
-
+from pipelines.utils.jdbc import JDBC
 
 @task
 def wait_sleeping(interval_seconds: int = 54000, wait=None):
@@ -14,10 +12,5 @@ def wait_sleeping(interval_seconds: int = 54000, wait=None):
 
 
 @task
-def setup_serpro(secret_path: str = "radar_serpro"):
-    data = get_secret(secret_path=secret_path)["setup.sh"]
-    log("Got Secret")
-    os.popen("touch setup.sh")
-    with open("setup.sh", "w") as f:
-        f.write(data)
-    return os.popen("sh setup.sh")
+def get_db_object(secret_path='radar_serpro', environment:str='dev'):
+    return JDBC(db_params_secret_path=secret_path, environment=environment)
---
+++ pipelines/utils/jdbc.py
@@ -0,0 +1,78 @@
+import jaydebeapi as jdb
+from typing import List
+
+from pipelines.utils.secret import get_secret
+
+
+class JDBC:
+    def __init__(
+        self,
+        db_params_secret_path:str,
+        environment:str='staging'
+    ) -> None:
+            self._environment = environment
+            self._secret_path = db_params_secret_path
+            self._conn_kwargs = self.get_conn_kwargs()
+            self._connection = self.connect()
+            self._cursor = self.get_cursor()
+        
+    def get_conn_kwargs(self):
+        
+        data = get_secret(
+        secret_path=self._secret_path,
+        environment=self._environment
+        )
+        conn_kwargs=dict(
+            jclassname=data['jclassname'],
+            user=data['user'],
+            password=data['password'],
+            url=data['url'],
+            jars=[data['jars']]
+        )
+        return conn_kwargs
+
+    def connect(self):
+        data = get_secret(
+        secret_path=self._secret_path,
+        environment=self._environment
+        )
+
+        return jdb.connect(
+            jclassname=data['jclassname'],
+            url=data['url'],
+            jars=rf"{data['jars']}",
+            driver_args=[data['user'], data['password']]
+        )
+
+    def get_cursor(self):
+        """
+        Returns a cursor for the JDBC database.
+        """
+        return self._connection.cursor()
+
+    def execute_query(self, query: str) -> None:
+        """
+        Execute query on the JDBC database.
+
+        Args:
+            query: The query to execute.
+        """
+        self._cursor.execute(query)
+
+    def get_columns(self) -> List[str]:
+        """
+        Returns the column names of the JDBC database.
+        """
+        return [column[0] for column in self._cursor.description]
+
+    def fetch_batch(self, batch_size: int) -> List[List]:
+        """
+        Fetches a batch of rows from the JDBC database.
+        """
+        return [list(item) for item in self._cursor.fetchmany(batch_size)]
+
+    def fetch_all(self) -> List[List]:
+        """
+        Fetches all rows from the JDBC database.
+        """
+        return [list(item) for item in self._cursor.fetchall()]
