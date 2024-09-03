9d7465c3dddf99d4c1596f924ae9a589940ad034
hellcassius <caiorogerio.santos@gmail.com>
2024-09-03 13:34:23 -0300

    add setup script

--- pipelines/serpro/flows.py
+++ pipelines/serpro/flows.py
@@ -4,9 +4,10 @@ from prefect.storage import GCS
 from prefeitura_rio.pipelines_utils.custom import Flow
 
 from pipelines.constants import constants as smtr_constants
-from pipelines.serpro.tasks import wait_sleeping
+from pipelines.serpro.tasks import setup_serpro, wait_sleeping
 
 with Flow("SMTR - Teste Conexão Serpro") as flow:
+    setup_serpro()
     wait_sleeping()
 
 flow.storage = GCS(smtr_constants.GCS_FLOWS_BUCKET.value)
@@ -14,5 +15,3 @@ flow.run_config = KubernetesRun(
     image=smtr_constants.DOCKER_IMAGE_FEDORA.value,
     labels=[smtr_constants.RJ_SMTR_AGENT_LABEL.value],
 )
-import pyodbc
-pyodbc.connect()
--- pipelines/serpro/tasks.py
+++ pipelines/serpro/tasks.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 from time import sleep
-
+from pathlib import Path
 from prefect import task
+import os
 
+from pipelines.utils.secret import get_secret   
 
 @task
-def wait_sleeping(interval_seconds: int = 3600):
+def wait_sleeping(interval_seconds: int = 3600, wait=None):
     sleep(interval_seconds)
+
+@task
+def setup_serpro(secret_path:str='radar_serpro'):
+    data = get_secret(secret_path=secret_path)['setup.sh']
+    with open('setup.sh','w') as f:
+        f.write(data)
+    return os.popen("sh setup.sh")
