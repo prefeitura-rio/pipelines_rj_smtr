40a1d723d7dbccc4c24cdcb81349b331ac28c9b7
vtr363 <victor.m.rocha.vr@gmail.com>
2024-09-06 12:36:20 -0300

    cria tabela de receita de autuação

---
+++ queries/models/transito/receita_autuacao.sql
@@ -0,0 +1,36 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH receita_unpivot AS (
+  SELECT
+    ano,
+    CASE
+      WHEN mes = 'janeiro' THEN '01'
+      WHEN mes = 'fevereiro' THEN '02'
+      WHEN mes = 'marco' THEN '03'
+      WHEN mes = 'abril' THEN '04'
+      WHEN mes = 'maio' THEN '05'
+      WHEN mes = 'junho' THEN '06'
+      WHEN mes = 'julho' THEN '07'
+      WHEN mes = 'agosto' THEN '08'
+      WHEN mes = 'setembro' THEN '09'
+      WHEN mes = 'outubro' THEN '10'
+      WHEN mes = 'novembro' THEN '11'
+      WHEN mes = 'dezembro' THEN '12'
+    END AS mes,
+    SAFE_CAST( REPLACE(REPLACE(valor_arrecadacao, '.', ''), ',', '.') AS NUMERIC ) AS valor_arrecadacao
+  FROM `rj-smtr-dev.transito_staging.receita_autuacao`
+  UNPIVOT (
+    valor_arrecadacao FOR mes IN (janeiro, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro)
+  )
+  
+)
+
+SELECT
+  PARSE_DATE('%Y-%m-%d', CONCAT(ano, '-', mes, '-01')) AS data,
+  ano,
+  mes,
+  valor_arrecadacao,
+FROM receita_unpivot
+WHERE valor_arrecadacao IS NOT NULL
