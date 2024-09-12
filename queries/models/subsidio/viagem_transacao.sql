57ece04df876663f932e328b67d31176308eacc9
Rodrigo Cunha <66736583+eng-rodrigocunha@users.noreply.github.com>
2024-09-12 14:47:01 -0300

    Corrige viagens sem transação (#210)
    * commit inicial
    
    * Ajusta estado_equipamento_aux
    
    * corrige referencias das tabelas
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * atualiza changelog
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: vtr363 <victor.m.rocha.vr@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- queries/models/subsidio/viagem_transacao.sql
+++ queries/models/subsidio/viagem_transacao.sql
@@ -7,7 +7,49 @@
 }}
 
 WITH
--- 1. Viagens realizadas
+  -- 1. Transações Jaé
+  transacao AS (
+    SELECT
+      id_veiculo,
+      datetime_transacao
+    FROM
+      {{ ref("transacao") }}
+      -- rj-smtr.br_rj_riodejaneiro_bilhetagem.transacao
+    WHERE
+    data BETWEEN DATE("{{ var("start_date") }}")
+    AND DATE_ADD(DATE("{{ var("end_date") }}"), INTERVAL 1 DAY)
+  ),
+  -- 2. Transações RioCard
+  transacao_riocard AS (
+    SELECT
+      id_veiculo,
+      datetime_transacao
+    FROM
+      {{ ref("transacao_riocard") }}
+      -- rj-smtr.br_rj_riodejaneiro_bilhetagem.transacao_riocard
+    WHERE
+      data BETWEEN DATE("{{ var("start_date") }}")
+      AND DATE_ADD(DATE("{{ var("end_date") }}"), INTERVAL 1 DAY)
+  ),
+  -- 3. GPS Validador
+  gps_validador AS (
+    SELECT
+      data,
+      datetime_gps,
+      id_veiculo,
+      id_validador,
+      estado_equipamento,
+      latitude,
+      longitude
+    FROM
+      {{ ref("gps_validador") }}
+      -- rj-smtr.br_rj_riodejaneiro_bilhetagem.gps_validador
+    WHERE
+      data BETWEEN DATE("{{ var("start_date") }}")
+      AND DATE_ADD(DATE("{{ var("end_date") }}"), INTERVAL 1 DAY)
+      AND (latitude != 0 OR longitude != 0)
+  ),
+  -- 4. Viagens realizadas
   viagem AS (
   SELECT
     data,
@@ -23,7 +65,7 @@ WITH
   WHERE
     data BETWEEN DATE("{{ var("start_date") }}")
     AND DATE( "{{ var("end_date") }}" ) ),
--- 2. Status dos veículos
+  -- 5. Status dos veículos
   veiculos AS (
   SELECT
     data,
@@ -35,82 +77,141 @@ WITH
   WHERE
     data BETWEEN DATE("{{ var("start_date") }}")
     AND DATE("{{ var("end_date") }}") ),
--- 3. Viagem com tolerância de 30 minutos, limitada pela viagem anterior
-viagem_com_tolerancia AS (
-  SELECT
-    v.*,
-    LAG(v.datetime_chegada) OVER (PARTITION BY v.id_veiculo ORDER BY v.datetime_partida) AS viagem_anterior_chegada,
-    CASE
-      WHEN LAG(v.datetime_chegada) OVER (PARTITION BY v.id_veiculo ORDER BY v.datetime_partida) IS NULL THEN
-        DATETIME(TIMESTAMP_SUB(datetime_partida, INTERVAL 30 MINUTE))
-      ELSE
-        DATETIME(TIMESTAMP_ADD(GREATEST(
-          TIMESTAMP_SUB(datetime_partida, INTERVAL 30 MINUTE),
-          LAG(v.datetime_chegada) OVER (PARTITION BY v.id_veiculo ORDER BY v.datetime_partida)
-        ), INTERVAL 1 SECOND))
-    END AS datetime_partida_com_tolerancia
-  FROM
-    viagem AS v
-),
--- 4. Contagem de transações
-transacao_contagem AS (
-  SELECT
-    v.data,
-    v.id_viagem,
-    COUNT(t.datetime_transacao) AS quantidade_transacao
-  FROM
-    {{ ref("transacao") }} AS t
-    -- rj-smtr.br_rj_riodejaneiro_bilhetagem.transacao AS t
-  JOIN
-    viagem_com_tolerancia AS v
-  ON
-    t.id_veiculo = SUBSTR(v.id_veiculo, 2)
-    AND t.data = v.data
-    AND t.datetime_transacao BETWEEN v.datetime_partida_com_tolerancia AND v.datetime_chegada
-  GROUP BY
-    v.data, v.id_viagem
-),
--- 5. Contagem de transações Riocard
-transacao_riocard_contagem AS (
-  SELECT
-    v.data,
-    v.id_viagem,
-    COUNT(tr.datetime_transacao) AS quantidade_transacao_riocard
-  FROM
-    {{ ref("transacao_riocard") }} AS tr
-    -- rj-smtr.br_rj_riodejaneiro_bilhetagem.transacao_riocard AS tr
-  JOIN
-    viagem_com_tolerancia AS v
-  ON
-    tr.id_veiculo = SUBSTR(v.id_veiculo, 2)
-    AND tr.data = v.data
-    AND tr.datetime_transacao BETWEEN v.datetime_partida_com_tolerancia AND v.datetime_chegada
-  GROUP BY
-    v.data, v.id_viagem
-),
--- 6. Verificação de estado do equipamento
-estado_equipamento_verificacao AS (
-  SELECT
-    v.data,
-    v.id_viagem,
-    IF(COUNT(CASE WHEN g.estado_equipamento != "ABERTO" THEN 1 END) = 0, TRUE, FALSE) AS indicador_estado_equipamento_aberto
-  FROM
-    {{ ref("gps_validador") }} AS g
-    -- rj-smtr.br_rj_riodejaneiro_bilhetagem.gps_validador AS g
-  JOIN
-    viagem AS v
-  ON
-    g.id_veiculo = SUBSTR(v.id_veiculo, 2)
-    AND g.data = v.data
-    AND g.datetime_gps BETWEEN v.datetime_partida AND v.datetime_chegada
-  GROUP BY
-    v.data, v.id_viagem
-)
+  -- 6. Viagem, para fins de contagem de passageiros, com tolerância de 30 minutos, limitada pela viagem anterior
+  viagem_com_tolerancia AS (
+    SELECT
+      v.*,
+      LAG(v.datetime_chegada) OVER (PARTITION BY v.id_veiculo ORDER BY v.datetime_partida) AS viagem_anterior_chegada,
+      CASE
+        WHEN LAG(v.datetime_chegada) OVER (PARTITION BY v.id_veiculo ORDER BY v.datetime_partida) IS NULL THEN
+          DATETIME(TIMESTAMP_SUB(datetime_partida, INTERVAL 30 MINUTE))
+        ELSE
+          DATETIME(TIMESTAMP_ADD(GREATEST(
+            TIMESTAMP_SUB(datetime_partida, INTERVAL 30 MINUTE),
+            LAG(v.datetime_chegada) OVER (PARTITION BY v.id_veiculo ORDER BY v.datetime_partida)
+          ), INTERVAL 1 SECOND))
+      END AS datetime_partida_com_tolerancia
+    FROM
+      viagem AS v
+  ),
+  -- 7. Contagem de transações Jaé
+  transacao_contagem AS (
+    SELECT
+      v.data,
+      v.id_viagem,
+      COUNT(t.datetime_transacao) AS quantidade_transacao
+    FROM
+      transacao AS t
+    JOIN
+      viagem_com_tolerancia AS v
+    ON
+      t.id_veiculo = SUBSTR(v.id_veiculo, 2)
+      AND t.datetime_transacao BETWEEN v.datetime_partida_com_tolerancia AND v.datetime_chegada
+    GROUP BY
+      v.data, v.id_viagem
+  ),
+  -- 5. Contagem de transações RioCard
+  transacao_riocard_contagem AS (
+    SELECT
+      v.data,
+      v.id_viagem,
+      COUNT(tr.datetime_transacao) AS quantidade_transacao_riocard
+    FROM
+      transacao_riocard AS tr
+    JOIN
+      viagem_com_tolerancia AS v
+    ON
+      tr.id_veiculo = SUBSTR(v.id_veiculo, 2)
+      AND tr.datetime_transacao BETWEEN v.datetime_partida_com_tolerancia AND v.datetime_chegada
+    GROUP BY
+      v.data, v.id_viagem
+  ),
+  -- 6. Ajusta estado do equipamento
+  -- Agrupa mesma posição para mesmo validador e veículo, mantendo preferencialmente o estado do equipamento "ABERTO"
+  estado_equipamento_aux AS (
+    SELECT
+      data,
+      id_validador,
+      id_veiculo,
+      latitude,
+      longitude,
+      IF(COUNT(CASE WHEN estado_equipamento = "ABERTO" THEN 1 END) >= 1, "ABERTO", "FECHADO") AS estado_equipamento,
+      MIN(datetime_gps) AS datetime_gps,
+    FROM
+      gps_validador
+    GROUP BY
+      1,
+      2,
+      3,
+      4,
+      5
+  ),
+  -- 7. Relacionamento entre estado do equipamento e viagem
+  gps_validador_viagem AS (
+    SELECT
+      v.data,
+      e.datetime_gps,
+      v.id_viagem,
+      e.id_validador,
+      e.estado_equipamento,
+      e.latitude,
+      e.longitude
+    FROM
+      estado_equipamento_aux AS e
+    JOIN
+      viagem AS v
+    ON
+      e.id_veiculo = SUBSTR(v.id_veiculo, 2)
+      AND e.datetime_gps BETWEEN v.datetime_partida AND v.datetime_chegada
+  ),
+  -- 8. Calcula a porcentagem de estado do equipamento "ABERTO" por validador e viagem
+  estado_equipamento_perc AS (
+    SELECT
+      data,
+      id_viagem,
+      id_validador,
+      COUNTIF(estado_equipamento = "ABERTO") / COUNT(*) AS percentual_estado_equipamento_aberto
+    FROM
+      gps_validador_viagem
+    GROUP BY
+      1,
+      2,
+      3
+  ),
+  -- 9. Considera o validador com maior porcentagem de estado do equipamento "ABERTO" por viagem
+  estado_equipamento_max_perc AS (
+    SELECT
+      data,
+      id_viagem,
+      MAX_BY(id_validador, percentual_estado_equipamento_aberto) AS id_validador,
+      MAX(percentual_estado_equipamento_aberto) AS percentual_estado_equipamento_aberto
+    FROM
+      estado_equipamento_perc
+    GROUP BY
+      1,
+      2
+  ),
+  -- 10. Verifica se a viagem possui estado do equipamento "ABERTO" em pelo menos 80% dos registros
+  estado_equipamento_verificacao AS (
+    SELECT
+      data,
+      id_viagem,
+      id_validador,
+      percentual_estado_equipamento_aberto,
+      IF(percentual_estado_equipamento_aberto >= 0.8 OR percentual_estado_equipamento_aberto IS NULL, TRUE, FALSE) AS indicador_estado_equipamento_aberto
+    FROM
+      viagem
+    LEFT JOIN
+      estado_equipamento_max_perc
+    USING
+      (data, id_viagem)
+  )
 SELECT
   v.data,
   v.id_viagem,
   v.id_veiculo,
   v.servico,
+  eev.id_validador,
   CASE
     WHEN v.data >= DATE("{{ var("DATA_SUBSIDIO_V8_INICIO") }}")
       AND (COALESCE(tr.quantidade_transacao_riocard, 0) = 0
@@ -122,7 +223,8 @@ SELECT
   v.distancia_planejada,
   COALESCE(t.quantidade_transacao, 0) AS quantidade_transacao,
   COALESCE(tr.quantidade_transacao_riocard, 0) AS quantidade_transacao_riocard,
-  COALESCE(eev.indicador_estado_equipamento_aberto, FALSE) AS indicador_estado_equipamento_aberto,
+  eev.percentual_estado_equipamento_aberto,
+  eev.indicador_estado_equipamento_aberto,
   v.datetime_partida_com_tolerancia AS datetime_partida_bilhetagem,
   v.datetime_partida,
   v.datetime_chegada,
