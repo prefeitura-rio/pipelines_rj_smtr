9eac0d9779c904f1ab687ae39278e563b61984b4
Rafael <rafael.c.pinheiro96@gmail.com>
2024-09-13 09:38:44 -0300

    correcoes prod

--- queries/models/planejamento/shapes_geom_planejamento.sql
+++ queries/models/planejamento/shapes_geom_planejamento.sql
@@ -10,6 +10,11 @@
   )
 }}
 
+-- depends_on: {{ ref('feed_info_gtfs') }}
+{% if execute and is_incremental() %}
+  {% set last_feed_version = get_last_feed_start_date(var("data_versao_gtfs")) %}
+{% endif %}
+
 WITH shapes AS (
   SELECT
     feed_version,
@@ -20,11 +25,12 @@ WITH shapes AS (
     ST_GEOGPOINT(shape_pt_lon, shape_pt_lat) AS ponto_shape,
     CONCAT(shape_pt_lon, " ", shape_pt_lat) AS lon_lat,
   FROM
-    -- {{ ref("shapes_gtfs") }}
-    rj-smtr.gtfs.shapes
-  WHERE
-    feed_start_date = '2024-09-01'
-    AND shape_id = "hj1m"
+    {{ ref("shapes_gtfs") }} s
+    -- rj-smtr.gtfs.shapes s
+  {% if is_incremental() %}
+    WHERE
+      feed_start_date IN ('{{ last_feed_version }}', '{{ var("data_versao_gtfs") }}')
+  {% endif %}
 )
 SELECT
   feed_version,
@@ -33,6 +39,7 @@ SELECT
   shape_id,
   ST_MAKELINE(ARRAY_AGG(ponto_shape ORDER BY shape_pt_sequence)) AS shape,
   CONCAT("LINESTRING(", STRING_AGG(lon_lat, ", " ORDER BY shape_pt_sequence), ")") AS wkt_shape,
+  '{{ var("version") }}' as versao
 FROM
   shapes
 GROUP BY
