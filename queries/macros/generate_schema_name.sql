a21236890a7be804b7bf02e3a3535637c62296ff
Rafael <rafael.c.pinheiro96@gmail.com>
2024-09-19 11:03:54 -0300

    ambiente dev/hmg

--- queries/macros/generate_schema_name.sql
+++ queries/macros/generate_schema_name.sql
@@ -1,14 +1,20 @@
+-- fmt: off
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
     {%- if custom_schema_name is none -%}
 
-        {{ default_schema }}
+        {% set schema_name = default_schema %}
 
     {%- else -%}
 
-        {{ custom_schema_name | trim }}
+        {% set schema_name = custom_schema_name | trim %}
 
     {%- endif -%}
 
+    {% if target.name == "dev" %}
+        {% set schema_name = env_var("DBT_USER") + "__" + schema_name %}
+    {% endif %}
+
+    {{ schema_name }}
 {%- endmacro %}
