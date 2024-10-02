32d33b54c0b7ac5ba52da06e1099136728b7289c
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-10-02 19:13:55 -0300

    altera where

--- pipelines/serpro/constants.py
+++ pipelines/serpro/constants.py
@@ -25,7 +25,7 @@ class constants(Enum):  # pylint: disable=c0103
             FROM
                 dbpro_radar_view_SMTR_VBL.tb_infracao_view
             WHERE
-                CAST(auinf_dt_infracao AS DATE) = CAST('{date}' AS DATE)
+                auinf_dt_infracao= '{date}'
         """,
         "primary_key": ["auinf_num_auto"],
     }
