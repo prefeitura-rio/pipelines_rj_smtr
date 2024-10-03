fce39c3dea243f48a9a5419c940fd8b4a9ab2002
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-10-03 16:42:32 -0300

    comenta bucket_name

--- pipelines/serpro/flows.py
+++ pipelines/serpro/flows.py
@@ -83,7 +83,7 @@ with Flow("SMTR: SERPRO - Captura/Tratamento") as serpro_captura:
         raw_filepath=raw_filepaths,
         partitions=partitions,
         error=unmapped(None),
-        bucket_name=unmapped(constants.INFRACAO_PRIVATE_BUCKET.value),
+        # bucket_name=unmapped(constants.INFRACAO_PRIVATE_BUCKET.value),
     )
 
     wait_captura_true = upload_staging_data_to_gcs.map(
@@ -93,7 +93,7 @@ with Flow("SMTR: SERPRO - Captura/Tratamento") as serpro_captura:
         partitions=partitions,
         timestamp=timestamps,
         error=errors,
-        bucket_name=unmapped(constants.INFRACAO_PRIVATE_BUCKET.value),
+        # bucket_name=unmapped(constants.INFRACAO_PRIVATE_BUCKET.value),
     )
 
     wait_run_dbt_model = run_dbt_model(
