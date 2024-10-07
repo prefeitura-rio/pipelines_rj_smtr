3533707558d5465e1f52bceef7c4ffba76076287
vtr363 <victor.m.rocha.vr@gmail.com>
2024-10-07 14:52:37 -0300

    ajusta tipo_os das datas 13/09/24, 14/09/24, 15/09/24, 19/09/24, 20/09/24, 21/09/24 e 22/09/24 para Rock in Rio

--- queries/models/projeto_subsidio_sppo/subsidio_data_versao_efetiva.sql
+++ queries/models/projeto_subsidio_sppo/subsidio_data_versao_efetiva.sql
@@ -347,8 +347,13 @@ WITH
       WHEN data = DATE(2024,05,04) THEN "Madonna 2024-05-04"
       WHEN data = DATE(2024,05,05) THEN "Madonna 2024-05-05"
       WHEN data = DATE(2024,08,18) THEN "CNU" -- Processo.Rio MTR-PRO-2024/13252
-      WHEN data = DATE(2024,09,14) THEN "Extraordinária - Verão" -- Processo.Rio
-      WHEN data = DATE(2024,09,15) THEN "Extraordinária - Verão" -- Processo.Rio
+      WHEN data = DATE(2024,09,13) THEN "Rock in Rio"
+      WHEN data = DATE(2024,09,14) THEN "Rock in Rio"
+      WHEN data = DATE(2024,09,15) THEN "Rock in Rio"
+      WHEN data = DATE(2024,09,19) THEN "Rock in Rio" 
+      WHEN data = DATE(2024,09,20) THEN "Rock in Rio" 
+      WHEN data = DATE(2024,09,21) THEN "Rock in Rio" 
+      WHEN data = DATE(2024,09,22) THEN "Rock in Rio" 
       ELSE "Regular"
     END AS tipo_os,
   FROM UNNEST(GENERATE_DATE_ARRAY("{{var('DATA_SUBSIDIO_V6_INICIO')}}", "2024-12-31")) AS data),
