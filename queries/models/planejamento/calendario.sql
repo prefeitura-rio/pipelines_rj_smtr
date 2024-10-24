1c31eadec197033c96b8511a23663460a70cf0af
Rafael <rafael.c.pinheiro96@gmail.com>
2024-10-24 18:23:46 -0300

    cria tabela calendario

--- queries/models/planejamento/calendario.sql
+++ queries/models/planejamento/calendario.sql
@@ -1,24 +1,36 @@
 {{
     config(
         partition_by={
-            "field": "date",
+            "field": "data",
             "data_type": "date",
             "granularity": "day",
         },
+        materialized="incremental",
+        incremental_strategy="insert_overwrite",
     )
 }}
 
 {# {% set gtfs_feed_info = ref("feed_info_gtfs") %} #}
 {% set gtfs_feed_info = "rj-smtr.gtfs.feed_info" %}
+{% set calendario_manual = ref("aux_calendario_manual") %}
 
 {% if execute %}
     {% if is_incremental() %}
         {% set gtfs_feeds_query %}
-            select concat("'", feed_start_date, "'")
+            select concat("'", feed_start_date, "'") as feed_start_date
             from {{ gtfs_feed_info }}
             where
                 feed_start_date <= date("{{ var('date_range_end') }}")
                 and (feed_end_date IS NULL OR feed_end_date >= date("{{ var('date_range_end') }}"))
+
+            union distinct
+
+            select distinct concat("'", feed_start_date, "'") as feed_start_date
+            from {{ calendario_manual }}
+            where
+                data between date("{{ var('date_range_start') }}")
+                and date("{{ var('date_range_end') }}")
+                and feed_start_date is not null
         {% endset %}
 
         {% set gtfs_feeds = run_query(gtfs_feeds_query).columns[0].values() %}
@@ -29,53 +41,79 @@ with
     calendar as (
         select *
         from `rj-smtr.gtfs.calendar`
-        where feed_start_date in ({{ gtfs_feeds | join(", ") }})
+        {% if is_incremental() %}
+            where feed_start_date in ({{ gtfs_feeds | join(", ") }})
+        {% endif %}
+    ),
+    datas as (
+        select data, extract(dayofweek from data) as dia_semana, feed_start_date
+        from
+            {{ gtfs_feed_info }},
+            unnest(
+                generate_date_array(
+                    {% if is_incremental() %}
+                        date("{{ var('date_range_start') }}"),
+                        date("{{ var('date_range_end') }}")
+                    {% else %}date("2024-09-01"), current_date("America/Sao_Paulo")
+                    {% endif %}
+                )
+            ) as data
+        where
+            data >= feed_start_date and (data <= feed_end_date or feed_end_date is null)
+    ),
+    modificacao_manual as (
+        select
+            d.data,
+            d.dia_semana,
+            coalesce(m.feed_start_date, d.feed_start_date) as feed_start_date,
+            m.tipo_dia,
+            ifnull(m.tipo_os, "Regular") as tipo_os
+        from datas d
+        left join {{ calendario_manual }} m using (data)
     ),
     calendar_dates as (
         select
-            date as data,
+            cd.date as data,
+            m.tipo_dia,
+            m.tipo_os,
             case
-                when exception_type = '2'
+                when cd.exception_type = '2'
                 then
                     regexp_replace(
-                        service_id,
+                        cd.service_id,
                         "^[U|S|D]_",
                         case
-                            when extract(dayofweek from date) = 7
+                            when extract(dayofweek from cd.date) = 7
                             then "S_"
-                            when extract(dayofweek from date) = 1
+                            when extract(dayofweek from cd.date) = 1
                             then "D_"
                             else "U_"
                         end
                     )
-                else service_id
+                else cd.service_id
             end as service_id,
-            exception_type,
-            feed_start_date,
-        from `rj-smtr.gtfs.calendar_dates`
+            cd.exception_type,
+            cd.feed_start_date,
+        from `rj-smtr.gtfs.calendar_dates` cd
+        join
+            modificacao_manual m
+            on cd.date = m.data
+            and cd.feed_start_date = m.feed_start_date
         where
-            feed_start_date in ({{ gtfs_feeds | join(", ") }})
-            and date between date("{{ var('date_range_start') }}") and date(
-                "{{ var('date_range_end') }}"
-            )
-    ),
-    datas as (
-        select distinct data, extract(dayofweek from data) as dia_semana,
-        from
-            unnest(
-                generate_date_array(
-                    date("{{ var('date_range_start') }}"),
-                    date("{{ var('date_range_end') }}")
+            {% if is_incremental() %}
+                feed_start_date in ({{ gtfs_feeds | join(", ") }})
+                and date between date("{{ var('date_range_start') }}") and date(
+                    "{{ var('date_range_end') }}"
                 )
-            ) as data
+            {% else %} date <= current_date("America/Sao_Paulo")
+            {% endif %}
     ),
-    datas_service as (
-        select d.data, c.service_id, c.feed_version, c.feed_start_date
-        from datas d
+    datas_service_id as (
+        select d.data, d.tipo_dia, d.tipo_os, c.service_id, d.feed_start_date
+        from modificacao_manual d
         join
             calendar c
-            on d.data >= c.feed_start_date
-            and (d.data <= feed_end_date or feed_end_date is null)
+            on d.feed_start_date = c.feed_start_date
             and (
                 (d.dia_semana = 1 and c.sunday = '1')
                 or (d.dia_semana = 2 and c.monday = '1')
@@ -87,8 +125,8 @@ with
             )
     ),
     service_ids_retirados as (
-        select d.*, c.service_id as c_service_id,
-        from datas_calendar d
+        select d.*
+        from datas_service_id d
         left join
             (select * from calendar_dates where exception_type = '2') c using (
                 data, service_id
@@ -97,49 +135,54 @@ with
 
     ),
     service_ids_adicionados as (
-        select data, service_id, dia_semana, feed_start_date
+        select data, tipo_dia, tipo_os, service_id, feed_start_date
         from service_ids_retirados
 
         union distinct
 
-        select data, service_id, dia_semana, feed_start_date
+        select data, tipo_dia, tipo_os, service_id, feed_start_date
         from calendar_dates
         where exception_type = '1'
     ),
-    service_id_agg as (
-        select data, feed_start_date, array_agg(service_id) as service_ids
-        from service_ids_adicionados
-        group by 1, 2, 3
-    ),
-    calendario_gtfs as (
+    service_id_corrigido as (
         select
             data,
+            tipo_dia,
+            tipo_os,
             case
-                when "D_REG" in unnest(service_ids)
-                then "Domingo"
-                when "S_REG" in unnest(service_ids)
-                then "Sábado"
-                when "U_REG" in unnest(service_ids)
-                then "Dia Útil"
-            end as tipo_dia,
-            "Regular" as tipo_os,
-            service_ids,
+                when tipo_dia = "Domingo"
+                then regexp_replace(service_id, "^[U|S]_", "D_")
+                when tipo_dia = "Sábado"
+                then regexp_replace(service_id, "^[U|D]_", "S_")
+                when tipo_dia = "Dia Útil"
+                then regexp_replace(service_id, "^[S|D]_", "U_")
+                else service_id
+            end as service_id,
             feed_start_date
-        from service_id_agg
+        from service_ids_adicionados
     ),
-    modificacoes_manuais as (
+    service_id_agg as (
         select
-            c.data,
-            coalesce(m.tipo_dia, c.tipo_dia) as tipo_dia,
-            coalesce(m.tipo_os, c.tipo_os) as tipo_os,
-            service_ids,
-            coalesce(m.feed_start_date, c.feed_start_date) as feed_start_date
-        from calendario_gtfs c
-        left join {{ ref("aux_calendario_manual") }} m using (data)
+            data,
+            tipo_dia,
+            tipo_os,
+            feed_start_date,
+            array_agg(service_id) as service_ids
+        from service_id_corrigido
+        group by 1, 2, 3, 4
     )
 select
     data,
-    tipo_dia,
+    case
+        when c.tipo_dia is not null
+        then c.tipo_dia
+        when "D_REG" in unnest(c.service_ids)
+        then "Domingo"
+        when "S_REG" in unnest(c.service_ids)
+        then "Sábado"
+        when "U_REG" in unnest(c.service_ids)
+        then "Dia Útil"
+    end as tipo_dia,
     case
         when c.tipo_os = "Extraordinária - Verão"
         then "Verão"
@@ -151,9 +194,9 @@ select
     end as subtipo_dia,
     c.tipo_os,
     c.service_ids,
-    i.feed_version
-    feed_start_date,
+    i.feed_version,
+    c.feed_start_date,
     '{{ var("version") }}' as versao,
     current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao
-from modificacoes_manuais c
-join `rj-smtr.gtfs.feed_info` i using (feed_start_date)
+from service_id_agg c
+join {{ gtfs_feed_info }} i using (feed_start_date)
--- queries/models/planejamento/staging/aux_calendario_manual.sql
+++ queries/models/planejamento/staging/aux_calendario_manual.sql
@@ -1,21 +1,40 @@
-{{ config(materialized="ephemeral") }}
+{{
+    config(
+        partition_by={
+            "field": "data",
+            "data_type": "date",
+            "granularity": "day",
+        },
+        materialized="incremental",
+        incremental_strategy="insert_overwrite",
+    )
+}}
 
-select
-    data,
-    date(null) as feed_start_date,
-    cast(null as string) as tipo_dia,
-    case
-        when data between date(2024, 09, 14) and date(2024, 09, 15)
-        then "Verão + Rock in Rio"
-        when data between date(2024, 09, 19) and date(2024, 09, 22)
-        then "Rock in Rio"
-        when data = date(2024, 10, 06)
-        then "Eleição"
-    end as tipo_os
-from
-    unnest(
-        generate_date_array(
-            date({{ var("data_versao_gtfs") }}),
-            date_add(date({{ var("data_versao_gtfs") }}), interval 1 year)
-        )
-    ) as data
+with
+    datas as (
+        select
+            data,
+            date(null) as feed_start_date,
+            cast(null as string) as tipo_dia,
+            case
+                when data between date(2024, 09, 14) and date(2024, 09, 15)
+                then "Verão + Rock in Rio"
+                when data between date(2024, 09, 19) and date(2024, 09, 22)
+                then "Rock in Rio"
+                when data = date(2024, 10, 06)
+                then "Eleição"
+            end as tipo_os
+        from
+            unnest(
+                generate_date_array(
+                    {% if is_incremental() %}
+                        date("{{ var('date_range_start') }}"),
+                        date("{{ var('date_range_end') }}")
+                    {% else %}date("2024-09-01"), current_date("America/Sao_Paulo")
+                    {% endif %}
+                )
+            ) as data
+    )
+select *
+from datas
+where feed_start_date is not null or tipo_dia is not null or tipo_os is not null
