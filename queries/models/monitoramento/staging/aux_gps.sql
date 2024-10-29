57f741d0bf79c14f7e2a3f0be43415073ef8f975
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-10-29 20:28:43 -0300

    Commit inicial

---
+++ queries/models/monitoramento/staging/aux_gps.sql
@@ -0,0 +1,17 @@
+{{ config(alias=this.name ~ var('fonte_gps')) }}
+
+SELECT
+    SAFE_CAST(ordem AS STRING) ordem,
+    SAFE_CAST(REPLACE(latitude, ',', '.') AS FLOAT64) latitude,
+    SAFE_CAST(REPLACE(longitude, ',', '.') AS FLOAT64) longitude,
+    SAFE_CAST(DATETIME(TIMESTAMP(datahora), "America/Sao_Paulo") AS DATETIME) timestamp_gps,
+    SAFE_CAST(velocidade AS INT64) velocidade,
+    concat(
+        ifnull(REGEXP_EXTRACT(linha, r'[A-Z]+'), ""),
+        ifnull(REGEXP_EXTRACT(linha, r'[0-9]+'), "")
+    ) as linha,
+    SAFE_CAST(DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS DATETIME) timestamp_captura,
+    SAFE_CAST(data AS DATE) data,
+    SAFE_CAST(hora AS INT64) hora
+from
+    {{var('sppo_registros_staging')}} as t
---
+++ queries/models/monitoramento/staging/aux_gps_realocacao.sql
@@ -0,0 +1,76 @@
+{% if var("fifteen_minutes") == "_15_minutos" %}
+{{
+  config(
+      materialized='ephemeral',
+  )
+}}
+{% else %}
+{{
+  config(
+      materialized='incremental',
+      partition_by={
+            "field":"data",
+            "data_type": "date",
+            "granularity":"day"
+      },
+      alias=this.name ~ var('fonte_gps')
+  )
+}}
+{% endif %}
+
+-- 1. Filtra realocações válidas dentro do intervalo de GPS avaliado
+with realocacao as (
+  select
+    * except(datetime_saida),
+    case
+      when datetime_saida is null then datetime_operacao
+      else datetime_saida
+    end as datetime_saida,
+  from
+    {{ ref('aux_realocacao'~ var('fonte_gps')) }}
+  where
+    -- Realocação deve acontecer após o registro de GPS e até 1 hora depois
+    datetime_diff(datetime_operacao, datetime_entrada, minute) between 0 and 60
+    and data between DATE("{{var('date_range_start')}}")
+    and DATE(datetime_add("{{var('date_range_end')}}", interval 1 hour))
+    and (datetime_saida >= datetime("{{var('date_range_start')}}") or datetime_operacao >= datetime("{{var('date_range_start')}}"))
+),
+-- 2. Altera registros de GPS com servicos realocados
+gps as (
+  select
+    ordem,
+    timestamp_gps,
+    linha,
+    data,
+    hora
+  from {{ ref('aux_gps' ~ var('fonte_gps') }}
+  where
+    data between DATE("{{var('date_range_start')}}") and DATE("{{var('date_range_end')}}")
+    and timestamp_gps > "{{var('date_range_start')}}" and timestamp_gps <= "{{var('date_range_end')}}"
+),
+combinacao as (
+  select
+    r.id_veiculo,
+    g.timestamp_gps,
+    g.linha as servico_gps,
+    r.servico as servico_realocado,
+    r.datetime_operacao as datetime_realocacao,
+    g.data,
+    g.hora
+  from gps g
+  inner join realocacao r
+  on
+    g.ordem = r.id_veiculo
+    and g.linha != r.servico
+    and g.timestamp_gps between r.datetime_entrada and r.datetime_saida
+)
+-- Filtra realocacao mais recente para cada timestamp
+select
+  * except(rn)
+from (
+  select
+    *,
+    row_number() over (partition by id_veiculo, timestamp_gps order by datetime_realocacao desc) as rn
+  from combinacao
+)
+where rn = 1
