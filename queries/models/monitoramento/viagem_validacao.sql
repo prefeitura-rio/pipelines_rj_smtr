6da7e1bc27a686895cf83f691cbf05938eeb6324
Rafael <rafael.c.pinheiro96@gmail.com>
2024-11-08 09:27:13 -0300

    adiciona indicador de viagem nao planejada

--- queries/models/monitoramento/viagem_validacao.sql
+++ queries/models/monitoramento/viagem_validacao.sql
@@ -8,6 +8,13 @@
     )
 }}
 
+{% set incremental_filter %}
+    data between
+        date('{{ var("date_range_start") }}')
+        and date('{{ var("date_range_end") }}')
+{% endset %}
+
+
 {% set parametro_validacao = 0.9 %}
 
 with
@@ -26,16 +33,13 @@ with
             sentido,
             count(*) as quantidade_segmentos_verificados,
             countif(quantidade_gps > 0) as quantidade_segmentos_validos,
+            tipo_dia,
             feed_version,
             feed_start_date
         from {{ ref("gps_segmento_viagem") }}
         where
             not indicador_segmento_desconsiderado
-            {% if is_incremental() %}
-                and data between date('{{ var("date_range_start") }}') and date(
-                    '{{ var("date_range_end") }}'
-                )
-            {% endif %}
+            {% if is_incremental() %} {{ incremental_filter }} {% endif %}
         group by
             data,
             id_viagem,
@@ -48,6 +52,7 @@ with
             shape_id,
             servico,
             sentido,
+            tipo_dia,
             feed_version,
             feed_start_date
     ),
@@ -68,9 +73,35 @@ with
             quantidade_segmentos_validos,
             quantidade_segmentos_validos
             / quantidade_segmentos_verificados as indice_validacao,
+            tipo_dia,
             feed_version,
             feed_start_date
         from contagem
+    ),
+    trips as (
+        select distinct
+            feed_start_date,
+            feed_version,
+            route_id,
+            case
+                when service_id like "D_%"
+                then "Domingo"
+                when service_id like "S_%"
+                then "Sabado"
+                when service_id like "U_%"
+                then "Dia Útil"
+            end as tipo_dia
+        from {{ ref("trips_gtfs") }}
+        where
+            (service_id like "D_%" or service_id like "S_%" or service_id like "U_%")
+            {% if is_incremental() %}
+                and feed_start_date in ({{ gtfs_feeds | join(", ") }})
+            {% endif %}
+    ),
+    servicos_planejados as (
+        select i.*, t.tipo_dia is not null as indicador_servico_planejado
+        from indice i
+        left join trips t using (feed_start_date, feed_version, route_id, tipo_dia)
     )
 select
     data,
@@ -87,10 +118,14 @@ select
     quantidade_segmentos_verificados,
     quantidade_segmentos_validos,
     indice_validacao,
-    indice_validacao >= {{ parametro_validacao }} as indicador_viagem_valida,
+    indice_validacao >= {{ parametro_validacao }} as indicador_trajeto_valido,
+    indicador_servico_planejado,
+    indice_validacao >= {{ parametro_validacao }}
+    and indicador_servico_planejado as indicador_viagem_valida,
     {{ parametro_validacao }} as parametro_validacao,
+    tipo_dia,
     feed_version,
     feed_start_date,
     '{{ var("version") }}' as versao,
     current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao
-from indice
+from servicos_planejados
