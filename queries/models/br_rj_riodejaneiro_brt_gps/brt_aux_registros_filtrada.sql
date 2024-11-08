368799eeb8bab98a010db1f3847b9f0facdbff3b
hellcassius <caiorogerio.santos@gmail.com>
2024-11-08 15:42:24 -0300

    test sqlfmt

--- queries/models/br_rj_riodejaneiro_brt_gps/brt_aux_registros_filtrada.sql
+++ queries/models/br_rj_riodejaneiro_brt_gps/brt_aux_registros_filtrada.sql
@@ -1,12 +1,8 @@
 {{
-  config(
-      materialized='incremental',
-      partition_by={
-            "field":"data",
-            "data_type": "date",
-            "granularity":"day"
-      }
-  )
+    config(
+        materialized="incremental",
+        partition_by={"field": "data", "data_type": "date", "granularity": "day"},
+    )
 }}
 /*
 - Descrição:
@@ -16,56 +12,52 @@ Filtragem e tratamento básico de registros de gps.
 2. Filtra registros que estão fora de uma caixa que contém a área do
    município de Rio de Janeiro.
 */
-WITH
-box AS (
-  /*1. Geometria de caixa que contém a área do município de Rio de Janeiro.*/
-	SELECT
-	*
-	FROM
-	{{ var('limites_caixa') }}),
-gps AS (
-  /* 1. Filtra registros antigos. Remove registros que tem diferença maior
-   que 1 minuto entre o timestamp_captura e timestamp_gps.*/
-  SELECT
-    *,
-    ST_GEOGPOINT(longitude, latitude) posicao_veiculo_geo
-  FROM
-    {{ ref('brt_registros_desaninhada') }}
-  {% if is_incremental() -%}
-  WHERE
-    data between DATE("{{var('date_range_start')}}") and DATE("{{var('date_range_end')}}")
-    AND timestamp_gps > "{{var('date_range_start')}}" and timestamp_gps <= "{{var('date_range_end')}}"
-    AND DATETIME_DIFF(timestamp_captura, timestamp_gps, MINUTE) BETWEEN 0 AND 1
-  {%- endif %}
+with
+    box as (
+        /* 1. Geometria de caixa que contém a área do município de Rio de Janeiro.*/
+        select * from {{ var("limites_caixa") }}
     ),
-filtrada AS (
-  /* 2. Filtra registros que estão fora de uma caixa que contém a área do
+    gps as (
+        /* 1. Filtra registros antigos. Remove registros que tem diferença maior
+   que 1 minuto entre o timestamp_captura e timestamp_gps.*/
+        select *, st_geogpoint(longitude, latitude) posicao_veiculo_geo
+        from {{ ref("brt_registros_desaninhada") }}
+        {% if is_incremental() -%}
+            where
+                data between date("{{var('date_range_start')}}") and date(
+                    "{{var('date_range_end')}}"
+                )
+                and timestamp_gps > "{{var('date_range_start')}}"
+                and timestamp_gps <= "{{var('date_range_end')}}"
+                and datetime_diff(timestamp_captura, timestamp_gps, minute)
+                between 0 and 1
+        {%- endif %}
+    ),
+    filtrada as (
+        /* 2. Filtra registros que estão fora de uma caixa que contém a área do
    município de Rio de Janeiro.*/
-  SELECT
-    id_veiculo,
-    latitude,
-    longitude,
-    posicao_veiculo_geo,
-    velocidade,
-    servico,
-    timestamp_gps,
-    timestamp_captura,
-    data,
-    hora,
-    row_number() over (partition by id_veiculo, timestamp_gps, servico) rn
-  FROM
-    gps
-  WHERE
-    ST_INTERSECTSBOX(posicao_veiculo_geo,
-      ( SELECT min_longitude FROM box),
-      ( SELECT min_latitude FROM box),
-      ( SELECT max_longitude FROM box),
-      ( SELECT max_latitude FROM box))
-  )
-SELECT
-  * except(rn),
-  "{{ var("version") }}" as versao
-FROM
-  filtrada
-WHERE
-  rn = 1
+        select
+            id_veiculo,
+            latitude,
+            longitude,
+            posicao_veiculo_geo,
+            velocidade,
+            servico,
+            timestamp_gps,
+            timestamp_captura,
+            data,
+            hora,
+            row_number() over (partition by id_veiculo, timestamp_gps, servico) rn
+        from gps
+        where
+            st_intersectsbox(
+                posicao_veiculo_geo,
+                (select min_longitude from box),
+                (select min_latitude from box),
+                (select max_longitude from box),
+                (select max_latitude from box)
+            )
+    )
+select * except (rn), "{{ var(" version ") }}" as versao
+from filtrada
+where rn = 1
