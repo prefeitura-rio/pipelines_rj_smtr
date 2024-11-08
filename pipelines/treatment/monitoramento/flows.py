776b4281fd3c29b848cd9f4c7c27b1fb2b556c25
Rafael <rafael.c.pinheiro96@gmail.com>
2024-11-08 09:28:24 -0300

    cria pipeline validacao viagens

--- pipelines/treatment/monitoramento/flows.py
+++ pipelines/treatment/monitoramento/flows.py
@@ -9,6 +9,10 @@ from pipelines.capture.rioonibus.constants import (
     constants as rioonibus_source_constants,
 )
 from pipelines.constants import constants as smtr_constants
+from pipelines.migration.br_rj_riodejaneiro_onibus_gps_zirix.constants import (
+    constants as gps_zirix_constants,
+)
+from pipelines.schedules import cron_every_hour_minute_6
 from pipelines.treatment.monitoramento.constants import constants
 from pipelines.treatment.templates.flows import create_default_materialization_flow
 
@@ -17,4 +21,29 @@ VIAGEM_INFORMADA_MATERIALIZACAO = create_default_materialization_flow(
     selector=constants.VIAGEM_INFORMADA_SELECTOR.value,
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     wait=[rioonibus_source_constants.VIAGEM_INFORMADA_SOURCE.value],
+)
+
+VIAGEM_VALIDACAO_MATERIALIZACAO = create_default_materialization_flow(
+    flow_name="viagem_validacao - materializacao",
+    selector=constants.VIAGEM_VALIDACAO_SELECTOR.value,
+    agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
+    wait=[
+        VIAGEM_INFORMADA_MATERIALIZACAO,
+        {
+            "redis_key": f"{smtr_constants.GPS_SPPO_DATASET_ID.value}\
+.{smtr_constants.GPS_SPPO_TREATED_TABLE_ID.value}",
+            "dict_key": "last_run_timestamp",
+            "datetime_format": "%Y-%m-%dT%H:%M:%S",
+            "delay_hours": smtr_constants.GPS_SPPO_MATERIALIZE_DELAY_HOURS.value,
+            "schedule_cron": cron_every_hour_minute_6,
+        },
+        {
+            "redis_key": f"{gps_zirix_constants.GPS_SPPO_ZIRIX_RAW_DATASET_ID.value}\
+.{smtr_constants.GPS_SPPO_TREATED_TABLE_ID.value}",
+            "dict_key": "last_run_timestamp",
+            "datetime_format": "%Y-%m-%dT%H:%M:%S",
+            "delay_hours": smtr_constants.GPS_SPPO_MATERIALIZE_DELAY_HOURS.value,
+            "schedule_cron": cron_every_hour_minute_6,
+        },
+    ],
 )
