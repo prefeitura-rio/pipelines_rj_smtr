137c70f742db19ed9d0a7fb17cccbf95b04347bb
Rafael <rafael.c.pinheiro96@gmail.com>
2024-11-21 11:17:29 -0300

    add aninhamento condicional

--- pipelines/capture/templates/tasks.py
+++ pipelines/capture/templates/tasks.py
@@ -275,7 +275,8 @@ def transform_raw_to_nested_structure(
         for step in pretreat_funcs:
             data = step(data=data, timestamp=timestamp, primary_keys=primary_keys)
 
-        data = transform_to_nested_structure(data=data, primary_keys=primary_keys)
+        if len(primary_keys) < len(data.columns):
+            data = transform_to_nested_structure(data=data, primary_keys=primary_keys)
 
         timestamp = create_timestamp_captura(timestamp=timestamp)
         data["timestamp_captura"] = timestamp
