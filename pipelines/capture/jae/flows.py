050d621c464f0a1301150c792f2ba8f85781e27d
Rafael <rafael.c.pinheiro96@gmail.com>
2024-11-21 11:17:54 -0300

    cria flow de captura da transacao_ordem

---
+++ pipelines/capture/jae/flows.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Flows de captura dos dados da Jaé"""
+from pipelines.capture.jae.constants import constants
+from pipelines.capture.jae.tasks import create_jae_general_extractor
+from pipelines.capture.templates.flows import create_default_capture_flow
+from pipelines.constants import constants as smtr_constants
+from pipelines.utils.prefect import set_default_parameters
+
+CAPTURA_VIAGEM_INFORMADA = create_default_capture_flow(
+    flow_name="jae: transacao_ordem - captura",
+    source=constants.TRANSACAO_ORDEM_SOURCE.value,
+    create_extractor_task=create_jae_general_extractor,
+    agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
+)
+set_default_parameters(CAPTURA_VIAGEM_INFORMADA, {"recapture": True})
