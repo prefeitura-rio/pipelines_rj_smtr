fffe2a6351b28daf839c6490c0220cea9b833e38
Rafael <rafael.c.pinheiro96@gmail.com>
2024-11-21 12:00:15 -0300

    corrige nome variavel

--- pipelines/capture/jae/flows.py
+++ pipelines/capture/jae/flows.py
@@ -6,10 +6,10 @@ from pipelines.capture.templates.flows import create_default_capture_flow
 from pipelines.constants import constants as smtr_constants
 from pipelines.utils.prefect import set_default_parameters
 
-CAPTURA_VIAGEM_INFORMADA = create_default_capture_flow(
+CAPTURA_TRANSACAO_ORDEM = create_default_capture_flow(
     flow_name="jae: transacao_ordem - captura",
     source=constants.TRANSACAO_ORDEM_SOURCE.value,
     create_extractor_task=create_jae_general_extractor,
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
 )
-set_default_parameters(CAPTURA_VIAGEM_INFORMADA, {"recapture": True})
+set_default_parameters(CAPTURA_TRANSACAO_ORDEM, {"recapture": True})
