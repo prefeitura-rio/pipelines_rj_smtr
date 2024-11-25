802c2b8cb8e583ca726ea067a425ff2e89930d5e
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-11-25 17:35:47 -0300

    Bilhetagem - Cria ligação entre ordem de pagamento e transação (#333)
    * add metodo get_last_scheduled_timestamp
    
    * add aninhamento condicional
    
    * cria flow de captura da transacao_ordem
    
    * altera modo de particionamento para auto
    
    * altera criacao do schema
    
    * corrige nome variavel
    
    * add sample_filepath
    
    * cria funcao para gerar cron
    
    * cria flow materializacao bilhetagem
    
    * add flows
    
    * transacao ordem
    
    * adiciona datatetime_ultima_atualizacao
    
    * corrige query
    
    * corrige funcao get_raw_db
    
    * move transacao_ordem para dataset novo
    
    * cria source jae
    
    * altera para prod
    
    * add changelog
    
    * espera materializacao ordem pagamento
    
    * adiciona novas tabelas no exclude
    
    * add novas colunas transacao
    
    * corrige nome da coluna id_ordem_pagamento_consorcio_operador_dia
    
    * corrige nome da coluna id_ordem_pagamento_consorcio_operador_dia
    
    * remove comentario
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pipelines/capture/rioonibus/CHANGELOG.md
+++ pipelines/capture/rioonibus/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog - source_rioonibus
 
+## [1.0.1] - 2024-11-25
+
+### Alterado
+
+- Substitui variavel de expressão cron pela função `create_daily_cron` (https://github.com/prefeitura-rio/pipelines_rj_smtr/pull/333)
+
 ## [1.0.0] - 2024-10-21
 
 ### Adicionado
---
+++ pipelines/treatment/bilhetagem/flows.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Flows de tratamento dos dados de bilhetagem
+"""
+
+from pipelines.constants import constants as smtr_constants
+from pipelines.migration.br_rj_riodejaneiro_bilhetagem.constants import (
+    constants as old_constants,
+)
+from pipelines.schedules import create_daily_cron
+from pipelines.treatment.bilhetagem.constants import constants
+from pipelines.treatment.templates.flows import create_default_materialization_flow
+
+ordem_pagamento_materialize_params = (
+    old_constants.BILHETAGEM_MATERIALIZACAO_ORDEM_PAGAMENTO_PARAMS.value
+)
+
+PLANEJAMENTO_DIARIO_MATERIALIZACAO = create_default_materialization_flow(
+    flow_name="transacao_ordem - materializacao",
+    selector=constants.TRANSACAO_ORDEM_SELECTOR.value,
+    agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
+    wait=[
+        {
+            "redis_key": f"{ordem_pagamento_materialize_params['dataset_id']}\
+.{ordem_pagamento_materialize_params['table_id']}",
+            "dict_key": "last_run_timestamp",
+            "datetime_format": "%Y-%m-%dT%H:%M:%S",
+            "delay_hours": ordem_pagamento_materialize_params["dbt_vars"]["date_range"][
+                "delay_hours"
+            ],
+            "schedule_cron": create_daily_cron(hour=5),
+        }
+    ],
+)
