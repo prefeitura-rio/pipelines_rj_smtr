1bec04f51e4e12c613cee952e2f5c7fde19380d2
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2024-12-18 14:29:54 -0300

    cria flow de verificacao de conexao com a jae

--- pipelines/capture/jae/flows.py
+++ pipelines/capture/jae/flows.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 """Flows de captura dos dados da Jaé"""
+from prefect import case
+from prefeitura_rio.pipelines_utils.custom import Flow
+
 from pipelines.capture.jae.constants import constants
-from pipelines.capture.jae.tasks import create_jae_general_extractor
+from pipelines.capture.jae.tasks import (
+    create_database_error_discord_message,
+    create_jae_general_extractor,
+    test_jae_databases_connections,
+)
 from pipelines.capture.templates.flows import create_default_capture_flow
 from pipelines.constants import constants as smtr_constants
+from pipelines.tasks import log_discord
 from pipelines.utils.prefect import set_default_parameters
 
 CAPTURA_TRANSACAO_ORDEM = create_default_capture_flow(
@@ -13,3 +21,14 @@ CAPTURA_TRANSACAO_ORDEM = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
 )
 set_default_parameters(CAPTURA_TRANSACAO_ORDEM, {"recapture": True})
+
+with Flow("jae: verifica ip do banco de dados") as verificacao_ip:
+    success, failed_connections = test_jae_databases_connections()
+    with case(success, False):
+        message = create_database_error_discord_message(failed_connections=failed_connections)
+        send_discord_message = log_discord(
+            message=message,
+            key=constants.ALERT_WEBHOOK.value,
+            dados_tag=True,
+        )
+    verificacao_ip.set_reference_tasks(tasks=[send_discord_message, success])
