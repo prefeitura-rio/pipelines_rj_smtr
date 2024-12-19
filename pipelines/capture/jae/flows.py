8eeed056642e2c429f19a0fc39034997a5929339
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2024-12-19 08:54:37 -0300

    flow config

--- pipelines/capture/jae/flows.py
+++ pipelines/capture/jae/flows.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """Flows de captura dos dados da Jaé"""
 from prefect import case
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
 from prefeitura_rio.pipelines_utils.custom import Flow
+from prefeitura_rio.pipelines_utils.state_handlers import (
+    handler_initialize_sentry,
+    handler_inject_bd_credentials,
+)
 
 from pipelines.capture.jae.constants import constants
 from pipelines.capture.jae.tasks import (
@@ -11,6 +17,7 @@ from pipelines.capture.jae.tasks import (
 )
 from pipelines.capture.templates.flows import create_default_capture_flow
 from pipelines.constants import constants as smtr_constants
+from pipelines.schedules import every_hour
 from pipelines.tasks import log_discord
 from pipelines.utils.prefect import set_default_parameters
 
@@ -32,3 +39,11 @@ with Flow("jae: verifica ip do banco de dados") as verificacao_ip:
             dados_tag=True,
         )
     verificacao_ip.set_reference_tasks(tasks=[send_discord_message, success])
+
+verificacao_ip.storage = GCS(smtr_constants.GCS_FLOWS_BUCKET.value)
+verificacao_ip.run_config = KubernetesRun(
+    image=smtr_constants.DOCKER_IMAGE.value,
+    labels=[smtr_constants.RJ_SMTR_AGENT_LABEL.value],
+)
+verificacao_ip.state_handlers = [handler_inject_bd_credentials, handler_initialize_sentry]
+verificacao_ip.schedule = every_hour
