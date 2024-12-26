f1b7f703a08d87830262e4d47eb00c5f08f4a05c
Guilherme Botelho <guilhermehcb@hotmail.com>
2024-12-26 10:37:52 -0300

    import movidesk flows

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -3,6 +3,7 @@
 Imports all flows for every project so we can register all of them.
 """
 from pipelines.capture.jae.flows import *  # noqa
+from pipelines.capture.movidesk.flows import *  # noqa
 from pipelines.capture.rioonibus.flows import *  # noqa
 from pipelines.capture.sonda.flows import *  # noqa
 from pipelines.exemplo import *  # noqa
