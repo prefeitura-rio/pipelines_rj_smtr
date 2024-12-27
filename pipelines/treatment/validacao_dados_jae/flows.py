1775ca3196cf885fe8feda87259ab98cced3b50b
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2024-12-27 15:14:46 -0300

    add validacao_dados_jae

--- pipelines/treatment/validacao_dados_jae/flows.py
+++ pipelines/treatment/validacao_dados_jae/flows.py
@@ -7,7 +7,7 @@ from pipelines.constants import constants as smtr_constants
 from pipelines.treatment.templates.flows import create_default_materialization_flow
 from pipelines.treatment.validacao_dados_jae.constants import constants
 
-PLANEJAMENTO_DIARIO_MATERIALIZACAO = create_default_materialization_flow(
+VALIDACAO_DADOS_JAE_MATERIALIZACAO = create_default_materialization_flow(
     flow_name="validacao_dados_jae - materializacao",
     selector=constants.VALIDACAO_DADOS_JAE_SELECTOR.value,
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
