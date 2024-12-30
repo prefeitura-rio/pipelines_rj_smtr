ee19094a700d694aa76c905e9797a4af871c35cf
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2024-12-30 13:20:23 -0300

    Cria validação dos dados de integração da Jaé com base na Matriz da SMTR (#371)
    * cria nova validação de integrações
    
    * add data_inicio_matriz
    
    * cria tabela matriz_integracao com base na matriz da smtr
    
    * corrige integração com transação duplicada
    
    * cria validação das integrações usando a matriz da smtr
    
    * cria flow de materialização da nova validação dos dados da Jaé
    
    * schedule_cron opcional
    
    * cria materialização da matriz de integração da smtr
    
    * add matriz_integracao_smtr / add validacao_dados_jae
    
    * add validacao_dados_jae
    
    * wait integracao e transacao
    
    * add change log pr 371
    
    * ajusta schedule
    
    * corrige mensagem de erro
    
    * descomenta refs
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pipelines/migration/br_rj_riodejaneiro_gtfs/flows.py
+++ pipelines/migration/br_rj_riodejaneiro_gtfs/flows.py
@@ -238,7 +238,8 @@ with Flow("SMTR: GTFS - Captura/Tratamento") as gtfs_captura_nova:
             + " "
             + constants.PLANEJAMENTO_MATERIALIZACAO_DATASET_ID.value,
             _vars=dbt_vars,
-            exclude="calendario aux_calendario_manual viagem_planejada_planejamento",
+            exclude="calendario aux_calendario_manual viagem_planejada_planejamento \
+matriz_integracao",
         ).set_upstream(task=wait_captura)
 
         run_dbt_failed = task_value_is_none(wait_run_dbt_model)
