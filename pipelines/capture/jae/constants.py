69b654634e5ea8ac7001fae341567cb6875565ba
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-01-02 08:54:54 -0300

    add tabelas auxiliares

--- pipelines/capture/jae/constants.py
+++ pipelines/capture/jae/constants.py
@@ -50,6 +50,186 @@ class constants(Enum):  # pylint: disable=c0103
 
     JAE_SECRET_PATH = "smtr_jae_access_data"
 
+    JAE_PRIVATE_BUCKET_NAMES = {"prod": "rj-smtr-jae-private", "dev": "rj-smtr-dev-private"}
+
+    JAE_AUXILIAR_CAPTURE_PARAMS = {
+        "linha": {
+            "query": """
+                SELECT
+                    *
+                FROM
+                    LINHA
+                WHERE
+                    DT_INCLUSAO BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "principal_db",
+            "primary_key": ["CD_LINHA"],
+        },
+        "operadora_transporte": {
+            "query": """
+                SELECT
+                    o.*,
+                    m.DS_TIPO_MODAL
+                FROM
+                    OPERADORA_TRANSPORTE o
+                LEFT JOIN
+                    TIPO_MODAL m
+                ON
+                    o.CD_TIPO_MODAL = m.CD_TIPO_MODAL
+                WHERE
+                    DT_INCLUSAO BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "principal_db",
+            "primary_key": ["CD_OPERADORA_TRANSPORTE"],
+        },
+        "cliente": {
+            "query": """
+                SELECT
+                    c.*
+                FROM
+                    CLIENTE c
+                WHERE
+                    DT_CADASTRO BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "principal_db",
+            "primary_key": ["CD_CLIENTE"],
+            "pre_treatment_reader_args": {"dtype": {"NR_DOCUMENTO": "object"}},
+        },
+        "pessoa_fisica": {
+            "query": """
+                SELECT
+                    p.*,
+                    c.DT_CADASTRO
+                FROM
+                    PESSOA_FISICA p
+                JOIN
+                    CLIENTE c
+                ON
+                    p.CD_CLIENTE = c.CD_CLIENTE
+                WHERE
+                    c.DT_CADASTRO BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "principal_db",
+            "primary_key": ["CD_CLIENTE"],
+            "save_bucket_names": JAE_PRIVATE_BUCKET_NAMES,
+        },
+        "gratuidade": {
+            "query": """
+                SELECT
+                    g.*,
+                    t.descricao AS tipo_gratuidade
+                FROM
+                    gratuidade g
+                LEFT JOIN
+                    tipo_gratuidade t
+                ON
+                    g.id_tipo_gratuidade = t.id
+                WHERE
+                    g.data_inclusao BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "gratuidade_db",
+            "primary_key": ["id"],
+            "save_bucket_names": JAE_PRIVATE_BUCKET_NAMES,
+        },
+        "consorcio": {
+            "query": """
+                SELECT
+                    *
+                FROM
+                    CONSORCIO
+                WHERE
+                    DT_INCLUSAO BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "principal_db",
+            "primary_key": ["CD_CONSORCIO"],
+        },
+        "percentual_rateio_integracao": {
+            "query": """
+                SELECT
+                    *
+                FROM
+                    percentual_rateio_integracao
+                WHERE
+                    dt_inclusao BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "ressarcimento_db",
+            "primary_key": ["id"],
+        },
+        "linha_consorcio": {
+            "query": """
+                SELECT
+                    *
+                FROM
+                    LINHA_CONSORCIO
+                WHERE
+                    DT_INCLUSAO BETWEEN '{start}'
+                    AND '{end}'
+                    OR DT_FIM_VALIDADE BETWEEN DATE('{start}')
+                    AND DATE('{end}')
+            """,
+            "database": "principal_db",
+            "primary_key": [
+                "CD_CONSORCIO",
+                "CD_LINHA",
+            ],
+        },
+        "linha_consorcio_operadora_transporte": {
+            "query": """
+                SELECT
+                    *
+                FROM
+                    LINHA_CONSORCIO_OPERADORA_TRANSPORTE
+                WHERE
+                    DT_INCLUSAO BETWEEN '{start}'
+                    AND '{end}'
+                    OR DT_FIM_VALIDADE BETWEEN DATE('{start}')
+                    AND DATE('{end}')
+            """,
+            "database": "principal_db",
+            "primary_key": [
+                "CD_CONSORCIO",
+                "CD_OPERADORA_TRANSPORTE",
+                "CD_LINHA",
+            ],
+        },
+        "endereco": {
+            "query": """
+                SELECT
+                    *
+                FROM
+                    ENDERECO
+                WHERE
+                    DT_INCLUSAO BETWEEN '{start}'
+                    AND '{end}'
+                    OR
+                    DT_INATIVACAO BETWEEN '{start}'
+                    AND '{end}'
+            """,
+            "database": "principal_db",
+            "primary_key": [
+                "NR_SEQ_ENDERECO",
+            ],
+        },
+    }
+
+    # JAE_AUXILIAR_SOURCES = {
+    #     k: SourceTable(
+    #         source_name=JAE_SORCE_NAME,
+    #         table_id=k,
+    #         first_timestamp=datetime(2024, 12, 30, 0, 0, 0),
+    #         schedule_cron=create_hourly_cron(),
+    #         primary_keys=v["primary_keys"],
+    #     )
+    #     for k, v in JAE_AUXILIAR_CAPTURE_PARAMS
+    # }
+
     TRANSACAO_ORDEM_TABLE_ID = "transacao_ordem"
 
     JAE_TABLE_CAPTURE_PARAMS = {
