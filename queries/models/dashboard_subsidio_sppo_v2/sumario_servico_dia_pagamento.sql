ba730ab6ddfac1668091c27ab40685888a007738
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-01-27 16:33:15 -0300

    add lógica para não materializar após DATA_SUBSIDIO_V14_INICIO

--- queries/models/dashboard_subsidio_sppo_v2/sumario_servico_dia_pagamento.sql
+++ queries/models/dashboard_subsidio_sppo_v2/sumario_servico_dia_pagamento.sql
@@ -1,105 +1,121 @@
+-- depends_on: {{ ref('subsidio_faixa_servico_dia') }}
+-- depends_on: {{ ref('subsidio_sumario_servico_dia_pagamento') }}
+-- depends_on: {{ ref('subsidio_faixa_servico_dia_tipo_viagem') }}
 {{
-  config(
-    materialized="incremental",
-    partition_by={"field": "data", "data_type": "date", "granularity": "day"},
-    incremental_strategy="insert_overwrite",
-  )
+    config(
+        materialized="incremental",
+        partition_by={"field": "data", "data_type": "date", "granularity": "day"},
+        incremental_strategy="insert_overwrite",
+    )
 }}
 
-WITH
-  subsidio_dia AS (
-  SELECT
-    data,
-    tipo_dia,
-    consorcio,
-    servico,
-    SAFE_CAST(AVG(pof) AS NUMERIC) AS media_pof,
-    SAFE_CAST(COALESCE(STDDEV(pof), 0) AS NUMERIC) AS desvp_pof
-  FROM
-    {{ ref("subsidio_faixa_servico_dia") }}
-    -- rj-smtr.financeiro_staging.subsidio_faixa_servico_dia
-  WHERE
-    data BETWEEN DATE("{{ var("start_date") }}")
-    AND DATE("{{ var("end_date") }}")
-  GROUP BY
-    data,
-    tipo_dia,
-    consorcio,
-    servico
-  ),
-  valores_subsidio AS (
-  SELECT
-    *
-  FROM
-    {{ ref("subsidio_sumario_servico_dia_pagamento") }}
-    -- rj-smtr.financeiro.subsidio_sumario_servico_dia_pagamento
-  WHERE
-    data BETWEEN DATE("{{ var("start_date") }}")
-    AND DATE("{{ var("end_date") }}")
-  ),
-  pivot_data AS (
-  SELECT
-    *
-  FROM (
-    SELECT
-      data,
-      tipo_dia,
-      consorcio,
-      servico,
-      tipo_viagem,
-      km_apurada_faixa
-    FROM
-      {{ ref("subsidio_faixa_servico_dia_tipo_viagem") }}
-      -- rj-smtr.financeiro.subsidio_faixa_servico_dia_tipo_viagem
-    WHERE
-      data BETWEEN DATE("{{ var("start_date") }}")
-      AND DATE("{{ var("end_date") }}")
-    )
-    PIVOT(SUM(km_apurada_faixa) AS km_apurada FOR tipo_viagem IN (
-      "Registrado com ar inoperante" AS registrado_com_ar_inoperante,
-      "Não licenciado" AS n_licenciado,
-      "Autuado por ar inoperante" AS autuado_ar_inoperante,
-      "Autuado por segurança" AS autuado_seguranca,
-      "Autuado por limpeza/equipamento" AS autuado_limpezaequipamento,
-      "Licenciado sem ar e não autuado" AS licenciado_sem_ar_n_autuado,
-      "Licenciado com ar e não autuado" AS licenciado_com_ar_n_autuado,
-      "Não vistoriado" AS n_vistoriado,
-      "Sem transação" AS sem_transacao))
-  )
-SELECT
-  vs.data,
-  vs.tipo_dia,
-  vs.consorcio,
-  vs.servico,
-  vs.viagens_dia,
-  vs.km_apurada_dia,
-  vs.km_subsidiada_dia,
-  vs.km_planejada_dia,
-  sd.media_pof,
-  sd.desvp_pof,
-  COALESCE(km_apurada_registrado_com_ar_inoperante, 0) AS km_apurada_registrado_com_ar_inoperante,
-  COALESCE(km_apurada_n_licenciado, 0) AS km_apurada_n_licenciado,
-  COALESCE(km_apurada_autuado_ar_inoperante, 0) AS km_apurada_autuado_ar_inoperante,
-  COALESCE(km_apurada_autuado_seguranca, 0) AS km_apurada_autuado_seguranca,
-  COALESCE(km_apurada_autuado_limpezaequipamento, 0) AS km_apurada_autuado_limpezaequipamento,
-  COALESCE(km_apurada_licenciado_sem_ar_n_autuado, 0) AS km_apurada_licenciado_sem_ar_n_autuado,
-  COALESCE(km_apurada_licenciado_com_ar_n_autuado, 0) AS km_apurada_licenciado_com_ar_n_autuado,
-  COALESCE(km_apurada_n_vistoriado, 0) AS km_apurada_n_vistoriado,
-  COALESCE(km_apurada_sem_transacao, 0) AS km_apurada_sem_transacao,
-  vs.valor_a_pagar,
-  vs.valor_glosado,
-  vs.valor_acima_limite,
-  vs.valor_total_sem_glosa,
-  vs.valor_total_apurado,
-  vs.valor_judicial,
-  vs.valor_penalidade,
-  '{{ var("version") }}' as versao,
-  CURRENT_DATETIME("America/Sao_Paulo") as datetime_ultima_atualizacao
-FROM
-  valores_subsidio AS vs
-LEFT JOIN
-  subsidio_dia AS sd
-USING(data, tipo_dia, consorcio, servico)
-LEFT JOIN
-  pivot_data AS pd
-USING(data, tipo_dia, consorcio, servico)
+{% set run_model = false %}
+{% if var("end_date") >= var("DATA_SUBSIDIO_V14_INICIO") %}
+    {% set end_date = (
+        modules.datetime.datetime.strptime(
+            var("DATA_SUBSIDIO_V14_INICIO"), "%Y-%m-%d"
+        )
+        - modules.datetime.timedelta(days=1)
+    ).strftime("%Y-%m-%d") %}
+    {% set run_model = true %}
+{% endif %}
+
+{% if run_model == true %}
+    with
+        subsidio_dia as (
+            select
+                data,
+                tipo_dia,
+                consorcio,
+                servico,
+                safe_cast(avg(pof) as numeric) as media_pof,
+                safe_cast(coalesce(stddev(pof), 0) as numeric) as desvp_pof
+            from {{ ref("subsidio_faixa_servico_dia") }}
+            -- from `rj-smtr.financeiro_staging.subsidio_faixa_servico_dia`
+            where
+                data between date('{{ var("start_date") }}') and date('{{ end_date }}')
+            group by data, tipo_dia, consorcio, servico
+        ),
+        valores_subsidio as (
+            select *
+            from {{ ref("subsidio_sumario_servico_dia_pagamento") }}
+            -- from `rj-smtr.financeiro.subsidio_sumario_servico_dia_pagamento`
+            where
+                data between date('{{ var("start_date") }}') and date('{{ end_date }}')
+        ),
+        pivot_data as (
+            select *
+            from
+                (
+                    select
+                        data,
+                        tipo_di '{{ end_date }}' a,
+                        consorcio,
+                        servico,
+                        tipo_viagem,
+                        km_apurada_faixa
+                    from {{ ref("subsidio_faixa_servico_dia_tipo_viagem") }}
+                    -- from `rj-smtr.financeiro.subsidio_faixa_servico_dia_tipo_viagem`
+                    where
+                        data between date('{{ var("start_date") }}') and date(
+                            '{{ end_date }}'
+                        )
+                ) pivot (
+                    sum(km_apurada_faixa) as km_apurada for tipo_viagem in (
+                        "Registrado com ar inoperante" as registrado_com_ar_inoperante,
+                        "Não licenciado" as n_licenciado,
+                        "Autuado por ar inoperante" as autuado_ar_inoperante,
+                        "Autuado por segurança" as autuado_seguranca,
+                        "Autuado por limpeza/equipamento" as autuado_limpezaequipamento,
+                        "Licenciado sem ar e não autuado"
+                        as licenciado_sem_ar_n_autuado,
+                        "Licenciado com ar e não autuado"
+                        as licenciado_com_ar_n_autuado,
+                        "Não vistoriado" as n_vistoriado,
+                        "Sem transação" as sem_transacao
+                    )
+                )
+        )
+    select
+        vs.data,
+        vs.tipo_dia,
+        vs.consorcio,
+        vs.servico,
+        vs.viagens_dia,
+        vs.km_apurada_dia,
+        vs.km_subsidiada_dia,
+        vs.km_planejada_dia,
+        sd.media_pof,
+        sd.desvp_pof,
+        coalesce(
+            km_apurada_registrado_com_ar_inoperante, 0
+        ) as km_apurada_registrado_com_ar_inoperante,
+        coalesce(km_apurada_n_licenciado, 0) as km_apurada_n_licenciado,
+        coalesce(
+            km_apurada_autuado_ar_inoperante, 0
+        ) as km_apurada_autuado_ar_inoperante,
+        coalesce(km_apurada_autuado_seguranca, 0) as km_apurada_autuado_seguranca,
+        coalesce(
+            km_apurada_autuado_limpezaequipamento, 0
+        ) as km_apurada_autuado_limpezaequipamento,
+        coalesce(
+            km_apurada_licenciado_sem_ar_n_autuado, 0
+        ) as km_apurada_licenciado_sem_ar_n_autuado,
+        coalesce(
+            km_apurada_licenciado_com_ar_n_autuado, 0
+        ) as km_apurada_licenciado_com_ar_n_autuado,
+        coalesce(km_apurada_n_vistoriado, 0) as km_apurada_n_vistoriado,
+        coalesce(km_apurada_sem_transacao, 0) as km_apurada_sem_transacao,
+        vs.valor_a_pagar,
+        vs.valor_glosado,
+        vs.valor_acima_limite,
+        vs.valor_total_sem_glosa,
+        vs.valor_total_apurado,
+        vs.valor_judicial,
+        vs.valor_penalidade,
+        '{{ var("version") }}' as versao,
+        current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao
+    from valores_subsidio as vs
+    left join subsidio_dia as sd using (data, tipo_dia, consorcio, servico)
+    left join pivot_data as pd using (data, tipo_dia, consorcio, servico)
+{% endif %}
--- queries/models/financeiro/subsidio_sumario_servico_dia_pagamento.sql
+++ queries/models/financeiro/subsidio_sumario_servico_dia_pagamento.sql
@@ -1,3 +1,6 @@
+-- depends_on: {{ ref('valor_km_tipo_viagem') }}
+-- depends_on: {{ ref('subsidio_penalidade_servico_faixa') }}
+-- depends_on: {{ ref('subsidio_faixa_servico_dia_tipo_viagem') }}
 {{
     config(
         materialized="incremental",
@@ -6,119 +9,129 @@
     )
 }}
 
-with
-    subsidio_dia as (
-        select
-            data,
-            tipo_dia,
-            consorcio,
-            servico,
-            sum(viagens_faixa) as viagens_dia,
-            sum(km_planejada_faixa) as km_planejada_dia
-        from {{ ref("subsidio_faixa_servico_dia") }}
-        -- from `rj-smtr.financeiro_staging.subsidio_faixa_servico_dia`
-        where
-            data
-            between date('{{ var("start_date") }}') and date('{{ var("end_date") }}')
-        group by data, tipo_dia, consorcio, servico
-    ),
-    subsidio_parametros as (
-        select distinct
-            data_inicio,
-            data_fim,
-            status,
-            subsidio_km,
-            max(subsidio_km) over (
-                partition by data_inicio, data_fim
-            ) as subsidio_km_teto
-        from {{ ref("valor_km_tipo_viagem") }}
-    -- from `rj-smtr.subsidio.valor_km_tipo_viagem`
-    ),
-    penalidade as (
-        select
-            data,
-            tipo_dia,
-            consorcio,
-            servico,
-            sum(valor_penalidade) as valor_penalidade
-        from {{ ref("subsidio_penalidade_servico_faixa") }}
-        -- from `rj-smtr.financeiro.subsidio_penalidade_servico_faixa`
-        where
-            data
-            between date('{{ var("start_date") }}') and date('{{ var("end_date") }}')
-        group by data, tipo_dia, consorcio, servico
-    ),
-    subsidio_dia_tipo_viagem as (
-        select *
-        from {{ ref("subsidio_faixa_servico_dia_tipo_viagem") }}
-        -- from `rj-smtr.financeiro.subsidio_faixa_servico_dia_tipo_viagem`
-        where
-            data
-            between date('{{ var("start_date") }}') and date('{{ var("end_date") }}')
-    ),
-    valores_calculados as (
-        select
-            s.data,
-            s.tipo_dia,
-            s.consorcio,
-            s.servico,
-            pe.valor_penalidade,
-            sum(s.km_apurada_faixa) as km_apurada_dia,
-            sum(s.km_subsidiada_faixa) as km_subsidiada_dia,
-            coalesce(sum(s.valor_acima_limite), 0) as valor_acima_limite,
-            coalesce(sum(s.valor_total_sem_glosa), 0) as valor_total_sem_glosa,
-            sum(s.valor_apurado) + pe.valor_penalidade as valor_total_com_glosa,
-            case
-                when pe.valor_penalidade != 0
-                then - pe.valor_penalidade
-                else
-                    safe_cast(
-                        (
-                            sum(
-                                if(
-                                    indicador_viagem_dentro_limite = true
-                                    and indicador_penalidade_judicial = true,
-                                    km_apurada_faixa * subsidio_km_teto,
-                                    0
+{% set run_model = false %}
+{% if var("end_date") >= var("DATA_SUBSIDIO_V14_INICIO") %}
+    {% set end_date = (
+        modules.datetime.datetime.strptime(
+            var("DATA_SUBSIDIO_V14_INICIO"), "%Y-%m-%d"
+        )
+        - modules.datetime.timedelta(days=1)
+    ).strftime("%Y-%m-%d") %}
+    {% set run_model = true %}
+{% endif %}
+
+{% if run_model == true %}
+    with
+        subsidio_dia as (
+            select
+                data,
+                tipo_dia,
+                consorcio,
+                servico,
+                sum(viagens_faixa) as viagens_dia,
+                sum(km_planejada_faixa) as km_planejada_dia
+            from {{ ref("subsidio_faixa_servico_dia") }}
+            -- from `rj-smtr.financeiro_staging.subsidio_faixa_servico_dia`
+            where
+                data between date('{{ var("start_date") }}') and date('{{ end_date }}')
+            group by data, tipo_dia, consorcio, servico
+        ),
+        subsidio_parametros as (
+            select distinct
+                data_inicio,
+                data_fim,
+                status,
+                subsidio_km,
+                max(subsidio_km) over (
+                    partition by data_inicio, data_fim
+                ) as subsidio_km_teto
+            from {{ ref("valor_km_tipo_viagem") }}
+        -- from `rj-smtr.subsidio.valor_km_tipo_viagem`
+        ),
+        penalidade as (
+            select
+                data,
+                tipo_dia,
+                consorcio,
+                servico,
+                sum(valor_penalidade) as valor_penalidade
+            from {{ ref("subsidio_penalidade_servico_faixa") }}
+            -- from `rj-smtr.financeiro.subsidio_penalidade_servico_faixa`
+            where
+                data between date('{{ var("start_date") }}') and date('{{ end_date }}')
+            group by data, tipo_dia, consorcio, servico
+        ),
+        subsidio_dia_tipo_viagem as (
+            select *
+            from {{ ref("subsidio_faixa_servico_dia_tipo_viagem") }}
+            -- from `rj-smtr.financeiro.subsidio_faixa_servico_dia_tipo_viagem`
+            where
+                data between date('{{ var("start_date") }}') and date('{{ end_date }}')
+        ),
+        valores_calculados as (
+            select
+                s.data,
+                s.tipo_dia,
+                s.consorcio,
+                s.servico,
+                pe.valor_penalidade,
+                sum(s.km_apurada_faixa) as km_apurada_dia,
+                sum(s.km_subsidiada_faixa) as km_subsidiada_dia,
+                coalesce(sum(s.valor_acima_limite), 0) as valor_acima_limite,
+                coalesce(sum(s.valor_total_sem_glosa), 0) as valor_total_sem_glosa,
+                sum(s.valor_apurado) + pe.valor_penalidade as valor_total_com_glosa,
+                case
+                    when pe.valor_penalidade != 0
+                    then - pe.valor_penalidade
+                    else
+                        safe_cast(
+                            (
+                                sum(
+                                    if(
+                                        indicador_viagem_dentro_limite = true
+                                        and indicador_penalidade_judicial = true,
+                                        km_apurada_faixa * subsidio_km_teto,
+                                        0
+                                    )
+                                ) - sum(
+                                    if(
+                                        indicador_viagem_dentro_limite = true
+                                        and indicador_penalidade_judicial = true,
+                                        km_apurada_faixa * subsidio_km,
+                                        0
+                                    )
                                 )
-                            ) - sum(
-                                if(
-                                    indicador_viagem_dentro_limite = true
-                                    and indicador_penalidade_judicial = true,
-                                    km_apurada_faixa * subsidio_km,
-                                    0
-                                )
-                            )
-                        ) as numeric
-                    )
-            end as valor_judicial,
-        from subsidio_dia_tipo_viagem as s
-        left join penalidade as pe using (data, tipo_dia, consorcio, servico)
-        left join
-            subsidio_parametros as sp
-            on s.data between sp.data_inicio and sp.data_fim
-            and s.tipo_viagem = sp.status
-        group by s.data, s.tipo_dia, s.consorcio, s.servico, pe.valor_penalidade
-    )
-select
-    sd.data,
-    sd.tipo_dia,
-    sd.consorcio,
-    sd.servico,
-    sd.viagens_dia,
-    vc.km_apurada_dia,
-    vc.km_subsidiada_dia,
-    sd.km_planejada_dia,
-    vc.valor_total_com_glosa as valor_a_pagar,
-    vc.valor_total_com_glosa - vc.valor_total_sem_glosa as valor_glosado,
-    vc.valor_acima_limite,
-    vc.valor_total_sem_glosa,
-    vc.valor_acima_limite
-    + vc.valor_penalidade
-    + vc.valor_total_sem_glosa as valor_total_apurado,
-    vc.valor_judicial,
-    vc.valor_penalidade,
-    '{{ var("version") }}' as versao,
-    current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao
-from subsidio_dia as sd
-left join valores_calculados as vc using (data, tipo_dia, consorcio, servico)
+                            ) as numeric
+                        )
+                end as valor_judicial,
+            from subsidio_dia_tipo_viagem as s
+            left join penalidade as pe using (data, tipo_dia, consorcio, servico)
+            left join
+                subsidio_parametros as sp
+                on s.data between sp.data_inicio and sp.data_fim
+                and s.tipo_viagem = sp.status
+            group by s.data, s.tipo_dia, s.consorcio, s.servico, pe.valor_penalidade
+        )
+    select
+        sd.data,
+        sd.tipo_dia,
+        sd.consorcio,
+        sd.servico,
+        sd.viagens_dia,
+        vc.km_apurada_dia,
+        vc.km_subsidiada_dia,
+        sd.km_planejada_dia,
+        vc.valor_total_com_glosa as valor_a_pagar,
+        vc.valor_total_com_glosa - vc.valor_total_sem_glosa as valor_glosado,
+        vc.valor_acima_limite,
+        vc.valor_total_sem_glosa,
+        vc.valor_acima_limite
+        + vc.valor_penalidade
+        + vc.valor_total_sem_glosa as valor_total_apurado,
+        vc.valor_judicial,
+        vc.valor_penalidade,
+        '{{ var("version") }}' as versao,
+        current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao
+    from subsidio_dia as sd
+    left join valores_calculados as vc using (data, tipo_dia, consorcio, servico)
+{% endif %}
