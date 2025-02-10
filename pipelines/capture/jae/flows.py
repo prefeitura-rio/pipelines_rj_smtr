2eebab57be145bfdd8d7a1b4746037a2f26fed41
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-02-10 19:32:18 -0300

    cria flow de backup da billingpay

--- pipelines/capture/jae/flows.py
+++ pipelines/capture/jae/flows.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Flows de captura dos dados da Jaé"""
-from prefect import case
+from prefect import Parameter, case, unmapped
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 from prefeitura_rio.pipelines_utils.custom import Flow
@@ -13,12 +13,17 @@ from pipelines.capture.jae.constants import constants
 from pipelines.capture.jae.tasks import (
     create_database_error_discord_message,
     create_jae_general_extractor,
+    get_jae_db_config,
+    get_raw_backup_billingpay,
+    get_table_info,
+    set_redis_backup_billingpay,
     test_jae_databases_connections,
+    upload_backup_billingpay,
 )
 from pipelines.capture.templates.flows import create_default_capture_flow
 from pipelines.constants import constants as smtr_constants
 from pipelines.schedules import every_hour
-from pipelines.tasks import log_discord
+from pipelines.tasks import get_run_env, get_scheduled_timestamp, log_discord
 from pipelines.utils.prefect import set_default_parameters
 
 CAPTURA_TRANSACAO_ORDEM = create_default_capture_flow(
@@ -47,3 +52,45 @@ verificacao_ip.run_config = KubernetesRun(
 )
 verificacao_ip.state_handlers = [handler_inject_bd_credentials, handler_initialize_sentry]
 verificacao_ip.schedule = every_hour
+
+with Flow("jae: backup dados BillingPay") as backup_billingpay:
+
+    database_name = Parameter(name="database_name")
+
+    env = get_run_env()
+
+    database_config = get_jae_db_config(database_name=database_name)
+
+    timestamp = get_scheduled_timestamp()
+
+    table_info = get_table_info(
+        env=env,
+        database_name=database_name,
+        database_config=database_config,
+        timestamp=timestamp,
+    )
+
+    table_info = get_raw_backup_billingpay(
+        table_info=table_info,
+        database_config=database_config,
+        timestamp=timestamp,
+    )
+
+    table_info = upload_backup_billingpay.map(
+        env=unmapped(env),
+        table_info=table_info,
+        database_name=unmapped(database_name),
+    )
+
+    SET_REDIS_BACKUP = set_redis_backup_billingpay.map(
+        env=unmapped(env),
+        table_info=table_info,
+        timestamp=unmapped(timestamp),
+    )
+
+verificacao_ip.storage = GCS(smtr_constants.GCS_FLOWS_BUCKET.value)
+verificacao_ip.run_config = KubernetesRun(
+    image=smtr_constants.DOCKER_IMAGE.value,
+    labels=[smtr_constants.RJ_SMTR_AGENT_LABEL.value],
+)
+verificacao_ip.state_handlers = [handler_inject_bd_credentials, handler_initialize_sentry]
---
+++ pipelines/capture/jae/utils.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import os
+from datetime import datetime
+from typing import Union
+
+from prefeitura_rio.pipelines_utils.logging import log
+from prefeitura_rio.pipelines_utils.redis_pal import get_redis_client
+
+from pipelines.capture.jae.constants import constants
+from pipelines.constants import constants as smtr_constants
+from pipelines.utils.fs import get_data_folder_path
+from pipelines.utils.utils import convert_timezone
+
+
+def create_billingpay_backup_filepath(
+    table_name: str,
+    database_name: str,
+    partition: str,
+    timestamp: datetime,
+) -> str:
+    return os.path.join(
+        get_data_folder_path(),
+        constants.BACKUP_BILLING_PAY_FOLDER.value,
+        database_name,
+        table_name,
+        partition,
+        f"{timestamp.strftime(smtr_constants.FILENAME_PATTERN.value)}.json",
+    )
+
+
+def get_redis_last_backup(env: str, table_name: str, incremental_type: str) -> Union[int, datetime]:
+    redis_key = f"{env}.backup_jae_billingpay.{table_name}"
+    log(f"Consultando Redis: {redis_key}")
+    redis_client = get_redis_client(host="localhost")
+    content = redis_client.get(redis_key)
+    log(f"content = {content}")
+    if incremental_type == "datetime":
+        last_datetime = (
+            datetime(1900, 1, 1, 0, 0, 0)
+            if content is None
+            else datetime.strptime(
+                content[constants.BACKUP_BILLING_LAST_VALUE_REDIS_KEY.value],
+                smtr_constants.MATERIALIZATION_LAST_RUN_PATTERN.value,
+            )
+        )
+
+        return convert_timezone(timestamp=last_datetime)
+    if incremental_type == "integer":
+        last_id = (
+            0
+            if content is None
+            else int(content[constants.BACKUP_BILLING_LAST_VALUE_REDIS_KEY.value])
+        )
+        return last_id
+
+    raise ValueError(f"Tipo {incremental_type} não encontrado.")
