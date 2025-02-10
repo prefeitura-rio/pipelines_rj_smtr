f7ad0896e4143c5a759128d1126aadc759df0dc9
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-02-10 18:04:31 -0300

    commit inicial

--- queries/models/planejamento/tecnologia_servico.sql
+++ queries/models/planejamento/tecnologia_servico.sql
@@ -4,7 +4,39 @@
     )
 }}
 
+with
+    tecnologia as (
+        select
+            safe_cast(servico as string) as servico,
+            safe_cast(modo as string) as modo,
+            safe_cast(codigo_tecnologia as string) as codigo_tecnologia
+        from {{ source("planejamento_staging", "tecnologia_servico") }}
+    )
 select
-    safe_cast(servico as string) as servico,
-    safe_cast(codigo_tecnologia as string) as codigo_tecnologia
-from {{ source("planejamento_staging", "tecnologia_servico") }}
+    servico,
+    modo,
+    codigo_tecnologia,
+    case
+        when substring(codigo_tecnologia, 4, 1) = "1"
+        then "PADRON"
+        when substring(codigo_tecnologia, 3, 1) = "1"
+        then "BASICO"
+        when substring(codigo_tecnologia, 2, 1) = "1"
+        then "MIDI"
+        when substring(codigo_tecnologia, 1, 1) = "1"
+        then "MINI"
+        else null
+    end as maior_tecnologia_permitida,
+    case
+        when substring(codigo_tecnologia, 1, 1) = "1"
+        then "MINI"
+        when substring(codigo_tecnologia, 2, 1) = "1"
+        then "MIDI"
+        when substring(codigo_tecnologia, 3, 1) = "1"
+        then "BASICO"
+        when substring(codigo_tecnologia, 4, 1) = "1"
+        then "PADRON"
+        else null
+    end as menor_tecnologia_permitida,
+-- feed_start_date -- TODO: puxar essa coluna do gtfs
+from tecnologia
