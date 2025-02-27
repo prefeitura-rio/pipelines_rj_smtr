9c94b86242ed5c0a7ab16205aa53a9e2e26630ef
vtr363 <victor.m.rocha.vr@gmail.com>
2025-02-27 16:27:47 -0300

    corrige variavel

--- queries/models/monitoramento/staging/monitoramento_servico_dia.sql
+++ queries/models/monitoramento/staging/monitoramento_servico_dia.sql
@@ -15,7 +15,7 @@ select
 from {{ ref("sumario_servico_dia_historico") }}
     -- `rj-smtr.dashboard_subsidio_sppo.sumario_servico_dia_historico`
 where
-    data < date("{{ var('data_subsidio_v9_inicio') }}")  -- noqa
+    data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}")  -- noqa
     {% if is_incremental() %}
         and data between date("{{ var('start_date') }}") and date_add(
             date("{{ var('end_date') }}"), interval 1 day
