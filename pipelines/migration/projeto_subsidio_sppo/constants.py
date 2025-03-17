ba30dd2cda888de7a6e891829a3bd0e1ee324e03
vtr363 <victor.m.rocha.vr@gmail.com>
2025-03-17 16:03:24 -0300

    testes da tabela viagem_planejada

--- pipelines/migration/projeto_subsidio_sppo/constants.py
+++ pipelines/migration/projeto_subsidio_sppo/constants.py
@@ -739,6 +739,24 @@ class constants(Enum):  # pylint: disable=c0103
                 "description": "Todas as datas possuem dados"
             },
         },
+        "projeto_subsidio_sppo": {
+            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
+            "dbt_utils.accepted_range": {
+                "description": "Todos os valores da coluna `{column_name}` maiores ou iguais a zero"
+            },
+            "dbt_utils.unique_combination_of_columns__data_id_veiculo__sppo_veiculo_dia": {
+                "description": "Todos os registros são únicos"
+            },
+            "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart__sppo_veiculo_dia": {
+                "description": "Todas as datas possuem dados"
+            },
+            "accepted_values": {
+                "description": "Todos os valores da coluna `{column_name}` são aceitos"
+            },
+            "dbt_expectations.expect_table_aggregation_to_equal_other_table__viagem_planejada": {
+                "description": "Todos os dados de 'tipo_os' correspondem 1:1 entre as tabelas 'subsidio_data_versao_efetiva' e 'viagem_planejada'."  # noqa
+            },
+        },
     }
 
     SUBSIDIO_SPPO_POS_CHECKS_LIST = {
