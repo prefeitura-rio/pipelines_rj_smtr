ca46c412a716b8da394896e6d9c04299b57efcdb
vtr363 <victor.m.rocha.vr@gmail.com>
2025-03-25 10:17:16 -0300

    corrige nome dos testes no flow

--- pipelines/migration/projeto_subsidio_sppo/constants.py
+++ pipelines/migration/projeto_subsidio_sppo/constants.py
@@ -744,10 +744,10 @@ class constants(Enum):  # pylint: disable=c0103
             "dbt_utils.accepted_range": {
                 "description": "Todos os valores da coluna `{column_name}` maiores ou iguais a zero"
             },
-            "dbt_utils.unique_combination_of_columns__data_id_veiculo__sppo_veiculo_dia": {
+            "dbt_utils.unique_combination_of_columns__data_id_veiculo": {
                 "description": "Todos os registros são únicos"
             },
-            "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart__sppo_veiculo_dia": {
+            "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart": {
                 "description": "Todas as datas possuem dados"
             },
             "accepted_values": {
