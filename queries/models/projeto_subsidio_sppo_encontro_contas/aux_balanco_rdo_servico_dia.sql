d9e9c62b2b21bebb18f6d5dc2a7b8850d507a247
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2025-04-03 16:13:29 -0300

    ajusta modelo

--- queries/models/projeto_subsidio_sppo_encontro_contas/aux_balanco_rdo_servico_dia.sql
+++ queries/models/projeto_subsidio_sppo_encontro_contas/aux_balanco_rdo_servico_dia.sql
@@ -18,7 +18,7 @@
 
 WITH
 -- 1. Calcula a receita tarifaria por servico e dia
-rdo AS (
+rdo_raw AS (
   SELECT
     data,
     consorcio,
@@ -39,9 +39,16 @@ rdo AS (
     DATA BETWEEN "{{ var('start_date') }}" AND "{{ var('end_date') }}"
     {# AND DATA NOT IN ("2022-10-02", "2022-10-30", '2023-02-07', '2023-02-08', '2023-02-10', '2023-02-13', '2023-02-17', '2023-02-18', '2023-02-19', '2023-02-20', '2023-02-21', '2023-02-22') #}
     and consorcio in ("Internorte", "Intersul", "Santa Cruz", "Transcarioca")
-    and (length(linha) != 4 and linha not like "2%") --  Remove rodoviarios
+    and (length(IFNULL(REGEXP_EXTRACT(linha, r"[0-9]+"), "")) != 4 and IFNULL(REGEXP_EXTRACT(linha, r"[0-9]+"), "") not like "2%") --  Remove rodoviarios
   group by 1,2,3,4,5,6
 ),
+rdo AS (
+  SELECT
+    *
+  from
+    rdo_raw
+  where
+  receita_tarifaria_aferida != 0),
 -- Remove servicos nao subsidiados
 sumario_dia AS (
   SELECT
