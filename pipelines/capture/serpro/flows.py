cc4a7557299d4d7906cbf0ae8c6437731619093e
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-04-03 22:30:51 -0300

    altera label

--- pipelines/capture/serpro/flows.py
+++ pipelines/capture/serpro/flows.py
@@ -15,13 +15,13 @@ CAPTURA_SERPRO = create_default_capture_flow(
     flow_name="SMTR: SERPRO - Captura",
     source=constants.AUTUACAO_SOURCE.value,
     create_extractor_task=create_serpro_extractor,
-    agent_label=smtr_constants.RJ_SMTR_DEV_AGENT_LABEL.value,
+    agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     generate_schedule=False,
 )
 
 CAPTURA_SERPRO.run_config = KubernetesRun(
     image=smtr_constants.DOCKER_IMAGE_FEDORA.value,
-    labels=[smtr_constants.RJ_SMTR_DEV_AGENT_LABEL.value],
+    labels=[smtr_constants.RJ_SMTR_AGENT_LABEL.value],
 )
 CAPTURA_SERPRO.state_handlers = [
     handler_setup_serpro,
