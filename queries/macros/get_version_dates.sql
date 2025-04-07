9a38d26cf218e01a6cd6155092b216f312d9de74
vtr363 <victor.m.rocha.vr@gmail.com>
2025-04-07 15:21:08 -0300

    remove comentario

--- queries/macros/get_version_dates.sql
+++ queries/macros/get_version_dates.sql
@@ -1,5 +1,4 @@
 {% macro get_version_dates(modelo) %}
-    -- juntar com get_license_datess
     select
         min(date(data_versao)) as min_data,
         coalesce(max(date(data_versao)), min(date(data_versao))) as max_data
