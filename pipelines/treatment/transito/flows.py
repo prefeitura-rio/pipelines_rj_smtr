bd451024561f9a4c1610f2d06dd16dd4780afd49
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-04-09 17:41:06 -0300

    altera flow_name

--- pipelines/treatment/transito/flows.py
+++ pipelines/treatment/transito/flows.py
@@ -10,7 +10,7 @@ from pipelines.treatment.templates.flows import create_default_materialization_f
 from pipelines.treatment.transito.constants import constants
 
 TRANSITO_AUTUACAO_MATERIALIZACAO = create_default_materialization_flow(
-    flow_name="autuacao - materializacao",
+    flow_name="transito_autuacao - materializacao",
     selector=constants.TRANSITO_AUTUACAO_SELECTOR.value,
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     generate_schedule=False,
