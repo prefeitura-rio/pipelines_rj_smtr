e2dfa38d39fbc1f495234f6bddf1d826787d7994
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-04-10 14:26:56 -0300

    add filtro incremental

--- queries/models/infraestrutura/custo_gcp.sql
+++ queries/models/infraestrutura/custo_gcp.sql
@@ -1,3 +1,41 @@
+{{
+    config(
+        materialized="incremental",
+        partition_by={"field": "data", "data_type": "date", "granularity": "day"},
+        incremental_strategy="insert_overwrite",
+    )
+}}
+
+{% set incremental_filter %}
+  date(_PARTITIONTIME) between date("{{var('date_range_start')}}") and date("{{var('date_range_end')}}")
+{% endset %}
+
+{% set billing_staging = source(
+    "cloudcosts",
+    "gcp_billing_export_resource_v1_010693_B1EC8D_C0D4DF",
+) %}
+
+{% if execute %}
+    {% if is_incremental() %}
+        {% set partitions_query %}
+            select distinct concat("'", date(usage_start_time), "'") as data
+            from {{ billing_staging }},
+            where
+                {{ incremental_filter }}
+
+        {% endset %}
+
+        {% set partitions = run_query(partitions_query).columns[0].values() %}
+
+    {% endif %}
+{% endif %}
+
+with
+    billing as (
+        select *
+        from {{ billing_staging }}
+        {% if is_incremental() %} where {{ incremental_filter }} {% endif %}
+    )
 select
     date(usage_start_time) as data,
     service.description as tipo_servico,
@@ -9,5 +47,5 @@ select
     usage.unit as unidade_uso,
     usage.amount_in_pricing_units as uso_quantidade_em_unidades_preco,
     usage.pricing_unit as unidade_de_preco_de_uso,
-from {{ source("cloudcosts", "gcp_billing_export_resource_v1_010693_B1EC8D_C0D4DF") }}
+from billing
 where usage_start_time >= "2024-10-01"
