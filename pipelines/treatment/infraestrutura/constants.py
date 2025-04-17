d4de81381fdf2ac235c33292516053d684b37fa3
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-04-17 16:42:34 -0300

    remove delay

--- pipelines/treatment/infraestrutura/constants.py
+++ pipelines/treatment/infraestrutura/constants.py
@@ -19,5 +19,4 @@ class constants(Enum):  # pylint: disable=c0103
         name="infraestrutura",
         schedule_cron=create_daily_cron(hour=4),
         initial_datetime=datetime(2025, 3, 26, 0, 0, 0),
-        incremental_delay_hours=24,
     )
