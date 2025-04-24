2b5d2291ba079c2ee18f985c20fca02a3dfc282f
vtr363 <victor.m.rocha.vr@gmail.com>
2025-04-24 19:05:33 -0300

    corrige modelo agente de verão

--- queries/models/veiculo/staging/sppo_registro_agente_verao_staging.sql
+++ queries/models/veiculo/staging/sppo_registro_agente_verao_staging.sql
@@ -1,18 +1,24 @@
-{{ config(
-        materialized="view",
-        alias='sppo_registro_agente_verao'
-)
-}}
+{{ config(materialized="view", alias="sppo_registro_agente_verao") }}
 
-SELECT
-  SAFE_CAST(PARSE_DATETIME("%d/%m/%Y %H:%M:%S", datetime_registro) AS DATE) AS data,
-  SAFE_CAST(PARSE_DATETIME("%d/%m/%Y %H:%M:%S", datetime_registro) AS DATETIME) AS datetime_registro,
-  SHA256(PARSE_DATETIME("%d/%m/%Y %H:%M:%S", datetime_registro) || "_" || SAFE_CAST(email AS STRING)) AS id_registro,
-  SAFE_CAST(JSON_VALUE(content,'$.id_veiculo') AS STRING) AS id_veiculo,
-  SAFE_CAST(JSON_VALUE(content,'$.servico') AS STRING) AS servico,
-  SAFE_CAST(JSON_VALUE(content,'$.link_foto') AS STRING) AS link_foto,
-  SAFE_CAST(JSON_VALUE(content,'$.validacao') AS BOOL) AS validacao,
-  SAFE_CAST(DATETIME(TIMESTAMP_TRUNC(TIMESTAMP(timestamp_captura), SECOND), "America/Sao_Paulo" ) AS DATETIME) AS datetime_captura,
-  "{{ var("version") }}" AS versao
-FROM
-  {{ source('veiculo_staging','sppo_registro_agente_verao') }}
+select
+    safe_cast(parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) as date) as data,
+    safe_cast(
+        parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) as datetime
+    ) as datetime_registro,
+    -- fmt: off
+    sha256(
+        parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) || "_" || safe_cast(email as string)
+    ) as id_registro,
+    -- fmt: on
+    safe_cast(json_value(content, '$.id_veiculo') as string) as id_veiculo,
+    safe_cast(json_value(content, '$.servico') as string) as servico,
+    safe_cast(json_value(content, '$.link_foto') as string) as link_foto,
+    safe_cast(json_value(content, '$.validacao') as bool) as validacao,
+    date(data) as data_captura,
+    safe_cast(
+        datetime(
+            timestamp_trunc(timestamp(timestamp_captura), second), "America/Sao_Paulo"
+        ) as datetime
+    ) as datetime_captura,
+    "{{ var('version') }}" as versao
+from {{ source("veiculo_staging", "sppo_registro_agente_verao") }}
