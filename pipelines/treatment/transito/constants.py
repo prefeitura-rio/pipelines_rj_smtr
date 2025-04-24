c967faf2159bf7c6a2e5f11cfdf311425b60a2ba
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-04-24 17:19:42 -0300

    altera schedule e add wait

--- pipelines/treatment/transito/constants.py
+++ pipelines/treatment/transito/constants.py
@@ -6,7 +6,7 @@ Valores constantes para materialização dos dados de trânsito
 from datetime import datetime
 from enum import Enum
 
-from pipelines.schedules import create_minute_cron
+from pipelines.schedules import create_daily_cron
 from pipelines.treatment.templates.utils import DBTSelector
 
 
@@ -17,8 +17,8 @@ class constants(Enum):  # pylint: disable=c0103
 
     TRANSITO_AUTUACAO_SELECTOR = DBTSelector(
         name="transito_autuacao",
-        schedule_cron=create_minute_cron(minute=30),
-        initial_datetime=datetime(2025, 3, 29, 0, 30, 0),
+        schedule_cron=create_daily_cron(hour=7, minute=40),
+        initial_datetime=datetime(2025, 3, 29, 0, 0, 0),
     )
 
     SNAPSHOT_TRANSITO_SELECTOR = DBTSelector(
