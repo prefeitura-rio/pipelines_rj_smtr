b70d4e2fc7e2acbd598655bd9f480c024852449e
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-05-08 14:12:56 -0300

    add filtro particao

--- queries/snapshots/projeto_subsidio_sppo/snapshot_viagem_completa.sql
+++ queries/snapshots/projeto_subsidio_sppo/snapshot_viagem_completa.sql
@@ -2,7 +2,7 @@
 
     {{
         config(
-            target_schema="projeto_subsidio_sppo",
+            target_schema="projeto_subsidio_sppo_staging",
             unique_key="id_viagem",
             strategy="timestamp",
             updated_at="timestamp_ultima_atualizacao",
@@ -11,12 +11,42 @@
         )
     }}
 
+    {% set viagem_completa = ref("viagem_completa") %}
+    {% set target_relation = adapter.get_relation(
+        database=this.database, schema=this.schema, identifier=this.name
+    ) %}
+    {% set table_exists = target_relation is not none %}
+
+    {% if execute and table_exists %}
+        {% set modified_partitions_query %}
+            select concat("'", parse_date("%Y%m%d", partition_id), "'") as data_particao
+            from `rj-smtr.{{ viagem_completa.schema }}.INFORMATION_SCHEMA.PARTITIONS`
+            where
+                table_name = "{{ viagem_completa.identifier }}"
+                and partition_id != "__NULL__"
+                and datetime(last_modified_time, "America/Sao_Paulo")
+                >= datetime_sub(current_datetime("America/Sao_Paulo"), interval 10 day)
+        {% endset %}
+
+        {% set modified_partitions = (
+            run_query(modified_partitions_query).columns[0].values()
+        ) %}
+
+    {% endif %}
+
     select
         * except (versao_modelo),
         timestamp(
             datetime_ultima_atualizacao, "America/Sao_Paulo"
         ) as timestamp_ultima_atualizacao
-    from {{ ref("viagem_completa") }}
+    from {{ viagem_completa }}
+    {% if table_exists %}
+        where
+            {% if modified_partitions | length > 0 %}
+                data in ({{ modified_partitions | join(", ") }})
+            {% else %} data = "2000-01-01"
+            {% endif %}
+    {% endif %}
     qualify row_number() over (partition by id_viagem) = 1
 
 {% endsnapshot %}
--- queries/snapshots/projeto_subsidio_sppo/snapshot_viagem_planejada.sql
+++ queries/snapshots/projeto_subsidio_sppo/snapshot_viagem_planejada.sql
@@ -11,11 +11,41 @@
         )
     }}
 
+    {% set viagem_planejada = ref("viagem_planejada") %}
+    {% set target_relation = adapter.get_relation(
+        database=this.database, schema=this.schema, identifier=this.name
+    ) %}
+    {% set table_exists = target_relation is not none %}
+
+    {% if execute and table_exists %}
+        {% set modified_partitions_query %}
+            select concat("'", parse_date("%Y%m%d", partition_id), "'") as data_particao
+            from `rj-smtr.{{ viagem_planejada.schema }}.INFORMATION_SCHEMA.PARTITIONS`
+            where
+                table_name = "{{ viagem_planejada.identifier }}"
+                and partition_id != "__NULL__"
+                and datetime(last_modified_time, "America/Sao_Paulo")
+                >= datetime_sub(current_datetime("America/Sao_Paulo"), interval 10 day)
+        {% endset %}
+
+        {% set modified_partitions = (
+            run_query(modified_partitions_query).columns[0].values()
+        ) %}
+
+    {% endif %}
+
     select
         *,
         timestamp(
             datetime_ultima_atualizacao, "America/Sao_Paulo"
         ) as timestamp_ultima_atualizacao
     from {{ ref("viagem_planejada") }}
-    where distancia_total_planejada != 0
+    where
+        distancia_total_planejada != 0
+        {% if table_exists %}
+            and {% if modified_partitions | length > 0 %}
+                data in ({{ modified_partitions | join(", ") }})
+            {% else %} data = "2000-01-01"
+            {% endif %}
+        {% endif %}
 {% endsnapshot %}
