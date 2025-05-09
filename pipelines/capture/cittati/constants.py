5ce026d08db0d35e498bd9b8524b13b14dd4df87
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-05-09 20:41:36 -0300

    add pretreat_funcs

--- pipelines/capture/cittati/constants.py
+++ pipelines/capture/cittati/constants.py
@@ -6,6 +6,10 @@ Valores constantes para captura de dados da CITTATI
 from datetime import datetime
 from enum import Enum
 
+from pipelines.capture.cittati.utils import (
+    pretreat_cittati_realocacao,
+    pretreat_cittati_registros,
+)
 from pipelines.schedules import create_minute_cron
 from pipelines.utils.gcp.bigquery import SourceTable
 
@@ -18,6 +22,7 @@ class constants(Enum):  # pylint: disable=c0103
     CITTATI_SOURCE_NAME = "cittati"
     CITTATI_SECRET_PATH = "cittati_api"
     CITTATI_BASE_URL = "https://servicos.cittati.com.br/WSIntegracaoCittati/SMTR/v2"
+    CITTATI_CAPTURE_DELAY = 60
 
     CITTATI_REGISTROS_TABLE_ID = "registros"
     CITTATI_REGISTROS_SOURCE = SourceTable(
@@ -26,6 +31,7 @@ class constants(Enum):  # pylint: disable=c0103
         first_timestamp=datetime(2025, 5, 9, 0, 0, 0),
         schedule_cron=create_minute_cron(),
         primary_keys=["id_veiculo", "datetime"],
+        pretreat_funcs=[pretreat_cittati_registros],
     )
 
     CITTATI_REALOCACAO_TABLE_ID = "realocacao"
@@ -35,4 +41,5 @@ class constants(Enum):  # pylint: disable=c0103
         first_timestamp=datetime(2025, 5, 9, 0, 0, 0),
         schedule_cron=create_minute_cron(minute=10),
         primary_keys=["id_veiculo", "datetime_processamento"],
+        pretreat_funcs=[pretreat_cittati_realocacao],
     )
---
+++ pipelines/capture/cittati/utils.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+import traceback
+from datetime import timedelta
+
+import pandas as pd
+
+from pipelines.capture.cittati.constants import constants
+from pipelines.constants import constants as smtr_constants
+from pipelines.utils.utils import log
+
+
+def pretreat_cittati_registros(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Basic data treatment for bus gps data. Converts unix time to datetime,
+    and apply filtering to stale data that may populate the API response.
+
+    Args:
+        data (pd.DataFrame): DataFrame with the data from CITTATI API
+
+    Returns:
+        pd.DataFrame: Treated dataframe
+    """
+
+    timezone = smtr_constants.TIMEZONE.value
+
+    try:
+        timestamp_cols = ["datetime", "datetime_envio", "datetime_servidor"]
+
+        for col in timestamp_cols:
+            if col in data.columns:
+                log(f"Before converting, {col} is: \n{data[col].head()}")
+
+                data[col] = (
+                    pd.to_datetime(data[col].astype(float), unit="ms")
+                    .dt.tz_localize(tz="UTC")
+                    .dt.tz_convert(timezone)
+                )
+
+                log(f"After converting the timezone, {col} is: \n{data[col].head()}")
+
+        log(f"Shape before filtering: {data.shape}")
+
+        filter_col = "datetime_envio"
+        time_delay = constants.CITTATI_CAPTURE_DELAY.value
+
+        mask = (data[filter_col] - data["datetime"]).apply(
+            lambda x: timedelta(seconds=-20) <= x <= timedelta(minutes=time_delay)
+        )
+
+        cols = [
+            "id_veiculo",
+            "servico",
+            "sentido",
+            "latitude",
+            "longitude",
+            "datetime",
+            "velocidade",
+            "direcao",
+            "route_id",
+            "trip_id",
+            "shape_id",
+            "datetime_envio",
+            "datetime_servidor",
+        ]
+
+        filtered_data = data[mask][cols]
+        filtered_data = filtered_data.drop_duplicates(
+            ["id_veiculo", "latitude", "longitude", "datetime", "datetime_servidor"]
+        )
+
+        log(f"Shape after filtering: {filtered_data.shape}")
+
+        if filtered_data.shape[0] == 0:
+            error = ValueError("After filtering, the dataframe is empty!")
+            log(f"[CATCHED] Task failed with error: \n{error}", level="error")
+            return pd.DataFrame()
+
+        return filtered_data
+
+    except Exception:
+        error = traceback.format_exc()
+        log(f"[CATCHED] Task failed with error: \n{error}", level="error")
+        return pd.DataFrame()
+
+
+def pretreat_cittati_realocacao(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Basic data treatment for bus gps relocation data. Converts unix time to datetime,
+    and apply filtering to stale data that may populate the API response.
+
+    Args:
+        data (pd.DataFrame): DataFrame with the data from CITTATI API
+
+    Returns:
+        pd.DataFrame: Treated dataframe
+    """
+
+    try:
+        dt_cols = [
+            "datetime_entrada",
+            "datetime_operacao",
+            "datetime_saida",
+            "datetime_processamento",
+        ]
+
+        for col in dt_cols:
+            if col in data.columns:
+                log(f"Converting column {col}")
+                log(f"Data received to treat: \n{data[col]}")
+
+                temp_time_col_sec = pd.to_datetime(
+                    data[col], format="%Y-%m-%dT%H:%M:%S", errors="coerce"
+                ).dt.tz_localize(tz=smtr_constants.TIMEZONE.value)
+
+                temp_time_col_msec = pd.to_datetime(
+                    data[col], format="%Y-%m-%dT%H:%M:%S.%f", errors="coerce"
+                ).dt.tz_localize(tz=smtr_constants.TIMEZONE.value)
+
+                data[col] = temp_time_col_sec.fillna(temp_time_col_msec).dt.strftime(
+                    "%Y-%m-%d %H:%M:%S%z"
+                )
+
+                if data[col].isna().sum() > 0:
+                    error = ValueError("After treating, there is null values!")
+                    log(f"[CATCHED] Task failed with error: \n{error}", level="error")
+
+                log(f"Treated data: \n{data[col]}")
+
+        if "datetime_saida" in data.columns:
+            data.loc[data.datetime_saida == "1971-01-01 00:00:00-0300", "datetime_saida"] = ""
+
+        return data.drop_duplicates()
+
+    except Exception:
+        error = traceback.format_exc()
+        log(f"[CATCHED] Task failed with error: \n{error}", level="error")
+        return pd.DataFrame()
