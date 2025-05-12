933f779cf85b960f456429b5db903be18c23456b
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-05-12 12:15:35 -0300

    altera para funções genéricas

--- pipelines/capture/cittati/constants.py
+++ pipelines/capture/cittati/constants.py
@@ -6,12 +6,9 @@ Valores constantes para captura de dados da CITTATI
 from datetime import datetime
 from enum import Enum
 
-from pipelines.capture.cittati.utils import (
-    pretreat_cittati_realocacao,
-    pretreat_cittati_registros,
-)
 from pipelines.schedules import create_minute_cron
 from pipelines.utils.gcp.bigquery import SourceTable
+from pipelines.utils.pretreatment import pretreat_gps_realocacao, pretreat_gps_registros
 
 
 class constants(Enum):  # pylint: disable=c0103
@@ -31,7 +28,7 @@ class constants(Enum):  # pylint: disable=c0103
         first_timestamp=datetime(2025, 5, 9, 0, 0, 0),
         schedule_cron=create_minute_cron(),
         primary_keys=["id_veiculo", "datetime"],
-        pretreat_funcs=[pretreat_cittati_registros],
+        pretreat_funcs=[pretreat_gps_registros],
     )
 
     CITTATI_REALOCACAO_TABLE_ID = "realocacao"
@@ -41,5 +38,5 @@ class constants(Enum):  # pylint: disable=c0103
         first_timestamp=datetime(2025, 5, 9, 0, 0, 0),
         schedule_cron=create_minute_cron(minute=10),
         primary_keys=["id_veiculo", "datetime_processamento"],
-        pretreat_funcs=[pretreat_cittati_realocacao],
+        pretreat_funcs=[pretreat_gps_realocacao],
     )
