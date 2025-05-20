c65b5da5b108bf66904bff22946a6b2d1d46eb64
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-05-20 19:32:17 -0300

    add tratamento no source

--- queries/models/monitoramento/staging/aux_gps.sql
+++ queries/models/monitoramento/staging/aux_gps.sql
@@ -1,58 +1,96 @@
 {{ config(alias=this.name ~ "_" ~ var("fonte_gps")) }}
 
+with
+    source_data as (
+        select
+            data,
+            safe_cast(hora as int64) hora,
+            safe_cast(
+                datetime(
+                    timestamp(safe_cast(json_value(content, '$.datetime') as string)),
+                    "America/Sao_Paulo"
+                ) as datetime
+            ) datetime_gps,
+            safe_cast(id_veiculo as string) id_veiculo,
+            concat(
+                ifnull(
+                    regexp_extract(
+                        safe_cast(json_value(content, '$.servico') as string), r'[A-Z]+'
+                    ),
+                    ""
+                ),
+                ifnull(
+                    regexp_extract(
+                        safe_cast(json_value(content, '$.servico') as string), r'[0-9]+'
+                    ),
+                    ""
+                )
+            ) as servico,
+            safe_cast(json_value(content, '$.sentido') as string) sentido,
+            safe_cast(
+                replace(json_value(content, '$.latitude'), ',', '.') as float64
+            ) latitude,
+            safe_cast(
+                replace(json_value(content, '$.longitude'), ',', '.') as float64
+            ) longitude,
+            safe_cast(json_value(content, '$.velocidade') as int64) velocidade,
+            safe_cast(json_value(content, '$.direcao') as numeric) direcao,
+            safe_cast(json_value(content, '$.route_id') as string) route_id,
+            safe_cast(json_value(content, '$.trip_id') as string) trip_id,
+            safe_cast(json_value(content, '$.shape_id') as string) shape_id,
+            safe_cast(
+                datetime(
+                    timestamp(
+                        safe_cast(json_value(content, '$.datetime_envio') as string)
+                    ),
+                    "America/Sao_Paulo"
+                ) as datetime
+            ) datetime_envio,
+            safe_cast(
+                datetime(
+                    timestamp(
+                        safe_cast(json_value(content, '$.datetime_servidor') as string)
+                    ),
+                    "America/Sao_Paulo"
+                ) as datetime
+            ) datetime_servidor,
+            safe_cast(
+                datetime(timestamp(timestamp_captura), "America/Sao_Paulo") as datetime
+            ) datetime_captura
+        from
+            {{
+                source(
+                    "source_" ~ var("fonte_gps"),
+                    "registros",
+                )
+            }}
+    ),
+    filtered_data as (
+        select *
+        from source_data
+        where
+            datetime_diff(datetime_envio, datetime_gps, second) >= -20
+            and datetime_diff(datetime_envio, datetime_gps, minute) <= 60
+    )
 select
-    data,
-    safe_cast(hora as int64) hora,
-    safe_cast(
-        datetime(
-            timestamp(safe_cast(json_value(content, '$.datetime') as string)),
-            "America/Sao_Paulo"
-        ) as datetime
-    ) datetime_gps,
-    safe_cast(id_veiculo as string) id_veiculo,
-    concat(
-        ifnull(
-            regexp_extract(
-                safe_cast(json_value(content, '$.servico') as string), r'[A-Z]+'
-            ),
-            ""
-        ),
-        ifnull(
-            regexp_extract(
-                safe_cast(json_value(content, '$.servico') as string), r'[0-9]+'
-            ),
-            ""
-        )
-    ) as servico,
-    safe_cast(json_value(content, '$.sentido') as string) sentido,
-    safe_cast(replace(json_value(content, '$.latitude'), ',', '.') as float64) latitude,
-    safe_cast(
-        replace(json_value(content, '$.longitude'), ',', '.') as float64
-    ) longitude,
-    safe_cast(json_value(content, '$.velocidade') as int64) velocidade,
-    safe_cast(json_value(content, '$.direcao') as numeric) direcao,
-    safe_cast(json_value(content, '$.route_id') as string) route_id,
-    safe_cast(json_value(content, '$.trip_id') as string) trip_id,
-    safe_cast(json_value(content, '$.shape_id') as string) shape_id,
-    safe_cast(
-        datetime(
-            timestamp(safe_cast(json_value(content, '$.datetime_envio') as string)),
-            "America/Sao_Paulo"
-        ) as datetime
-    ) datetime_envio,
-    safe_cast(
-        datetime(
-            timestamp(safe_cast(json_value(content, '$.datetime_servidor') as string)),
-            "America/Sao_Paulo"
-        ) as datetime
-    ) datetime_servidor,
-    safe_cast(
-        datetime(timestamp(timestamp_captura), "America/Sao_Paulo") as datetime
-    ) datetime_captura
-from
-    {{
-        source(
-            "source_" ~ var("fonte_gps"),
-            "registros",
-        )
-    }}
+    id_veiculo,
+    servico,
+    sentido,
+    latitude,
+    longitude,
+    datetime_gps,
+    velocidade,
+    direcao,
+    route_id,
+    trip_id,
+    shape_id,
+    datetime_envio,
+    datetime_servidor,
+    datetime_captura
+from filtered_data
+qualify
+    row_number() over (
+        partition by id_veiculo, latitude, longitude, datetime_gps, datetime_captura
+        order by datetime_captura desc
+    )
+    = 1
--- queries/models/monitoramento/staging/aux_realocacao.sql
+++ queries/models/monitoramento/staging/aux_realocacao.sql
@@ -1,53 +1,71 @@
 {{ config(alias=this.name ~ "_" ~ var("fonte_gps")) }}
 
-select
+with source_data as (
+    select
+        data,
+        safe_cast(hora as int64) hora,
+        safe_cast(
+            datetime(
+                timestamp(safe_cast(json_value(content, '$.datetime_operacao') as string)),
+                "America/Sao_Paulo"
+            ) as datetime
+        ) datetime_operacao,
+        safe_cast(id_veiculo as string) id_veiculo,
+        concat(
+            ifnull(
+                regexp_extract(
+                    safe_cast(json_value(content, '$.servico') as string), r'[A-Z]+'
+                ),
+                ""
+            ),
+            ifnull(
+                regexp_extract(
+                    safe_cast(json_value(content, '$.servico') as string), r'[0-9]+'
+                ),
+                ""
+            )
+        ) as servico,
+        safe_cast(
+            datetime(
+                timestamp(safe_cast(json_value(content, '$.datetime_entrada') as string)),
+                "America/Sao_Paulo"
+            ) as datetime
+        ) as datetime_entrada,
+        case
+            when safe_cast(json_value(content, '$.datetime_saida') as string) = '1971-01-01 00:00:00-0300'
+            then null
+            else safe_cast(
+                datetime(
+                    timestamp(
+                        nullif(safe_cast(json_value(content, '$.datetime_saida') as string), "")
+                    ),
+                    "America/Sao_Paulo"
+                ) as datetime
+            )
+        end as datetime_saida,
+        safe_cast(
+            datetime(timestamp(datetime_processamento), "America/Sao_Paulo") as datetime
+        ) as datetime_processamento,
+        safe_cast(
+            datetime(timestamp(timestamp_captura), "America/Sao_Paulo") as datetime
+        ) as datetime_captura
+    from
+        {{
+            source(
+                "source_" ~ var("fonte_gps"),
+                "realocacao",
+            )
+        }}
+)
+select distinct
     data,
-    safe_cast(hora as int64) hora,
-    safe_cast(
-        datetime(
-            timestamp(safe_cast(json_value(content, '$.datetime_operacao') as string)),
-            "America/Sao_Paulo"
-        ) as datetime
-    ) datetime_operacao,
-    safe_cast(id_veiculo as string) id_veiculo,
-    concat(
-        ifnull(
-            regexp_extract(
-                safe_cast(json_value(content, '$.servico') as string), r'[A-Z]+'
-            ),
-            ""
-        ),
-        ifnull(
-            regexp_extract(
-                safe_cast(json_value(content, '$.servico') as string), r'[0-9]+'
-            ),
-            ""
-        )
-    ) as servico,
-    safe_cast(
-        datetime(
-            timestamp(safe_cast(json_value(content, '$.datetime_entrada') as string)),
-            "America/Sao_Paulo"
-        ) as datetime
-    ) as datetime_entrada,
-    safe_cast(
-        datetime(
-            timestamp(
-                nullif(safe_cast(json_value(content, '$.datetime_saida') as string), "")
-            ),
-            "America/Sao_Paulo"
-        ) as datetime
-    ) as datetime_saida,
-    safe_cast(
-        datetime(timestamp(datetime_processamento), "America/Sao_Paulo") as datetime
-    ) as datetime_processamento,
-    safe_cast(
-        datetime(timestamp(timestamp_captura), "America/Sao_Paulo") as datetime
-    ) as datetime_captura
+    hora,
+    datetime_operacao,
+    id_veiculo,
+    servico,
+    datetime_entrada,
+    datetime_saida,
+    datetime_processamento,
+    datetime_captura
 from
-    {{
-        source(
-            "source_" ~ var("fonte_gps"),
-            "realocacao",
-        )
-    }}
+    source_data
