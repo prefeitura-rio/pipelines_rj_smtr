2607701542f70076253f590f7399a942698bd8ac
vtr363 <victor.m.rocha.vr@gmail.com>
2025-05-28 18:19:50 -0300

    corrige teste de teto km

--- queries/macros/test_teto_viagens.sql
+++ queries/macros/test_teto_viagens.sql
@@ -1,64 +1,67 @@
 {% test teto_viagens(model) -%}
-with
-    os as (
-        select
-            data,
-            servico,
-            case
-                when fh.tipo_os != 'Regular'
-                then concat(concat(fh.tipo_dia, ' - '), fh.tipo_os)
-                else fh.tipo_dia
-            end as tipo_dia,
-            safe_cast(
-                concat(concat(data, 'T'), faixa_horaria_inicio) as datetime
-            ) as faixa_horaria_inicio,
-            partidas,
-        from {{ ref('ordem_servico_faixa_horaria') }} fh
-        -- from `rj-smtr.planejamento.ordem_servico_faixa_horaria` fh
-        right join
-            {{ ref('subsidio_data_versao_efetiva') }} s
-            -- `rj-smtr.projeto_subsidio_sppo.subsidio_data_versao_efetiva` s
-            on s.feed_start_date = fh.feed_start_date
-            and fh.tipo_dia = s.tipo_dia
-            and coalesce(s.tipo_os, 'Regular') = fh.tipo_os
-        where
-            data between DATE("{{ var('date_range_start') }}") and DATE("{{ var('date_range_end') }}")
-            and partidas > 0
-            and quilometragem > 0
-    ),
-    viagens_remuneradas as (
-        select
-            r.data,
-            r.servico,
-            datetime(faixa_horaria_inicio) as faixa_horaria_inicio,
-            indicador_viagem_dentro_limite
-        from
-            {{ model }} r
+    with
+        planejado as (
+            select distinct
+                data,
+                servico,
+                faixa_horaria_inicio,
+                faixa_horaria_fim,
+                partidas_total_planejada,
+            from {{ ref("viagem_planejada") }}
+            -- from `rj-smtr.projeto_subsidio_sppo.viagem_planejada`
+            where
+                data between date("{{ var('date_range_start') }}") and date(
+                    "{{ var('date_range_end') }}"
+                )
+                and (distancia_total_planejada > 0 or distancia_total_planejada is null)
+                and (id_tipo_trajeto = 0 or id_tipo_trajeto is null)
+                and data >= date('{{ var("DATA_SUBSIDIO_V3A_INICIO") }}')
+        ),
+        viagens_remuneradas as (
+            select
+                r.data,
+                r.servico,
+                datetime(faixa_horaria_inicio) as faixa_horaria_inicio,
+                indicador_viagem_dentro_limite
+            from {{ model }} r
             -- `rj-smtr-dev.abr_reprocessamento__dashboard_subsidio_sppo.viagens_remuneradas` r
-        left join
-            {{ ref('viagens_completa') }} c
-            -- `rj-smtr.projeto_subsidio_sppo.viagem_completa` c
-            using (data, id_viagem)
-        left join
-            {{ ref('viagem_planejada') }}  p
-            -- `rj-smtr.projeto_subsidio_sppo.viagem_planejada` p
-            on p.data = c.data
-            and r.servico = p.servico
-            and c.datetime_partida
-            between datetime(faixa_horaria_inicio) and datetime(faixa_horaria_fim)
-        where r.data between DATE("{{ var('date_range_start') }}") and DATE("{{ var('date_range_end') }}")
-    ),
-    viagens as (
-        select
-            * except (indicador_viagem_dentro_limite),
+            left join
+                {{ ref("viagens_completa") }} c
+                -- `rj-smtr.projeto_subsidio_sppo.viagem_completa` c
+                using (data, id_viagem)
+            left join
+                {{ ref("viagem_planejada") }} p
+                -- `rj-smtr.projeto_subsidio_sppo.viagem_planejada` p
+                on p.data = c.data
+                and r.servico = p.servico
+                and c.datetime_partida
+                between datetime(faixa_horaria_inicio) and datetime(faixa_horaria_fim)
+            where
+                r.data between date("{{ var('date_range_start') }}") and date(
+                    "{{ var('date_range_end') }}"
+                )
+        ),
+        viagens as (
+            select
+                * except (indicador_viagem_dentro_limite),
 
-            countif(indicador_viagem_dentro_limite is true) as viagens_dentro_limite,
-            count(*) as viagens_total
-        from viagens_remuneradas
-        group by 1, 2, 3
-    )
-select *
-from viagens
-left join os using (data, servico, faixa_horaria_inicio)
-where viagens_total >= partidas and viagens_dentro_limite < partidas
+                countif(
+                    indicador_viagem_dentro_limite is true
+                ) as viagens_dentro_limite,
+                count(*) as viagens_total
+            from viagens_remuneradas
+            group by 1, 2, 3
+        )
+    select *
+    from viagens
+    left join planejado using (data, servico, faixa_horaria_inicio)
+    where
+        (
+            viagens_total >= partidas_total_planejada
+            and viagens_dentro_limite < partidas_total_planejada
+        )
+        or (
+            viagens_total <= partidas_total_planejada
+            and viagens_dentro_limite < viagens_total
+        )
 {% endtest %}
