78e9a7d56d47d7965349357d8c0e7ccdd2736b95
vtr363 <victor.m.rocha.vr@gmail.com>
2025-06-11 16:55:51 -0300

    corrige formatacao ordem_servico_trips_shapes

--- queries/models/gtfs/ordem_servico_trips_shapes_gtfs.sql
+++ queries/models/gtfs/ordem_servico_trips_shapes_gtfs.sql
@@ -9,105 +9,166 @@
     )
 }}
 
-WITH
-  -- 1. Busca os shapes em formato geográfico
-  shapes AS (
-    SELECT
-      *
-    FROM
-      {{ ref("shapes_geom_gtfs") }}
-    {% if is_incremental() -%}
-    WHERE
-      feed_start_date = '{{ var("data_versao_gtfs") }}'
-    {% endif -%}
-  ),
-  -- 2. Trata a OS, inclui trip_ids e ajusta nomes das colunas
-  ordem_servico_tratada AS (
-  SELECT
-    *
-  FROM
-    (
-      (
-      SELECT
-        o.feed_version,
-        o.feed_start_date,
-        o.feed_end_date,
-        o.tipo_os,
-        o.tipo_dia,
-        servico,
-        vista,
-        consorcio,
-        sentido,
-        distancia_planejada,
-        distancia_total_planejada,
-        inicio_periodo,
-        fim_periodo,
-        trip_id,
-        shape_id,
-        indicador_trajeto_alternativo
-      FROM
-        {{ ref("ordem_servico_sentido_atualizado_aux_gtfs") }} AS o
-      LEFT JOIN
-        {{ ref("trips_filtrada_aux_gtfs") }} AS t
-      ON
-        t.feed_version = o.feed_version
-        AND o.servico = t.trip_short_name
-        AND
-          ((o.tipo_dia = t.tipo_dia AND o.tipo_os NOT IN ("CNU", "Enem", "Eleição"))
-          OR (o.tipo_dia = "Ponto Facultativo" AND t.tipo_dia = "Dia Útil" AND o.tipo_os != "CNU")
-          OR (o.feed_start_date = "2024-08-16" AND o.tipo_os = "CNU" AND o.tipo_dia = "Domingo" AND t.tipo_dia = "Sabado") -- Domingo CNU
-          OR (o.feed_start_date IN ("2024-09-29", "2024-11-06") AND o.tipo_os IN ("Enem", "Eleição") AND o.tipo_dia = "Domingo" AND t.tipo_dia = "Sabado")) -- Domingo Enem/Eleição
-        AND
-          ((o.sentido IN ("I", "C") AND t.direction_id = "0")
-          OR (o.sentido = "V" AND t.direction_id = "1"))
-      WHERE
-        indicador_trajeto_alternativo IS FALSE
-      )
-    UNION ALL
-      (
-      SELECT
-        o.feed_version,
-        o.feed_start_date,
-        o.feed_end_date,
-        o.tipo_os,
-        o.tipo_dia,
-        servico,
-        o.vista || " " || ot.evento AS vista,
-        o.consorcio,
-        sentido,
-        ot.distancia_planejada,
-        distancia_total_planejada,
-        COALESCE(ot.inicio_periodo, o.inicio_periodo) AS inicio_periodo,
-        COALESCE(ot.fim_periodo, o.fim_periodo) AS fim_periodo,
-        trip_id,
-        shape_id,
-        indicador_trajeto_alternativo
-      FROM
-        {{ ref("ordem_servico_trajeto_alternativo_sentido_atualizado_aux_gtfs") }} AS ot
-      LEFT JOIN
-        {{ ref("ordem_servico_sentido_atualizado_aux_gtfs") }} AS o
-      USING
-        (feed_version,
-          tipo_os,
-          servico,
-          sentido)
-      LEFT JOIN
-        {{ ref("trips_filtrada_aux_gtfs") }} AS t
-      ON
-        t.feed_version = o.feed_version
-        AND o.servico = t.trip_short_name
-        AND
-          (o.tipo_dia = t.tipo_dia
-          OR (o.tipo_dia = "Ponto Facultativo" AND t.tipo_dia = "Dia Útil")
-          OR (t.tipo_dia = "EXCEP")) -- Inclui trips do service_id/tipo_dia "EXCEP"
-        AND
-          ((o.sentido IN ("I", "C") AND t.direction_id = "0")
-          OR (o.sentido = "V" AND t.direction_id = "1"))
-        AND t.trip_headsign LIKE CONCAT("%", ot.evento, "%")
-      WHERE
-        indicador_trajeto_alternativo IS TRUE
-        AND trip_id IS NOT NULL -- Remove serviços de tipo_dia sem planejamento
-      )
+with
+    -- 1. Busca os shapes em formato geográfico
+    shapes as (
+        select *
+        from {{ ref("shapes_geom_gtfs") }}
+        {% if is_incremental() -%}
+            where feed_start_date = '{{ var("data_versao_gtfs") }}'
+        {% endif -%}
+    ),
+    -- 2. Trata a OS, inclui trip_ids e ajusta nomes das colunas
+    ordem_servico_tratada as (
+        select *
+        from
+            (
+                (
+                    select
+                        o.feed_version,
+                        o.feed_start_date,
+                        o.feed_end_date,
+                        o.tipo_os,
+                        o.tipo_dia,
+                        servico,
+                        vista,
+                        consorcio,
+                        sentido,
+                        distancia_planejada,
+                        distancia_total_planejada,
+                        inicio_periodo,
+                        fim_periodo,
+                        trip_id,
+                        shape_id,
+                        indicador_trajeto_alternativo
+                    from {{ ref("ordem_servico_sentido_atualizado_aux_gtfs") }} as o
+                    left join
+                        {{ ref("trips_filtrada_aux_gtfs") }} as t
+                        on t.feed_version = o.feed_version
+                        and o.servico = t.trip_short_name
+                        and (
+                            (
+                                o.tipo_dia = t.tipo_dia
+                                and o.tipo_os not in ("CNU", "Enem")
+                            )
+                            or (
+                                o.tipo_dia = "Ponto Facultativo"
+                                and t.tipo_dia = "Dia Útil"
+                                and o.tipo_os != "CNU"
+                            )
+                            or (
+                                o.feed_start_date = "2024-08-16"
+                                and o.tipo_os = "CNU"
+                                and o.tipo_dia = "Domingo"
+                                and t.tipo_dia = "Sabado"
+                            )  -- Domingo CNU
+                            or (
+                                o.feed_start_date in ("2024-09-29", "2024-11-06")
+                                and o.tipo_os = "Enem"
+                                and o.tipo_dia = "Domingo"
+                                and t.tipo_dia = "Sabado"
+                            )
+                        )  -- Domingo Enem
+                        and (
+                            (o.sentido in ("I", "C") and t.direction_id = "0")
+                            or (o.sentido = "V" and t.direction_id = "1")
+                        )
+                    where indicador_trajeto_alternativo is false
+                )
+                union all
+                (
+                    select
+                        o.feed_version,
+                        o.feed_start_date,
+                        o.feed_end_date,
+                        o.tipo_os,
+                        o.tipo_dia,
+                        servico,
+                        o.vista || " " || ot.evento as vista,
+                        o.consorcio,
+                        sentido,
+                        ot.distancia_planejada,
+                        distancia_total_planejada,
+                        coalesce(ot.inicio_periodo, o.inicio_periodo) as inicio_periodo,
+                        coalesce(ot.fim_periodo, o.fim_periodo) as fim_periodo,
+                        trip_id,
+                        shape_id,
+                        indicador_trajeto_alternativo
+                    from
+                        {{
+                            ref(
+                                "ordem_servico_trajeto_alternativo_sentido_atualizado_aux_gtfs"
+                            )
+                        }}
+                        as ot
+                    left join
+                        {{ ref("ordem_servico_sentido_atualizado_aux_gtfs") }} as o
+                        using (feed_version, tipo_os, servico, sentido)
+                    left join
+                        {{ ref("trips_filtrada_aux_gtfs") }} as t
+                        on t.feed_version = o.feed_version
+                        and o.servico = t.trip_short_name
+                        and (
+                            o.tipo_dia = t.tipo_dia
+                            or (
+                                o.tipo_dia = "Ponto Facultativo"
+                                and t.tipo_dia = "Dia Útil"
+                            )
+                            or (t.tipo_dia = "EXCEP")
+                        )  -- Inclui trips do service_id/tipo_dia "EXCEP"
+                        and (
+                            (o.sentido in ("I", "C") and t.direction_id = "0")
+                            or (o.sentido = "V" and t.direction_id = "1")
+                        )
+                        and t.trip_headsign like concat("%", ot.evento, "%")
+                    where indicador_trajeto_alternativo is true and trip_id is not null  -- Remove serviços de tipo_dia sem planejamento
+                )
+            )
+    ),
+    -- 3. Inclui trip_ids de ida e volta para trajetos circulares, ajusta shape_id
+    -- para trajetos circulares e inclui id_tipo_trajeto
+    ordem_servico_trips as (
+        select
+            * except (shape_id, indicador_trajeto_alternativo),
+            shape_id as shape_id_planejado,
+            case
+                when sentido = "C"
+                then shape_id || "_" || split(trip_id, "_")[offset(1)]
+                else shape_id
+            end as shape_id,
+            case
+                when indicador_trajeto_alternativo is false
+                then 0  -- Trajeto regular
+                when indicador_trajeto_alternativo is true
+                then 1  -- Trajeto alternativo
+            end as id_tipo_trajeto,
+        from
+            (
+                (
+                    select distinct
+                        * except (trip_id), trip_id as trip_id_planejado, trip_id
+                    from ordem_servico_tratada
+                    where sentido = "I" or sentido = "V"
+                )
+                union all
+                (
+                    select
+                        * except (trip_id),
+                        trip_id as trip_id_planejado,
+                        concat(trip_id, "_0") as trip_id,
+                    from ordem_servico_tratada
+                    where sentido = "C"
+                )
+                union all
+                (
+                    select
+                        * except (trip_id),
+                        trip_id as trip_id_planejado,
+                        concat(trip_id, "_1") as trip_id,
+                    from ordem_servico_tratada
+                    where sentido = "C"
+                )
+            )
     )
 select
     feed_version,
