2363e40cf2d5e2fb447151b6ec64f8ce7e81e791
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-06-11 16:01:07 -0300

    add state_handler handler_notify_failure

--- pipelines/capture/cittati/flows.py
+++ pipelines/capture/cittati/flows.py
@@ -5,6 +5,7 @@ from pipelines.capture.cittati.tasks import create_gps_extractor
 from pipelines.capture.templates.flows import create_default_capture_flow
 from pipelines.constants import constants as smtr_constants
 from pipelines.schedules import create_hourly_cron
+from pipelines.utils.prefect import handler_notify_failure
 
 CAPTURA_REGISTROS_CITTATI = create_default_capture_flow(
     flow_name="cittati: registros - captura",
@@ -13,6 +14,7 @@ CAPTURA_REGISTROS_CITTATI = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     recapture_schedule_cron=create_hourly_cron(),
 )
+CAPTURA_REGISTROS_CITTATI.state_handlers.append(handler_notify_failure(webhook="alertas_gps"))
 
 CAPTURA_REALOCACAO_CITTATI = create_default_capture_flow(
     flow_name="cittati: realocacao - captura",
@@ -21,3 +23,4 @@ CAPTURA_REALOCACAO_CITTATI = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     recapture_schedule_cron=create_hourly_cron(),
 )
+CAPTURA_REALOCACAO_CITTATI.state_handlers.append(handler_notify_failure(webhook="alertas_gps"))
--- pipelines/capture/zirix/flows.py
+++ pipelines/capture/zirix/flows.py
@@ -5,6 +5,7 @@ from pipelines.capture.zirix.constants import constants
 from pipelines.capture.zirix.tasks import create_gps_extractor
 from pipelines.constants import constants as smtr_constants
 from pipelines.schedules import create_hourly_cron
+from pipelines.utils.prefect import handler_notify_failure
 
 CAPTURA_REGISTROS_ZIRIX = create_default_capture_flow(
     flow_name="zirix: registros - captura",
@@ -13,6 +14,7 @@ CAPTURA_REGISTROS_ZIRIX = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     recapture_schedule_cron=create_hourly_cron(),
 )
+CAPTURA_REGISTROS_ZIRIX.state_handlers.append(handler_notify_failure(webhook="alertas_gps"))
 
 CAPTURA_REALOCACAO_ZIRIX = create_default_capture_flow(
     flow_name="zirix: realocacao - captura",
@@ -21,3 +23,4 @@ CAPTURA_REALOCACAO_ZIRIX = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     recapture_schedule_cron=create_hourly_cron(),
 )
+CAPTURA_REALOCACAO_ZIRIX.state_handlers.append(handler_notify_failure(webhook="alertas_gps"))
