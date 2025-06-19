648dceca47d96eff6ca55fc00718e44310485ca3
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-06-18 21:26:36 -0300

    cria snapshots veiculo 2.0

---
+++ queries/snapshots/monitoramento/snapshot_veiculo_fiscalizacao_lacre.sql
@@ -0,0 +1,20 @@
+{% snapshot snapshot_veiculo_fiscalizacao_lacre %}
+
+    {{
+        config(
+            target_schema="monitoramento_staging",
+            unique_key="concat('data_inicio_lacre', '-', id_veiculo, '-', placa, '-', 'id_auto_infracao')",
+            strategy="timestamp",
+            updated_at="timestamp_ultima_atualizacao",
+            invalidate_hard_deletes=True,
+        )
+    }}
+
+    select
+        * except (versao),
+        timestamp(
+            datetime_ultima_atualizacao, "America/Sao_Paulo"
+        ) as timestamp_ultima_atualizacao
+    from {{ ref("veiculo_fiscalizacao_lacre") }}
+
+{% endsnapshot %}
