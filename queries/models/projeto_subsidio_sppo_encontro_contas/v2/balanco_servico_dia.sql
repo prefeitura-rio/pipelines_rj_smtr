9e00bf3a9b4ecb46c5e6b72be82a4aa14c3b91e6
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2025-06-24 21:14:51 -0300

    aprimora modelos

--- queries/models/projeto_subsidio_sppo_encontro_contas/v2/balanco_servico_dia.sql
+++ queries/models/projeto_subsidio_sppo_encontro_contas/v2/balanco_servico_dia.sql
@@ -24,20 +24,46 @@ with
     )
 select
     *,
-    ifnull(receita_total_aferida, 0)
-    - ifnull(receita_total_esperada - subsidio_glosado, 0) as saldo
+    if(
+        data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}"),
+        (
+            ifnull(receita_total_aferida, 0)
+            - ifnull(receita_total_esperada - subsidio_glosado, 0)
+        ),
+        (ifnull(receita_tarifaria_aferida, 0) - ifnull(receita_tarifaria_esperada, 0))
+    ) as saldo
 from
     (
         select
             b.* except (valor_subsidio_pago, receita_tarifaria_aferida),
-            b.km_apurada * par.irk as receita_total_esperada,
+            if(
+                b.data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}"),
+                b.km_apurada * par.irk,
+                null
+            ) as receita_total_esperada,
             b.km_apurada * par.irk_tarifa_publica as receita_tarifaria_esperada,
-            b.km_apurada * par.subsidio_km as subsidio_esperado,
-            (b.km_apurada * par.subsidio_km - valor_subsidio_pago) as subsidio_glosado,
-            ifnull(b.receita_tarifaria_aferida, 0)
-            + ifnull(b.valor_subsidio_pago, 0) as receita_total_aferida,
+            if(
+                b.data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}"),
+                b.km_apurada * par.subsidio_km,
+                null
+            ) as subsidio_esperado,
+            if(
+                b.data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}"),
+                (b.km_apurada * par.subsidio_km - valor_subsidio_pago),
+                null
+            ) as subsidio_glosado,
+            if(
+                b.data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}"),
+                ifnull(b.receita_tarifaria_aferida, 0)
+                + ifnull(b.valor_subsidio_pago, 0),
+                null
+            ) as receita_total_aferida,
             b.receita_tarifaria_aferida,
-            b.valor_subsidio_pago
+            if(
+                b.data < date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}"),
+                b.valor_subsidio_pago,
+                null
+            ) as valor_subsidio_pago
         from balanco_rdo_subsidio_servico_dia as b
         left join parametros as par on b.data between data_inicio and data_fim
     )
