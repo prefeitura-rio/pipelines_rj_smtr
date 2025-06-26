f3e3705524173e8d61c26b0ad8394b52e3222c0f
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-06-26 15:57:36 -0300

    ajustes finais prd

--- pipelines/treatment/infraestrutura/constants.py
+++ pipelines/treatment/infraestrutura/constants.py
@@ -18,5 +18,5 @@ class constants(Enum):  # pylint: disable=c0103
     INFRAESTRUTURA_SELECTOR = DBTSelector(
         name="infraestrutura",
         schedule_cron=create_daily_cron(hour=9),
-        initial_datetime=datetime(2025, 3, 26, 0, 0, 0),
+        initial_datetime=datetime(2025, 6, 26, 0, 0, 0),
     )
--- queries/models/infraestrutura/CHANGELOG.md
+++ queries/models/infraestrutura/CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog - infraestrutura
 
-## [1.0.0] - 2025-04-28
+## [1.0.0] - 2025-06-26
 
 ### Adicionado
 
