f058bb9df893381e8d0326a66a6f6fb284e5bf2e
vtr363 <victor.m.rocha.vr@gmail.com>
2025-07-07 15:21:53 -0300

    refatoração

--- queries/models/monitoramento/staging/staging_infracao.sql
+++ queries/models/monitoramento/staging/staging_infracao.sql
@@ -1,42 +1,86 @@
 {{ config(materialized="view", alias="infracao") }}
 
 
+with
+    infracoes_base as (
+        select
+            data,
+            safe_cast(json_value(content, '$.id_infracao') as string) as id_infracao,
+            safe_cast(json_value(content, '$.modo') as string) as modo,
+            safe_cast(json_value(content, '$.servico') as string) as servico,
+            safe_cast(json_value(content, '$.permissao') as string) as permissao,
+            safe_cast(json_value(content, '$.placa') as string) as placa,
+            safe_cast(id_auto_infracao as string) as id_auto_infracao,
+            safe_cast(
+                json_value(content, '$.data_infracao') as string
+            ) as raw_data_infracao,
+            safe_cast(json_value(content, '$.infracao') as string) as infracao,
+            safe_cast(json_value(content, '$.valor') as float64) as valor,
+            safe_cast(json_value(content, '$.status') as string) as status,
+            safe_cast(
+                datetime(
+                    timestamp_trunc(timestamp(timestamp_captura), second),
+                    "America/Sao_Paulo"
+                ) as datetime
+            ) as timestamp_captura,
+            json_value(content, '$.data_pagamento') as raw_data_pagamento
+        from {{ source("veiculo_staging", "infracao") }} as t
+        from `rj-smtr-staging.veiculo_staging.infracao`
+        where data = "2025-07-06"
+    ),
+
+    infracoes_com_datas as (
+        select
+            *,
+
+            -- Parse apenas a data (sem hora)
+            parse_date(
+                "%d/%m/%Y", split(raw_data_infracao, " ")[offset(0)]
+            ) as data_infracao,
+
+            -- Normaliza e converte para DATETIME (com hora, mesmo que seja 00:00:00)
+            safe.parse_datetime(
+                "%d/%m/%Y %H:%M:%S",
+                case
+                    when
+                        regexp_contains(
+                            raw_data_infracao, r'^\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}$'
+                        )
+                    then raw_data_infracao
+                    when
+                        regexp_contains(
+                            raw_data_infracao, r'^\d{2}/\d{2}/\d{4} \d{2}:\d{2}'
+                        )
+                    then concat(raw_data_infracao, ':00')
+                    when regexp_contains(raw_data_infracao, r'^\d{2}/\d{2}/\d{4}$')
+                    then concat(raw_data_infracao, ' 00:00:00')
+                    else null
+                end
+            ) as datetime_infracao,
+
+            -- Data de pagamento
+            if(
+                raw_data_pagamento = "",
+                null,
+                parse_date("%d/%m/%Y", raw_data_pagamento)
+            ) as data_pagamento
+
+        from infracoes_base
+    )
+
 select
     data,
-    safe_cast(json_value(content, '$.id_infracao') as string) id_infracao,
-    safe_cast(json_value(content, '$.modo') as string) modo,
-    safe_cast(json_value(content, '$.servico') as string) servico,
-    safe_cast(json_value(content, '$.permissao') as string) permissao,
-    safe_cast(json_value(content, '$.placa') as string) placa,
-    safe_cast(id_auto_infracao as string) id_auto_infracao,
-    parse_date(
-        "%d/%m/%Y",
-        split(safe_cast(json_value(content, '$.data_infracao') as string), " ")[
-            offset(0)
-        ]
-    ) data_infracao,
-    parse_datetime(
-        '%d/%m/%Y %H:%M:%S',
-        if(
-            regexp_contains(
-                json_value(content, '$.data_infracao'), r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}'
-            )
-            or data >= "2025-06-25",
-            concat(safe_cast(json_value(content, '$.data_infracao') as string), ':00'),
-            null
-        )
-    ) as datetime_infracao,
-    safe_cast(json_value(content, '$.infracao') as string) infracao,
-    safe_cast(json_value(content, '$.valor') as float64) valor,
-    safe_cast(json_value(content, '$.status') as string) status,
-    if(
-        json_value(content, '$.data_pagamento') = "",
-        null,
-        parse_date("%d/%m/%Y", json_value(content, '$.data_pagamento'))
-    ) data_pagamento,
-    safe_cast(
-        datetime(
-            timestamp_trunc(timestamp(timestamp_captura), second), "America/Sao_Paulo"
-        ) as datetime
-    ) timestamp_captura
-from {{ source("veiculo_staging", "infracao") }} as t
+    id_infracao,
+    modo,
+    servico,
+    permissao,
+    placa,
+    id_auto_infracao,
+    data_infracao,
+    datetime_infracao,
+    infracao,
+    valor,
+    status,
+    data_pagamento,
+    timestamp_captura
+from infracoes_com_datas
