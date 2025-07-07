c2ce428b3123d050bd374ea924164374c739f0b2
vtr363 <victor.m.rocha.vr@gmail.com>
2025-07-07 13:13:45 -0300

    corrige coluna datetime_infracao

--- queries/models/monitoramento/staging/staging_infracao.sql
+++ queries/models/monitoramento/staging/staging_infracao.sql
@@ -1,32 +1,42 @@
-
-{{ config(
-        materialized='view',
-        alias='infracao'
-  )
-}}
+{{ config(materialized="view", alias="infracao") }}
 
 
-SELECT
-  data,
-  SAFE_CAST(JSON_VALUE(content,'$.id_infracao') AS STRING) id_infracao,
-  SAFE_CAST(JSON_VALUE(content,'$.modo') AS STRING) modo,
-  SAFE_CAST(JSON_VALUE(content,'$.servico') AS STRING) servico,
-  SAFE_CAST(JSON_VALUE(content,'$.permissao') AS STRING) permissao,
-  SAFE_CAST(JSON_VALUE(content,'$.placa') AS STRING) placa,
-  SAFE_CAST(id_auto_infracao AS STRING) id_auto_infracao,
-  PARSE_DATE("%d/%m/%Y", split(SAFE_CAST(JSON_VALUE(content,'$.data_infracao') AS STRING), " ")[OFFSET(0)]) data_infracao,
-  PARSE_DATETIME('%d/%m/%Y %H:%M:%S',
-    IF(
-      REGEXP_CONTAINS(JSON_VALUE(content, '$.data_infracao'), r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}'),
-      CONCAT(SAFE_CAST(JSON_VALUE(content, '$.data_infracao') AS STRING), ':00'),
-      CONCAT(SAFE_CAST(JSON_VALUE(content, '$.data_infracao') AS STRING), ' 00:00:00')
-    )
-  ) AS datetime_infracao,
-  SAFE_CAST(JSON_VALUE(content,'$.infracao') AS STRING) infracao,
-  SAFE_CAST(JSON_VALUE(content,'$.valor') AS FLOAT64) valor,
-  SAFE_CAST(JSON_VALUE(content,'$.status') AS STRING) status,
-  IF(JSON_VALUE(content,'$.data_pagamento') = "", NULL, PARSE_DATE("%d/%m/%Y", JSON_VALUE(content,'$.data_pagamento'))) data_pagamento,
-  SAFE_CAST(DATETIME(TIMESTAMP_TRUNC(TIMESTAMP(timestamp_captura), SECOND), "America/Sao_Paulo" ) AS DATETIME) timestamp_captura
-FROM
-  {{ source('veiculo_staging','infracao') }} as t
-
+select
+    data,
+    safe_cast(json_value(content, '$.id_infracao') as string) id_infracao,
+    safe_cast(json_value(content, '$.modo') as string) modo,
+    safe_cast(json_value(content, '$.servico') as string) servico,
+    safe_cast(json_value(content, '$.permissao') as string) permissao,
+    safe_cast(json_value(content, '$.placa') as string) placa,
+    safe_cast(id_auto_infracao as string) id_auto_infracao,
+    parse_date(
+        "%d/%m/%Y",
+        split(safe_cast(json_value(content, '$.data_infracao') as string), " ")[
+            offset(0)
+        ]
+    ) data_infracao,
+    parse_datetime(
+        '%d/%m/%Y %H:%M:%S',
+        if(
+            regexp_contains(
+                json_value(content, '$.data_infracao'), r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}'
+            )
+            or data >= "2025-06-25",
+            concat(safe_cast(json_value(content, '$.data_infracao') as string), ':00'),
+            null
+        )
+    ) as datetime_infracao,
+    safe_cast(json_value(content, '$.infracao') as string) infracao,
+    safe_cast(json_value(content, '$.valor') as float64) valor,
+    safe_cast(json_value(content, '$.status') as string) status,
+    if(
+        json_value(content, '$.data_pagamento') = "",
+        null,
+        parse_date("%d/%m/%Y", json_value(content, '$.data_pagamento'))
+    ) data_pagamento,
+    safe_cast(
+        datetime(
+            timestamp_trunc(timestamp(timestamp_captura), second), "America/Sao_Paulo"
+        ) as datetime
+    ) timestamp_captura
+from {{ source("veiculo_staging", "infracao") }} as t
