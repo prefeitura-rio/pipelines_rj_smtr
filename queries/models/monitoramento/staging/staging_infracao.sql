8e0d1dbc86ed3ce49108dca79e5cc7f8ec0490dd
vtr363 <victor.m.rocha.vr@gmail.com>
2025-07-07 13:02:45 -0300

    Corrige coluna datetime_infracao para datas anteriores a 2025-06-25

--- queries/models/monitoramento/staging/staging_infracao.sql
+++ queries/models/monitoramento/staging/staging_infracao.sql
@@ -15,7 +15,13 @@ SELECT
   SAFE_CAST(JSON_VALUE(content,'$.placa') AS STRING) placa,
   SAFE_CAST(id_auto_infracao AS STRING) id_auto_infracao,
   PARSE_DATE("%d/%m/%Y", split(SAFE_CAST(JSON_VALUE(content,'$.data_infracao') AS STRING), " ")[OFFSET(0)]) data_infracao,
-  PARSE_DATETIME('%d/%m/%Y %H:%M:%S', CONCAT(SAFE_CAST(JSON_VALUE(content, '$.data_infracao') AS STRING), ':00')) AS datetime_infracao,
+  PARSE_DATETIME('%d/%m/%Y %H:%M:%S', 
+    IF(
+      REGEXP_CONTAINS(JSON_VALUE(content, '$.data_infracao'), r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}'),
+      CONCAT(SAFE_CAST(JSON_VALUE(content, '$.data_infracao') AS STRING), ':00'),
+      CONCAT(SAFE_CAST(JSON_VALUE(content, '$.data_infracao') AS STRING), ' 00:00:00')
+    )
+  ) AS datetime_infracao,
   SAFE_CAST(JSON_VALUE(content,'$.infracao') AS STRING) infracao,
   SAFE_CAST(JSON_VALUE(content,'$.valor') AS FLOAT64) valor,
   SAFE_CAST(JSON_VALUE(content,'$.status') AS STRING) status,
