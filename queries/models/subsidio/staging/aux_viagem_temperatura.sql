bab6a9686cf373d12b67d32992e643fb38368d27
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-07-15 20:03:49 -0300

    move modelo para staging e corrige DATA_SUBSIDIO_V16_INICIO

--- queries/models/subsidio/staging/aux_viagem_temperatura.sql
+++ queries/models/subsidio/staging/aux_viagem_temperatura.sql
@@ -7,23 +7,17 @@
 }}
 
 {% set incremental_filter %}
-    data between date("{{var('start_date')}}") and date("{{var('end_date')}}") and data >= date("{{ var('DATA_SUBSIDIO_V15C_INICIO') }}")
+    data between date("{{var('start_date')}}") and date("{{var('end_date')}}") and data >= date("{{ var('DATA_SUBSIDIO_V16_INICIO') }}")
 {% endset %}
 
 with
     viagens as (
-        select
-            data,
-            servico,
-            datetime_partida,
-            datetime_chegada,
-            id_veiculo,
-            id_viagem,
-            --ano_fabricacao
-            --distancia_planejada,
-            --sentido
+        select data, servico, datetime_partida, datetime_chegada, id_veiculo, id_viagem,
+        -- ano_fabricacao
+        -- distancia_planejada,
+        -- sentido
         -- from {{ ref("viagem_classificada") }}
-        FROM `rj-smtr.subsidio.viagem_classificada`
+        from `rj-smtr.subsidio.viagem_classificada`
         -- where {{ incremental_filter }}
         where data = "2025-06-17"
     ),
@@ -43,18 +37,14 @@ with
         from `rj-smtr`.`br_rj_riodejaneiro_bilhetagem`.`gps_validador`
         -- where {{ incremental_filter }}
         where data = "2025-06-17"
-            -- and temperatura != 0
+    -- and temperatura != 0
     ),
     veiculos as (
-        select distinct
-            id_veiculo,
-            data,
-            indicador_ar_condicionado,
-            ano_fabricacao
+        select distinct id_veiculo, data, indicador_ar_condicionado, ano_fabricacao
         -- from {{ ref("licenciamento") }}
         from `rj-smtr`.`cadastro`.`veiculo_licenciamento_dia`
         -- where
-        --     {{ incremental_filter }}
+        -- {{ incremental_filter }}
         where data = "2025-06-17"
 
     ),
@@ -72,16 +62,17 @@ with
             e.longitude,
             temperatura
         from gps_validador as e
-        full join viagens as v
+        full join
+            viagens as v
             on e.id_veiculo = substr(v.id_veiculo, 2)
             and e.datetime_gps between v.datetime_partida and v.datetime_chegada
     ),
-    temperatura_inmet as(
+    temperatura_inmet as (
         select
             id_estacao,
             temperatura,
-            data_particao as data,-- atualizar dps
-            horario -- atualizar dps
+            data_particao as data,  -- atualizar dps
+            horario  -- atualizar dps
         from `rj-cor.clima_estacao_meteorologica.meteorologia_inmet`
         -- where {{ incremental_filter }}
         where data_particao = "2025-06-17"
@@ -95,7 +86,8 @@ with
             temperatura,
             -- estado_equipamento as indicador_estado_equipamento_aberto,
             -- count(distinct temperatura) = 1) as indicador_temperatura_variacao,
-            -- countif(temperatura is not null) > 0 as indicador_temperatura_transmitida,
+            -- countif(temperatura is not null) > 0 as
+            -- indicador_temperatura_transmitida,
             -- count(distinct temperatura) = 1) as indicador_temperatura_descatada,
             datetime_gps
         from gps_validador
