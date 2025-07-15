3e68460acc9ef0b35c81e337e9e204d10cec4cc8
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-07-15 18:54:54 -0300

    altera variaveis de data

--- queries/models/subsidio/viagem_classificada.sql
+++ queries/models/subsidio/viagem_classificada.sql
@@ -130,7 +130,7 @@ with
                 then t.maior_tecnologia_permitida
                 when
                     p.prioridade < p_menor.prioridade
-                    and data >= date('{{ var("DATA_SUBSIDIO_V15A_INICIO") }}')
+                    and data >= date('{{ var("DATA_SUBSIDIO_V17_INICIO") }}')
                 then null
                 else vs.tecnologia
             end as tecnologia_remunerada,
@@ -186,11 +186,11 @@ with
                     vt.status = "Licenciado sem ar e não autuado"
                     and vt.servico
                     not in (select servico from {{ ref("servico_contrato_abreviado") }})
-                    and vt.data >= date("{{ var('DATA_SUBSIDIO_V15B_INICIO') }}")
+                    and vt.data >= date("{{ var('DATA_SUBSIDIO_V18_INICIO') }}")
                 then "Não autorizado por ausência de ar-condicionado"
                 when
                     vt.indicador_penalidade_tecnologia
-                    and vt.data >= date('{{ var("DATA_SUBSIDIO_V15A_INICIO") }}')
+                    and vt.data >= date('{{ var("DATA_SUBSIDIO_V17_INICIO") }}')
                 then "Não autorizado por capacidade"
                 when vt.status = "Autuado por ar inoperante"
                 then "Autuado por ar inoperante"
