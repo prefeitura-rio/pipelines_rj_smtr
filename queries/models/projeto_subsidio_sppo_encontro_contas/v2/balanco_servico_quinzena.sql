22cf1de8d0effa89040a549acb9b85fb2e7c9f2c
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2025-07-15 20:46:33 -0300

    aprimora modelo para evitar duplicação de dados e melhorar a performance

--- queries/models/projeto_subsidio_sppo_encontro_contas/v2/balanco_servico_quinzena.sql
+++ queries/models/projeto_subsidio_sppo_encontro_contas/v2/balanco_servico_quinzena.sql
@@ -29,28 +29,33 @@ with
             max(data) as data_final_quinzena
         from datas
         group by quinzena
+    ),
+    balanco_agg as (
+        select
+            quinzena,
+            data_inicial_quinzena,
+            data_final_quinzena,
+            servico,
+            consorcio,
+            count(data) as quantidade_dias_subsidiado,
+            sum(km_apurada) as km_apurada,
+            sum(receita_total_esperada) as receita_total_esperada,
+            sum(receita_tarifaria_esperada) as receita_tarifaria_esperada,
+            sum(subsidio_esperado) as subsidio_esperado,
+            sum(subsidio_glosado) as subsidio_glosado,
+            sum(receita_total_aferida) as receita_total_aferida,
+            sum(receita_tarifaria_aferida) as receita_tarifaria_aferida,
+            sum(valor_subsidio_pago) as subsidio_pago,
+            sum(saldo) as saldo
+        from quinzenas qz
+        left join
+            {{ ref("balanco_servico_dia") }} bs
+            on bs.data between qz.data_inicial_quinzena and qz.data_final_quinzena
+        group by all
     )
 select
-    quinzena,
-    data_inicial_quinzena,
-    data_final_quinzena,
-    servico,
-    consorcio,
-    count(data) as quantidade_dias_subsidiado,
-    sum(km_apurada) as km_apurada,
-    sum(receita_total_esperada) as receita_total_esperada,
-    sum(receita_tarifaria_esperada) as receita_tarifaria_esperada,
-    sum(subsidio_esperado) as subsidio_esperado,
-    sum(subsidio_glosado) as subsidio_glosado,
-    sum(receita_total_aferida) as receita_total_aferida,
-    sum(receita_tarifaria_aferida) as receita_tarifaria_aferida,
-    sum(valor_subsidio_pago) as subsidio_pago,
-    sum(saldo) as saldo,
+    *,
     '{{ var("version") }}' as versao,
     current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao,
     '{{ invocation_id }}' as id_execucao_dbt
-from quinzenas qz
-left join
-    {{ ref("balanco_servico_dia") }} bs
-    on bs.data between qz.data_inicial_quinzena and qz.data_final_quinzena
-group by all
+from balanco_agg
--- queries/models/projeto_subsidio_sppo_encontro_contas/v2/encontro_contas_parametros.sql
+++ queries/models/projeto_subsidio_sppo_encontro_contas/v2/encontro_contas_parametros.sql
@@ -4,15 +4,21 @@
     )
 }}
 
+with
+    parametros_agg as (
+        select
+            min(data_inicio) as data_inicio,
+            max(data_fim) as data_fim,
+            safe_cast(irk as numeric) as irk,
+            safe_cast(irk_tarifa_publica as numeric) as irk_tarifa_publica,
+            safe_cast(max(subsidio_km) as numeric) as subsidio_km
+        from {{ ref("valor_km_tipo_viagem") }}
+        where data_inicio >= "{{ var('encontro_contas_datas_v2_inicio') }}"
+        group by all
+    )
 select
-    min(data_inicio) as data_inicio,
-    max(data_fim) as data_fim,
-    safe_cast(irk as numeric) as irk,
-    safe_cast(irk_tarifa_publica as numeric) as irk_tarifa_publica,
-    safe_cast(max(subsidio_km) as numeric) as subsidio_km,
+    *,
     '{{ var("version") }}' as versao,
     current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao,
     '{{ invocation_id }}' as id_execucao_dbt
-from {{ ref("valor_km_tipo_viagem") }}
-where data_inicio >= "{{ var('encontro_contas_datas_v2_inicio') }}"
-group by all
+from parametros_agg
