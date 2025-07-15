406ea529039e6be5f75b8804bf5ebaad93854292
Rodrigo Cunha <66736583+eng-rodrigocunha@users.noreply.github.com>
2025-07-15 19:25:40 -0300

    Encontro de contas V2 (#511)
    * ajusta encontro de contas
    
    * ajusta balanço servico-quinzena
    
    * atualiza modelos
    
    * inclui modelo E1
    
    * Update queries/models/projeto_subsidio_sppo_encontro_contas/aux_balanco_rdo_servico_dia.sql
    
    * ajusta modelo
    
    * ajusta modelo
    
    * ajusta modelos
    
    * atualiza modelos
    
    * atualiza receita sem associação
    
    * altera receita sem associação
    
    * corrige servico_sem_associacao
    
    * ajusta receita tarifaria sem associação
    
    * atualiza modelos e cria v2
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * refatoração geral
    
    * refatoração
    
    * refatoração geral
    
    * refatora e aprimora
    
    * aprimora
    
    * inclui variavel no dbt_project
    
    * refatora
    
    * ajustes
    
    * aprimora modelos
    
    * retorna v1
    
    * ajusta modelos
    
    * Retorna recursos_sppo_servico_dia_pago ao original
    
    * ajusta sources
    
    * ajusta versões + schema
    
    * corrige sintaxe
    
    * ajusta estrutura
    
    * Atualiza a documentação
    
    * altera nome dos modelos
    
    * altera schema
    
    * refatoração + aprimoramentos
    
    * refatora código
    
    * altera tipo de coluna para numeric
    
    * muda estrutura do projeto
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * muda config de materialização
    
    * ajusta modelos
    
    * insere ajustes e descrições das colunas do modelos de encontro de contas
    
    * [FIX] ajusta jinja km_planejada
    
    * [FIX] ajusta jinja data_fim e data_inicio
    
    * insere ajustes com base nas sugestões do coderabbit
    
    * corrige ordem de colunas
    
    * Altera modelo de materialização e particionamento
    
    * Atualiza schema.yml com novas descrições e correções
    
    * Muda referências para prod
    
    * atualiza changelog
    
    * Altera referências em sources.yml
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Adriano Neto <adrianoneto27@gmail.com>

---
+++ queries/models/projeto_subsidio_sppo_encontro_contas/v2/balanco_servico_quinzena.sql
@@ -0,0 +1,53 @@
+{{
+    config(
+        partition_by={"field": "data_inicial_quinzena"},
+    )
+}}
+
+with
+    datas as (
+        select
+            data,
+            case
+                when extract(day from data) <= 15
+                then format_date('%Y-%m-Q1', data)
+                else format_date('%Y-%m-Q2', data)
+            end as quinzena,
+        from
+            unnest(
+                generate_date_array(
+                    date("{{ var('start_date') }}"),
+                    date("{{ var('end_date') }}"),
+                    interval 1 day
+                )
+            ) as data
+    ),
+    quinzenas as (
+        select
+            quinzena,
+            min(data) as data_inicial_quinzena,
+            max(data) as data_final_quinzena
+        from datas
+        group by quinzena
+    )
+select
+    quinzena,
+    data_inicial_quinzena,
+    data_final_quinzena,
+    servico,
+    consorcio,
+    count(data) as quantidade_dias_subsidiado,
+    sum(km_apurada) as km_apurada,
+    sum(receita_total_esperada) as receita_total_esperada,
+    sum(receita_tarifaria_esperada) as receita_tarifaria_esperada,
+    sum(subsidio_esperado) as subsidio_esperado,
+    sum(subsidio_glosado) as subsidio_glosado,
+    sum(receita_total_aferida) as receita_total_aferida,
+    sum(receita_tarifaria_aferida) as receita_tarifaria_aferida,
+    sum(valor_subsidio_pago) as subsidio_pago,
+    sum(saldo) as saldo
+from quinzenas qz
+left join
+    {{ ref("balanco_servico_dia") }} bs
+    on bs.data between qz.data_inicial_quinzena and qz.data_final_quinzena
+group by quinzena, data_inicial_quinzena, data_final_quinzena, consorcio, servico
