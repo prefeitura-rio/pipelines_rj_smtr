19f7962e2df4bb6313c20e50823e383e5894259e
vtr363 <victor.m.rocha.vr@gmail.com>
2025-07-22 10:12:26 -0300

    corrige viagem_planejada

--- queries/models/projeto_subsidio_sppo/viagem_planejada.sql
+++ queries/models/projeto_subsidio_sppo/viagem_planejada.sql
@@ -9,7 +9,7 @@ select *
 from {{ ref("viagem_planejada_v1") }}
 where
     data <= date("{{ var('DATA_SUBSIDIO_V6_INICIO') }}")
-union all
+union all by name
 select *
 from {{ ref("viagem_planejada_v2") }}
 where
