4cebceca78c05eb25923d8c914ff66db9d0af20c
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-07-24 21:55:36 -0300

    cria modelo aux_veiculo_falha_ar_condicionado

--- queries/models/monitoramento/veiculo_regularidade_temperatura_dia.sql
+++ queries/models/monitoramento/veiculo_regularidade_temperatura_dia.sql
@@ -10,98 +10,35 @@
     )
 }}
 
-with
-    viagem_temperatura as (
-        select
-            data,
-            id_viagem,
-            id_veiculo,
-            ano_fabricacao,
-            safe_cast(
-                json_value(indicadores, '$.indicador_ar_condicionado.valor') as bool
-            ) as indicador_ar_condicionado,
-            safe_cast(
-                json_value(
-                    indicadores, '$.indicador_temperatura_variacao.valor'
-                ) as bool
-            ) as indicador_temperatura_variacao,
-            safe_cast(
-                json_value(
-                    indicadores, '$.indicador_temperatura_transmitida.valor'
-                ) as bool
-            ) as indicador_temperatura_transmitida,
-            safe_cast(
-                json_value(
-                    indicadores,
-                    '$.indicador_temperatura_descartada.percentual_temperatura_nula_descartada'
-                ) as numeric
-            ) as percentual_temperatura_nula_descartada,
-            safe_cast(
-                json_value(
-                    indicadores,
-                    '$.indicador_temperatura_descartada.percentual_temperatura_atipica_descartada'
-                ) as numeric
-            ) as percentual_temperatura_atipica_descartada,
-            safe_cast(
-                json_value(
-                    indicadores, '$.indicador_temperatura_descartada.valor'
-                ) as bool
-            ) as indicador_temperatura_descartada
-        from {{ ref("aux_viagem_temperatura") }}
-        where
-            data
-            between date_sub(date("{{var('start_date')}}"), interval 4 day) and date(
-                "{{var('end_date')}}"
-            )
+{% set incremental_filter %}
+    data between date("{{var('start_date')}}") and date("{{var('end_date')}}")
             and data >= date("{{ var('DATA_SUBSIDIO_V16_INICIO') }}")
-    ),
-    agg_veiculo as (
+{% endset %}
+
+with
+    aux_veiculo as (
         select
             data,
             id_veiculo,
             ano_fabricacao,
             indicador_ar_condicionado,
-            max(indicador_temperatura_variacao) as indicador_temperatura_variacao,
-            max(indicador_temperatura_transmitida) as indicador_temperatura_transmitida,
-            max(
-                percentual_temperatura_nula_descartada
-            ) as percentual_temperatura_nula_descartada,
-            max(
-                percentual_temperatura_atipica_descartada
-            ) as percentual_temperatura_atipica_descartada,
-            max(indicador_temperatura_descartada) as indicador_temperatura_descartada
-        from viagem_temperatura
-        group by all
+            data_processamento_licenciamento,
+            data_verificacao_regularidade,
+            indicador_temperatura_variacao,
+            indicador_temperatura_transmitida,
+            percentual_temperatura_nula_descartada,
+            percentual_temperatura_atipica_descartada,
+            indicador_temperatura_descartada,
+            quantidade_dia_falha_operacional
+        from {{ ref("aux_veiculo_falha_ar_condicionado") }}
+        where {{ incremental_filter }} and indicio_falha
     ),
-    falha_dia as (
+    indicador_motivo as (
         select
             *,
+            quantidade_dia_falha_operacional >= 6 as indicador_falha_recorrente,
             case
-                when
-                    not indicador_temperatura_variacao
-                    or not indicador_temperatura_transmitida
-                    or (indicador_temperatura_descartada)
-                then true
-                else false
-            end as indicio_falha
-        from agg_veiculo
-    ),
-    dias_com_falha_consecutiva as (
-        select
-            *,
-            countif(indicio_falha) over (
-                partition by id_veiculo
-                order by data
-                rows between 4 preceding and current row
-            ) as quantidade_dia_falha_operacional
-        from falha_dia
-    ),
-    veiculo_com_falha as (
-        select
-            *,
-            quantidade_dia_falha_operacional = 5 as indicador_falha,
-            case
-                when quantidade_dia_falha_operacional = 5
+                when quantidade_dia_falha_operacional > 0
                 then
                     (
                         select string_agg(motivo, ', ')
@@ -110,17 +47,17 @@ with
                                 [
                                     if(
                                         not indicador_temperatura_variacao,
-                                        "Repetição de valores ao longo do dia",
+                                        "Repetição do mesmo valor de temperatura ao longo de todas as viagens realizadas em um dia de operação",
                                         ""
                                     ),
                                     if(
                                         not indicador_temperatura_transmitida,
-                                        "Ausência total de dados",
+                                        "Ausência total de transmissão de dados de temperatura interna durante um dia de operação",
                                         ""
                                     ),
                                     if(
                                         indicador_temperatura_descartada,
-                                        "Mais de 50% dos registros descartados",
+                                        "Descarte de mais de 50% dos registros de temperatura de todas as viagens realizadas em um dia de operação",
                                         ""
                                     )
                                 ]
@@ -129,20 +66,114 @@ with
                     )
                 else null
             end as motivo
-        from dias_com_falha_consecutiva
+        from aux_veiculo
+    ),
+    dados_novos as (
+        select
+            data,
+            id_veiculo,
+            ano_fabricacao,
+            to_json_string(
+                struct(
+                    struct(
+                        data_processamento_licenciamento,
+                        indicador_ar_condicionado as valor
+                    ) as indicador_ar_condicionado,
+                    struct(
+                        data_verificacao_regularidade,
+                        indicador_temperatura_variacao as valor
+                    ) as indicador_temperatura_variacao,
+                    struct(
+                        data_verificacao_regularidade,
+                        indicador_temperatura_transmitida as valor
+                    ) as indicador_temperatura_transmitida,
+                    struct(
+                        data_verificacao_regularidade,
+                        indicador_temperatura_descartada as valor,
+                        safe_cast(
+                            percentual_temperatura_nula_descartada as string
+                        ) as percentual_temperatura_nula_descartada,
+                        safe_cast(
+                            percentual_temperatura_atipica_descartada as string
+                        ) as percentual_temperatura_atipica_descartada
+                    ) as indicador_temperatura_descartada,
+                    struct(
+                        current_date("America/Sao_Paulo") as data_verificacao_falha,
+                        indicador_falha_recorrente as valor
+                    ) as indicador_falha_recorrente
+                )
+            ) as indicadores,
+            quantidade_dia_falha_operacional,
+            motivo,
+            current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao,
+            "{{ var('version') }}" as versao,
+            '{{ invocation_id }}' as id_execucao_dbt
+        from indicador_motivo
     )
-select
-    data,
-    id_veiculo,
-    ano_fabricacao,
-    indicador_ar_condicionado,
-    indicador_temperatura_variacao,
-    indicador_temperatura_transmitida,
-    percentual_temperatura_nula_descartada,
-    percentual_temperatura_atipica_descartada,
-    indicador_temperatura_descartada,
-    quantidade_dia_falha_operacional,
-    indicador_falha,
-    motivo
-from veiculo_com_falha
-where indicador_falha
+{% if is_incremental() %}
+        ,
+        dados_completos as (
+            select *, 1 as ordem
+            from dados_novos
+
+            union all
+
+            select *, 0 as ordem
+            from {{ this }}
+            where {{ incremental_filter }}
+
+        ),
+        sha_dados as (
+            {% set columns = (
+                list_columns()
+                | reject(
+                    "in",
+                    [
+                        "versao",
+                        "datetime_ultima_atualizacao",
+                        "id_execucao_dbt",
+                    ],
+                )
+                | list
+            ) %}
+
+            select
+                *,
+                sha256(
+                    concat(
+                        {% for c in columns %}
+                            ifnull(
+                                {% if c == "indicadores" %}to_json_string(indicadores)
+                                {% else %}cast({{ c }} as string)
+                                {% endif %},
+                                'n/a'
+                            )
+                            {% if not loop.last %}, {% endif %}
+                        {% endfor %}
+                    )
+                ) as sha_dado
+            from dados_completos
+        ),
+        dados_completos_invocation_id as (
+            select
+                * except (id_execucao_dbt, sha_dado),
+                case
+                    when
+                        lag(sha_dado) over (win) != sha_dado
+                        or (lag(sha_dado) over (win) is null and ordem = 1)
+                    then id_execucao_dbt
+                    else lag(id_execucao_dbt) over (win)
+                end as id_execucao_dbt
+            from sha_dados
+            window win as (partition by data, id_veiculo order by ordem)
+        )
+
+    select * except (ordem)
+    from dados_completos_invocation_id
+    where ordem = 1
+{% else %} select * from dados_novos
+{% endif %}
+
+    -- trocar variaveis dbt de acordo com o flow
+    -- materializar veiculo_dia todo dia
+
