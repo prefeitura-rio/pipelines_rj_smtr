d4c1dffb4a8cb89474f36d173fb58878572d28f0
Adriano Neto <adrianoneto27@gmail.com>
2025-07-25 18:20:01 -0300

    ajusta variavel data de filtragem do modelo

--- queries/models/monitoramento/temperatura_inmet.sql
+++ queries/models/monitoramento/temperatura_inmet.sql
@@ -13,7 +13,7 @@
 select data_particao as data, horario as hora, id_estacao, temperatura
 from {{ source("clima_estacao_meteorologica", "meteorologia_inmet") }}
 where
-    data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+    data_particao >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
     {% if is_incremental() %}
         and data_particao between date("{{ var('date_range_start') }}") and date(
             "{{ var('date_range_end') }}"
