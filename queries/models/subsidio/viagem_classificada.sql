60618ac801286fe82763847f394faa2e07334c65
Victor Miguel Rocha <victor.m.rocha.vr@gmail.com>
2025-07-25 13:25:04 -0300

    Update queries/models/subsidio/viagem_classificada.sql
    Co-authored-by: Guilherme Botelho <guilhermehcb@hotmail.com>

--- queries/models/subsidio/viagem_classificada.sql
+++ queries/models/subsidio/viagem_classificada.sql
@@ -163,7 +163,7 @@ with
                 then "Não autorizado por ausência de ar-condicionado"
                 when
                     vt.indicador_penalidade_tecnologia
-                    and vt.data >= date('{{ var("DATA_SUBSIDIO_V15B_INICIO") }}')
+                    and vt.data >= date('{{ var("DATA_SUBSIDIO_V15A_INICIO") }}')
                 then "Não autorizado por capacidade"
                 when vt.status = "Autuado por ar inoperante"
                 then "Autuado por ar inoperante"
