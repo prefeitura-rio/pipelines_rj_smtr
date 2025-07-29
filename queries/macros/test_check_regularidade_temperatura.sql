46668b6a2ce9665f7452cc419f0f5260bf146261
Adriano Neto <adrianoneto27@gmail.com>
2025-07-29 18:54:39 -0300

    add testes de validação cruzada e ajustes

--- queries/macros/test_check_regularidade_temperatura.sql
+++ queries/macros/test_check_regularidade_temperatura.sql
@@ -1,20 +1,54 @@
 {% test test_check_regularidade_temperatura(model) %}
 
+{% set incremental_filter %}
+    data between date("{{var('date_range_start')}}") and date("{{ var('date_range_end') }}") and data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+{% endset %}
+
 with
+    indicadores_aux_viagem_temperatura as (
+        select
+            data,
+            id_veiculo,
+            id_viagem,
+            safe_cast(json_value(indicadores, '$.indicador_temperatura_descartada.valor') as bool) as indicador_temperatura_descartada,
+            safe_cast(json_value(indicadores, '$.indicador_temperatura_transmitida.valor') as bool) as indicador_temperatura_transmitida,
+            safe_cast(json_value(indicadores, '$.indicador_temperatura_regular.valor') as bool) as indicador_temperatura_regular
+        from {{ ref("aux_viagem_temperatura") }}
+        where {{ incremental_filter }}
+    ),
+    indicadores_veiculo_regularidade_temperatura_dia as (
+        select
+            data,
+            id_veiculo,
+            indicadores.indicador_falha_recorrente.valor as indicador_falha_recorrente
+        from {{ ref("veiculo_regularidade_temperatura_dia") }}
+        where {{ incremental_filter }}
+    ),
     indicadores as (
         select
             data,
             id_veiculo,
             id_viagem,
             ano_fabricacao,
-            json_value(indicadores, '$.indicador_regularidade_ar_condicionado.valor') as indicador_regularidade_ar_condicionado,
-            json_value(indicadores, '$.indicador_falha_recorrente.valor') as indicador_falha_recorrente,
-            json_value(indicadores, '$.indicador_temperatura_descartada.valor') as indicador_temperatura_descartada,
-            json_value(indicadores, '$.indicador_temperatura_transmitida.valor') as indicador_temperatura_transmitida,
-            json_value(indicadores, '$.indicador_ar_condicionado.valor') as indicador_ar_condicionado,
-            json_value(indicadores, '$.indicador_temperatura_regular.valor') as indicador_temperatura_regular,
-            json_value(indicadores, '$.indicador_regularidade_ar_condicionado.valor') as indicador_regularidade_ar_condicionado
+            safe_cast(json_value(indicadores, '$.indicador_ar_condicionado.valor') as bool) as indicador_ar_condicionado,
+            safe_cast(json_value(indicadores, '$.indicador_regularidade_ar_condicionado.valor') as bool) as indicador_regularidade_ar_condicionado
         from {{ model }}
+    ),
+    indicadores_completo as (
+        select
+            i.data,
+            i.id_veiculo,
+            i.id_viagem,
+            i.ano_fabricacao,
+            i.indicador_ar_condicionado,
+            i.indicador_regularidade_ar_condicionado,
+            t.indicador_temperatura_descartada,
+            t.indicador_temperatura_transmitida,
+            t.indicador_temperatura_regular,
+            v.indicador_falha_recorrente
+        from indicadores i
+        left join indicadores_aux_viagem_temperatura t using (data, id_veiculo, id_viagem)
+        left join indicadores_veiculo_regularidade_temperatura_dia v using (data, id_veiculo)
     ),
     teste_1_falha as (
         select
@@ -23,7 +57,7 @@ with
             id_viagem,
             ano_fabricacao,
             'Quando deveria `indicador_regularidade_ar_condicionado` ser TRUE, mas não é' as falha
-        from indicadores
+        from indicadores_completo
         where
             (
                 -- Quando deveria `indicador_regularidade_ar_condicionado` ser TRUE, mas não é
@@ -44,13 +78,14 @@ with
             id_viagem,
             ano_fabricacao,
             'Quando `indicador_regularidade_ar_condicionado` deveria ser FALSE, mas não é' as falha
-        from indicadores
+        from indicadores_completo
         where
             (
                 -- Quando `indicador_regularidade_ar_condicionado` deveria ser FALSE, mas não é
-                (ano_fabricacao <= 2019 or data >= date("{{ var('DATA_SUBSIDIO_V19_INICIO') }}"))
+                (ano_fabricacao <= 2019
+                 or data >= date("{{ var('DATA_SUBSIDIO_V19_INICIO') }}"))
                 and (
-                    or indicador_falha_recorrente
+                    indicador_falha_recorrente
                     or indicador_temperatura_descartada
                     or not indicador_temperatura_transmitida
                     or not indicador_temperatura_regular
@@ -65,7 +100,7 @@ with
             id_viagem,
             ano_fabricacao,
             'Quando deveria ser NULL de acordo com o Art 2º-E da resolução, mas não é' as falha
-        from indicadores
+        from indicadores_completo
         where
             (
                 -- Quando deveria ser NULL de acordo com o Art 2º-E da resolução, mas não é
