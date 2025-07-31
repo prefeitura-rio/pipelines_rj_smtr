4f8b4d1ca1123db004c74ea23c5b981a3e31ffba
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-07-31 20:54:49 -0300

    Cria modelo `viagem_regularidade_temperatura` (#703)
    * cria modelo temperatura_inmet
    
    * teste freshness meteorologia_inmet
    
    * add ano_fabricacao na viagem_classificada
    
    * altera variaveis de data
    
    * inicia a construção do modelo auxiliar de viagem temperatura
    
    * adiciona data variavel do inicio das regras de climatizacao
    
    * cria veiculo_regularidade_temperatura_dia
    
    * move modelo para staging e corrige DATA_SUBSIDIO_V16_INICIO
    
    * add lógica indicador_estado_equipamento_aberto
    
    * continuação da construção do modelo auxiliar de viagem temperatura
    
    * ajustes viagem_transacao_aux
    
    * ajusta estrutura parcial do modelo auxiliar de viagem temperatura
    
    * sqlfmt
    
    * ajustes coderabbit
    
    * fix jinja
    
    * adiciona estrutura final provisória do modelo auxiliar de viagem temperatura
    
    * ajustes finais no modelo auxiliar de viagem e temperatura
    
    * ajustes coderabbit
    
    * fix virgulas
    
    * sqlfmt
    
    * ajusta filtragem por datas e utilizar incremental filter
    
    * correções nos modelos temperatura_inmet e aux_viagem_temperatura
    
    * ajustes modelo veiculo_regularidade_temperatura_dia
    
    * cria modelo viagem_regularidade_temperatura
    
    * ajustes nos modelos
    
    * ajustes
    
    * cria modelo aux_veiculo_falha_ar_condicionado
    
    * add veiculo_dia no selector monitoramento_veiculo
    
    * cria v1 e v2 da viagem_transacao_aux
    
    * add ano_fabricacao na veiculo_dia
    
    * altera monitoramento_viagem_transacao
    
    * add schemas
    
    * add descrições para as CTEs do modelo auxiliar de viagem  temperatura
    
    * Corrige datas subsídio
    
    * Ajustes e add testes
    
    * ajusta variavel data de filtragem do modelo
    
    * trigger coderabbit review
    
    * Ajustes nos modelos
    
    * add descrição de indicadores do modelo de veiculo regularidade temperatura dia
    
    * ajusta filtro viagem_transacao_aux_v1
    
    * Altera variáveis dbt
    
    * add teste consistência lógica e ajustes nos schemas
    
    * add testes de validação cruzada e ajustes
    
    * ajustes finais no schema e modelos de climatizacao
    
    * altera selector apuracao_subsidio_v9 para não executar o modelo aux_viagem_temperatura
    
    * remove virgulas
    
    * add changelogs
    
    * ajusta changelog
    
    * registra flows
    
    ---------
    
    Co-authored-by: Adriano Neto <adrianoneto27@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: hellcassius <caiorogerio.santos@gmail.com>

---
+++ queries/models/monitoramento/temperatura_inmet.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        materialized="incremental",
+        partition_by={
+            "field": "data",
+            "data_type": "date",
+            "granularity": "day",
+        },
+        incremental_strategy="insert_overwrite",
+    )
+}}
+
+select data_particao as data, horario as hora, id_estacao, temperatura
+from {{ source("clima_estacao_meteorologica", "meteorologia_inmet") }}
+where
+    data_particao >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+    {% if is_incremental() %}
+        and data_particao between date("{{ var('date_range_start') }}") and date(
+            "{{ var('date_range_end') }}"
+        )
+    {% endif %}
