96d477fdcaeb1841588ad9314b02d7b7d322b0cd
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-08-04 15:36:32 -0300

    fmt

--- queries/models/cadastro/staging/aux_linha_tarifa.sql
+++ queries/models/cadastro/staging/aux_linha_tarifa.sql
@@ -1,11 +1,11 @@
 {{ config(materialized="ephemeral") }}
 
-SELECT
-  cd_linha,
-  vl_tarifa_ida AS tarifa_ida,
-  vl_tarifa_volta AS tarifa_volta,
-  dt_inicio_validade,
-  lead(dt_inicio_validade) OVER (
-    PARTITION BY cd_linha ORDER BY nr_sequencia
-  ) AS data_fim_validade
-FROM {{ ref("staging_linha_tarifa") }}
+select
+    cd_linha,
+    vl_tarifa_ida as tarifa_ida,
+    vl_tarifa_volta as tarifa_volta,
+    dt_inicio_validade,
+    lead(dt_inicio_validade) over (
+        partition by cd_linha order by nr_sequencia
+    ) as data_fim_validade
+from {{ ref("staging_linha_tarifa") }}
