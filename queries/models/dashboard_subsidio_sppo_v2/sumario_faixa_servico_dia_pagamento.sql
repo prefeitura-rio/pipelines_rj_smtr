8f8d87f9f55f6457308a925053b99ffa45524d5a
vtr363 <victor.m.rocha.vr@gmail.com>
2025-08-07 20:47:03 -0300

    atualiza schema

--- queries/models/dashboard_subsidio_sppo_v2/sumario_faixa_servico_dia_pagamento.sql
+++ queries/models/dashboard_subsidio_sppo_v2/sumario_faixa_servico_dia_pagamento.sql
@@ -5,11 +5,15 @@
         incremental_strategy="insert_overwrite",
     )
 }}
-
-select *
-from {{ ref("sumario_faixa_servico_dia_pagamento_v2") }}
-where data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
-full outer union all by name
-select *
-from {{ ref("sumario_faixa_servico_dia_pagamento_v1") }}
-where data < date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+with
+    sumario_faixa_servico_dia_pagamento as (
+        select *
+        from {{ ref("sumario_faixa_servico_dia_pagamento_v2") }}
+        where data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}") 
+        full outer union all by name
+        select *
+        from {{ ref("sumario_faixa_servico_dia_pagamento_v1") }}
+        where data < date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+    )
+select *, '{{ invocation_id }}' as id_execucao_dbt
+from sumario_faixa_servico_dia_pagamento
