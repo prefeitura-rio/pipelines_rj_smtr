00a4fd6611a7d4fcf6b7d72452961f4ff14c7b96
Rodrigo Cunha <engtransportes.rodrigocunha@gmail.com>
2025-08-07 21:27:30 -0300

    adiciona schema

--- queries/models/financeiro/subsidio_faixa_servico_dia_tipo_viagem.sql
+++ queries/models/financeiro/subsidio_faixa_servico_dia_tipo_viagem.sql
@@ -8,12 +8,12 @@
 with
     subsidio_faixa_servico_dia_tipo_viagem as (
         select *
-        from {{ ref("subsidio_faixa_servico_dia_tipo_viagem_v1") }}
-        where data < date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
-        full outer union all by name
-        select *
         from {{ ref("subsidio_faixa_servico_dia_tipo_viagem_v2") }}
         where data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+        full outer union all by name
+        select *
+        from {{ ref("subsidio_faixa_servico_dia_tipo_viagem_v1") }}
+        where data < date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
     )
 select *, '{{ invocation_id }}' as id_execucao_dbt
 from subsidio_faixa_servico_dia_tipo_viagem
