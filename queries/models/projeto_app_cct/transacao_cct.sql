d7444cc0b139efbf5f3118bccb5dcdcecf5d3956
Adriano Neto <adrianoneto27@gmail.com>
2025-08-11 16:18:02 -0300

    sqlfmt

--- queries/models/projeto_app_cct/transacao_cct.sql
+++ queries/models/projeto_app_cct/transacao_cct.sql
@@ -1,13 +1,11 @@
-SELECT
-  id_transacao,
-  data,
-  datetime_transacao,
-  consorcio,
-  valor_pagamento,
-  id_ordem_pagamento,
-  id_ordem_pagamento_consorcio_operador_dia,
-  datetime_ultima_atualizacao
-FROM
-  {{ ref("transacao") }}
-WHERE
-  id_ordem_pagamento_consorcio_operador_dia IS NOT null
+select
+    id_transacao,
+    data,
+    datetime_transacao,
+    consorcio,
+    valor_pagamento,
+    id_ordem_pagamento,
+    id_ordem_pagamento_consorcio_operador_dia,
+    datetime_ultima_atualizacao
+from {{ ref("transacao") }}
+where id_ordem_pagamento_consorcio_operador_dia is not null
