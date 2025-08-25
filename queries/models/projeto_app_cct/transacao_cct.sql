3575cf37799826a58e4b0b3fd9df1bd3b0aae7fb
Adriano Neto <adrianoneto27@gmail.com>
2025-08-25 14:48:50 -0300

    Filtra transacaoes de vans e adiciona coluna tipo_transacao_jae

--- queries/models/projeto_app_cct/transacao_cct.sql
+++ queries/models/projeto_app_cct/transacao_cct.sql
@@ -4,9 +4,10 @@ select
     datetime_transacao,
     consorcio,
     tipo_transacao,
+    tipo_transacao_jae,
     valor_pagamento,
     id_ordem_pagamento,
     id_ordem_pagamento_consorcio_operador_dia,
     datetime_ultima_atualizacao
 from {{ ref("transacao") }}
-where id_ordem_pagamento_consorcio_operador_dia is not null
+where id_ordem_pagamento_consorcio_operador_dia is not null and modo = 'Van'
