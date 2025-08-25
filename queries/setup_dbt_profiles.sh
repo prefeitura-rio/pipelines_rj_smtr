be88e1686e71eb4ce7bfdfae6d6afa639f198760
Caio R S dos Santos <caiorogerio.santos@gmail.com>
2025-08-25 14:14:39 -0300

    [FEAT] Adiciona sqlformat (#731)
    * add dbt checks workflow, add sqlfmt to pre-commit
    
    * add dbt deps to workflow
    
    * add setup profiles shell script, fix pre commit
    
    * test sqlfmt
    
    * fix setup profile
    
    * add dbt user env var
    
    * fix conflicts
    
    * adjust gitignore, restore generate-docs.sh
    
    * fix wrong merge on dbt model
    
    * fix dbt-checks workflow and refactor setup pŕofiles script
    
    * fix cd-docs
    
    * fix cd-docs
    
    * test sqlfmt
    
    * fix setup profile
    
    * fix setup profile
    
    * fix setup profile
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * trigger sqlfmt
    
    * trigger sqlfmt
    
    * fix workflow
    
    * change setup-gcloud version
    
    * remove continue on error
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ queries/setup_dbt_profiles.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+mkdir -p ./credentials-dev
+mkdir -p ./credentials-prod
+mkdir -p ./profiles
+
+echo "Mounting files from env..."
+
+echo "$1" | base64 --decode > ./credentials-dev/dev.json
+echo "$1" | base64 --decode > ./credentials-prod/prod.json
+
+echo """
+queries:
+  target: dev
+  outputs:
+    dev:
+      type: bigquery
+      method: service-account
+      project: rj-smtr
+      dataset: dbt
+      location: US
+      threads: 2
+      keyfile: $PWD/credentials-dev/dev.json
+    prod:
+      type: bigquery
+      method: service-account
+      project: rj-smtr
+      dataset: dbt
+      location: US
+      threads: 2
+      keyfile: $PWD/credentials-prod/prod.json""" > profiles/profiles.yml
+
+cat profiles/profiles.yml
+
+dbt deps --profiles-dir profiles
