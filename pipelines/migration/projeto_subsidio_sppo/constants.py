3c17d20b66ad63e2f1fa098b7df8876366595355
Adriano Neto <adrianoneto27@gmail.com>
2025-09-04 11:01:43 -0300

    Ajusta estrutura de chaves do dicionario

--- pipelines/migration/projeto_subsidio_sppo/constants.py
+++ pipelines/migration/projeto_subsidio_sppo/constants.py
@@ -716,100 +716,86 @@ class constants(Enum):  # pylint: disable=c0103
         },
     }
 
-    SUBSIDIO_SPPO_PRE_CHECKS_LIST = {
-        "sppo_realocacao": {
-            "check_gps_capture__sppo_realocacao": {
-                "description": "Todos os dados de realocação foram capturados"
-            }
-        },
-        "sppo_registros": {
-            "check_gps_capture__sppo_registros": {
-                "description": "Todos os dados de GPS foram capturados"
-            }
-        },
-        "gps_sppo": {
-            "check_gps_treatment__gps_sppo": {
-                "description": "Todos os dados de GPS foram devidamente tratados"
-            },
-            "dbt_utils.unique_combination_of_columns__gps_sppo": {
-                "description": "Todos os registros são únicos"
-            },
-        },
-        "sppo_veiculo_dia": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "dbt_utils.unique_combination_of_columns__data_id_veiculo__sppo_veiculo_dia": {
-                "description": "Todos os registros são únicos"
-            },
-            "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart__sppo_veiculo_dia": {
-                "description": "Todas as datas possuem dados"
-            },
-        },
-        "veiculo_dia": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart__veiculo_dia": {
-                "description": "Todas as datas possuem dados"
-            },
-            "dbt_utils.unique_combination_of_columns__data_id_veiculo__veiculo_dia": {
-                "description": "Todos os registros são únicos"
-            },
-            "test_check_veiculo_lacre__veiculo_dia": {
-                "description": "Todos os veículos lacrados têm dados consistentes entre `veiculo_dia` e `veiculo_fiscalizacao_lacre`"  # noqa
-            },
-        },
-        "tecnologia_servico": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "dbt_utils.unique_combination_of_columns__tecnologia_servico": {
-                "description": "Todos os registros são únicos"
-            },
-        },
-        "viagem_planejada": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "dbt_utils.accepted_range": {
-                "description": "Todos os valores da coluna `{column_name}` maiores ou iguais a zero"
-            },
-            "dbt_utils.unique_combination_of_columns__viagem_planejada": {
-                "description": "Todos os registros são únicos"
-            },
-            "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart": {
-                "description": "Todas as datas possuem dados"
-            },
-            "accepted_values": {
-                "description": "Todos os valores da coluna `{column_name}` são aceitos"
-            },
-            "dbt_expectations.expect_table_aggregation_to_equal_other_table__viagem_planejada": {
-                "description": "Todos os dados de `tipo_os` correspondem 1:1 entre as tabelas `subsidio_data_versao_efetiva` e `viagem_planejada`."  # noqa
-            },
-            "dbt_utils.relationships_where__servico__viagem_planejada": {
-                "description": "Todos os serviços planejados possuem tecnologia permitida."  # noqa
-            },
-            "check_km_planejada": {
-                "description": "Todas as viagens possuem `km_planejada` correspondente à OS"
-            },
-            "check_partidas_planejadas": {
-                "description": "Todas as viagens possuem `partidas_total_planejada` correspondente à OS"
-            },
-        },
-        "viagens_remuneradas": {
-            "teto_viagens__viagens_remuneradas": {
-                "description": "Todas as viagens foram corretamente identificadas dentro das regras de limite"
-            },
-        },
-        "transacao": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "unique": {"description": "Todos os registros são únicos"},
-        },
-        "transacao_riocard": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "unique": {"description": "Todos os registros são únicos"},
-        },
-        "gps_validador": {
-            "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
-            "unique": {"description": "Todos os registros são únicos"},
-        },
-    }
-
     SUBSIDIO_SPPO_PRE_CHECKS_LIST = (
-        SUBSIDIO_SPPO_PRE_CHECKS_LIST
+        {
+            "sppo_realocacao": {
+                "check_gps_capture__sppo_realocacao": {
+                    "description": "Todos os dados de realocação foram capturados"
+                }
+            },
+            "sppo_registros": {
+                "check_gps_capture__sppo_registros": {
+                    "description": "Todos os dados de GPS foram capturados"
+                }
+            },
+            "gps_sppo": {
+                "check_gps_treatment__gps_sppo": {
+                    "description": "Todos os dados de GPS foram devidamente tratados"
+                },
+                "dbt_utils.unique_combination_of_columns__gps_sppo": {
+                    "description": "Todos os registros são únicos"
+                },
+            },
+            "sppo_veiculo_dia": {
+                "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
+                "dbt_utils.unique_combination_of_columns__data_id_veiculo__sppo_veiculo_dia": {
+                    "description": "Todos os registros são únicos"
+                },
+                "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart__sppo_veiculo_dia": {
+                    "description": "Todas as datas possuem dados"
+                },
+            },
+            "veiculo_dia": {
+                "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
+                "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart__veiculo_dia": {
+                    "description": "Todas as datas possuem dados"
+                },
+                "dbt_utils.unique_combination_of_columns__data_id_veiculo__veiculo_dia": {
+                    "description": "Todos os registros são únicos"
+                },
+                "test_check_veiculo_lacre__veiculo_dia": {
+                    "description": "Todos os veículos lacrados têm dados consistentes entre `veiculo_dia` e `veiculo_fiscalizacao_lacre`"  # noqa
+                },
+            },
+            "tecnologia_servico": {
+                "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
+                "dbt_utils.unique_combination_of_columns__tecnologia_servico": {
+                    "description": "Todos os registros são únicos"
+                },
+            },
+            "viagem_planejada": {
+                "not_null": {"description": "Todos os valores da coluna `{column_name}` não nulos"},
+                "dbt_utils.accepted_range": {
+                    "description": "Todos os valores da coluna `{column_name}` maiores ou iguais a zero"
+                },
+                "dbt_utils.unique_combination_of_columns__viagem_planejada": {
+                    "description": "Todos os registros são únicos"
+                },
+                "dbt_expectations.expect_row_values_to_have_data_for_every_n_datepart": {
+                    "description": "Todas as datas possuem dados"
+                },
+                "accepted_values": {
+                    "description": "Todos os valores da coluna `{column_name}` são aceitos"
+                },
+                "dbt_expectations.expect_table_aggregation_to_equal_other_table__viagem_planejada": {
+                    "description": "Todos os dados de `tipo_os` correspondem 1:1 entre as tabelas `subsidio_data_versao_efetiva` e `viagem_planejada`."  # noqa
+                },
+                "dbt_utils.relationships_where__servico__viagem_planejada": {
+                    "description": "Todos os serviços planejados possuem tecnologia permitida."  # noqa
+                },
+                "check_km_planejada": {
+                    "description": "Todas as viagens possuem `km_planejada` correspondente à OS"
+                },
+                "check_partidas_planejadas": {
+                    "description": "Todas as viagens possuem `partidas_total_planejada` correspondente à OS"
+                },
+            },
+            "viagens_remuneradas": {
+                "teto_viagens__viagens_remuneradas": {
+                    "description": "Todas as viagens foram corretamente identificadas dentro das regras de limite"
+                },
+            },
+        }
         | monitoramento_constants.GPS_VALIDADOR_POST_CHECKS_LIST.value
         | bilhetagem_constants.TRANSACAO_POST_CHECKS_LIST.value
     )
