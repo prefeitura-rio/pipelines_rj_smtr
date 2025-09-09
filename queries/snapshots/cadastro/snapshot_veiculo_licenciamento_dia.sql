485c15e34ab7801531f7a922b6a24e7d4616c64a
Duartejanaina <janainacduarte@gmail.com>
2025-09-09 15:24:59 -0300

    Cria o snapshot veiculo_licenciamento_dia

---
+++ queries/snapshots/cadastro/snapshot_veiculo_licenciamento_dia.sql
@@ -0,0 +1,21 @@
+{% snapshot snapshot_veiculo_licenciamento_dia %}
+
+    {{
+        config(
+            target_schema="cadastro_staging",
+            unique_key="concat(data,'-', data_processamento, '-', id_veiculo,'-',placa)"
+            strategy="timestamp",
+            updated_at="timestamp_ultima_atualizacao",
+            invalidate_hard_deletes=True,
+            partition_by={"field": "data", "data_type": "date", "granularity": "day"},
+        )
+    }}
+
+    select
+        * except (versao),
+        timestamp(
+            datetime_ultima_atualizacao, "America/Sao_Paulo"
+        ) as timestamp_ultima_atualizacao
+    from {{ ref("veiculo_licenciamento_dia") }}
+
+{% endsnapshot %}
