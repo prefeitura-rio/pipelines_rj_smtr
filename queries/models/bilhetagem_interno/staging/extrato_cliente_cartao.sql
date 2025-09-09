bdfc11b9236e618bd1552940f8c8e5f0dda5eb42
adriano <adrianoneto27@gmail.com>
2025-09-09 17:02:05 -0300

    Ajustes coderabbit

--- queries/models/bilhetagem_interno/staging/extrato_cliente_cartao.sql
+++ queries/models/bilhetagem_interno/staging/extrato_cliente_cartao.sql
@@ -11,23 +11,23 @@ with
             l.ds_tipo_movimento as tipo_movimento,
             l.timestamp_captura as datetime_captura,
             case
-                when regexp_contains(id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
-                then split(id_conta, ".")[offset(3)]
-            end as nr_logico_midia_tratado,
-            timestamp_captura
-        from {{ ref("staging_lancamento") }}
-        -- from `rj-smtr-dev.adriano__bilhetagem_interno_staging.lancamento` l
+                when regexp_contains(l.id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
+                then split(l.id_conta, ".")[offset(3)]
+            end as nr_logico_midia
+        from {{ ref("staging_lancamento") }} as l
+        -- from `rj-smtr-dev.adriano__bilhetagem_interno_staging.lancamento` 
         left join
             `rj-smtr.cadastro_interno.cliente_jae` j on j.id_cliente = l.cd_cliente
         where
             (
-                regexp_contains(id_conta, r'^2\.2\.1\.[A-Za-z0-9]+\.(1|2|6)$')
-                or regexp_contains(id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
+                regexp_contains(l.id_conta, r'^2\.2\.1\.[A-Za-z0-9]+\.(1|2|6)$')
+                or regexp_contains(l.id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
             )
-            and data = '2025-09-05'
+        -- and data = '2025-09-05'
         qualify
             row_number() over (
-                partition by id_lancamento, id_conta order by timestamp_captura desc
+                partition by l.id_lancamento, l.id_conta
+                order by l.timestamp_captura desc
             )
             = 1
     )
