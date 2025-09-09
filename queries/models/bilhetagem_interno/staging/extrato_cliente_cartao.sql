c03f99c1e6a99d403e1f1ebf411173d46505fb3d
adriano <adrianoneto27@gmail.com>
2025-09-09 14:36:46 -0300

    Ajusta tratamento no modelo lancamento e adiciona tratamentos finais do modelo de extrato

--- queries/models/bilhetagem_interno/staging/extrato_cliente_cartao.sql
+++ queries/models/bilhetagem_interno/staging/extrato_cliente_cartao.sql
@@ -1,43 +1,35 @@
 with
     extrato_cliente as (
         select
-            array_to_string(
-                array_concat(
-                    [
-                        split(id_conta, ".")[offset(0)],
-                        split(id_conta, ".")[offset(1)],
-                        split(id_conta, ".")[offset(2)]
-                    ],
-                    [split(id_conta, ".")[offset(4)]]
-                ),
-                "."
-            ) as id_conta,
-            cd_cliente as id_cliente,
-            id_lancamento,
-            data,
-            vl_lancamento as valor_lancamento,
-            split(id_conta, ".")[offset(3)] as nr_logico_midia_tratado,
+            l.id_conta,
+            l.cd_cliente as id_cliente,
+            j.nome as nome_cliente,
+            j.documento as nome_documento,
+            l.id_lancamento,
+            l.dt_lancamento as data_lancamento,
+            l.vl_lancamento as valor_lancamento,
+            l.ds_tipo_movimento as tipo_movimento,
+            l.timestamp_captura as datetime_captura,
+            case
+                when regexp_contains(id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
+                then split(id_conta, ".")[offset(3)]
+            end as nr_logico_midia_tratado,
             timestamp_captura
-        from `rj-smtr-dev.adriano__bilhetagem_interno_staging.lancamento`
+        from {{ ref("staging_lancamento") }}
+        -- from `rj-smtr-dev.adriano__bilhetagem_interno_staging.lancamento` l
+        left join
+            `rj-smtr.cadastro_interno.cliente_jae` j on j.id_cliente = l.cd_cliente
         where
-            regexp_contains(id_conta, r'^2\.2\.1\.[A-Za-z0-9]+\.(1|2|6)$')
-            or regexp_contains(id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
-            and data = '2025-09-05'
-    ),
-    extrato_cliente_deduplicada as (
-        select * except (rn)
-        from
             (
-                select
-                    * except (timestamp_captura),
-                    row_number() over (
-                        partition by id_lancamento, id_conta
-                        order by timestamp_captura desc
-                    ) as rn
-                from extrato_cliente
-            -- {% if is_incremental() -%} where {{ incremental_filter }} {%- endif %}
+                regexp_contains(id_conta, r'^2\.2\.1\.[A-Za-z0-9]+\.(1|2|6)$')
+                or regexp_contains(id_conta, r'^2\.2\.3\.[A-Za-z0-9]+\.1$')
             )
-        where rn = 1
+            and data = '2025-09-05'
+        qualify
+            row_number() over (
+                partition by id_lancamento, id_conta order by timestamp_captura desc
+            )
+            = 1
     )
 select *
-from extrato_cliente_deduplicada
+from extrato_cliente
--- queries/models/bilhetagem_interno/staging/staging_lancamento.sql
+++ queries/models/bilhetagem_interno/staging/staging_lancamento.sql
@@ -31,8 +31,8 @@ select
     safe_cast(json_value(content, '$.ds_tipo_conta') as string) as ds_tipo_conta,
     safe_cast(json_value(content, '$.id_tipo_moeda') as string) as id_tipo_moeda,
     safe_cast(json_value(content, '$.tipo_moeda') as string) as tipo_moeda,
-    replace(
-        safe_cast(json_value(content, '$.cd_cliente') as string), '.0', ''
+    regexp_replace(
+        safe_cast(json_value(content, '$.cd_cliente') as string), r'\.0$', ''
     ) as cd_cliente,
     safe_cast(json_value(content, '$.nr_logico_midia') as string) as nr_logico_midia
 from {{ source("source_jae", "lancamento") }}
