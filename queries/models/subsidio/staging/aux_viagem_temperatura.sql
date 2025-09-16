693c42f8a3c330f9957075988fca50ee348cab24
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-09-16 20:30:32 -0300

    remove alias

--- queries/models/subsidio/staging/aux_viagem_temperatura.sql
+++ queries/models/subsidio/staging/aux_viagem_temperatura.sql
@@ -3,7 +3,6 @@
         materialized="incremental",
         partition_by={"field": "data", "data_type": "date", "granularity": "day"},
         incremental_strategy="insert_overwrite",
-        alias="aux_viagem_temperatura_teste",
     )
 }}
 
