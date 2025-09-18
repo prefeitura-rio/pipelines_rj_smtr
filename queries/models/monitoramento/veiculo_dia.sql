d6bb1ed103b896bc321da3f4c5d26f4b55814fa6
vtr363 <victor.m.rocha.vr@gmail.com>
2025-09-18 14:59:02 -0300

    atualiza changelog

--- queries/models/monitoramento/veiculo_dia.sql
+++ queries/models/monitoramento/veiculo_dia.sql
@@ -58,8 +58,8 @@ with
                     and data_processamento between "2025-08-01" and "2025-08-26"
                 )
                 or (
-                    data between "2025-09-01" and "2025-09-18"  -- Exceção para lacres adicionados após o prazo em 2025-09-Q1
-                    and data_processamento between "2025-09-01" and "2025-09-18"
+                    data between "2025-09-01" and "2025-09-19"  -- Exceção para lacres adicionados após o prazo em 2025-09-Q1
+                    and data_processamento between "2025-09-01" and "2025-09-19"
                 )
             )
             {% if is_incremental() %}
