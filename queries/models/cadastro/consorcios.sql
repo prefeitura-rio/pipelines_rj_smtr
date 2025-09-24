968bafbc366d17dd54bef6050d62943ed983bcbf
adriano <adrianoneto27@gmail.com>
2025-09-24 10:04:31 -0300

    Sqlfmt

--- queries/models/cadastro/consorcios.sql
+++ queries/models/cadastro/consorcios.sql
@@ -1,58 +1,47 @@
-{{
-  config(
-    materialized="table"
-  )
-}}
+{{ config(materialized="table") }}
 
-WITH stu AS (
-  SELECT
-      perm_autor AS id_consorcio,
-      cnpj,
-      processo,
-      data_registro,
-      razao_social,
-      CASE
-        {% for id_stu, id_jae in var("ids_consorcios").items() %}
-          WHEN perm_autor = {{ id_stu }} THEN {{ id_jae }}
-        {% endfor %}
-      END AS cd_consorcio_jae
-  FROM
-    {{ ref("staging_operadora_empresa") }} AS stu
-  WHERE
-    perm_autor IN ({{ var("ids_consorcios").keys()|join(", ") }})
-), consorcio AS (
-  SELECT
-    COALESCE(s.id_consorcio, j.cd_consorcio) AS id_consorcio,
-    CASE
-      WHEN s.id_consorcio = '221000050' THEN "Consórcio BRT"
-      ELSE j.nm_consorcio
-    END AS consorcio,
-    s.cnpj,
-    s.razao_social,
-    s.id_consorcio AS id_consorcio_stu,
-    j.cd_consorcio AS id_consorcio_jae,
-    s.processo AS id_processo
-  FROM {{ ref("staging_consorcio") }} AS j
-  FULL OUTER JOIN
-    stu AS s
-  ON
-    j.cd_consorcio = s.cd_consorcio_jae
-)
-SELECT
-  c.id_consorcio,
-  c.consorcio,
-  m.modo,
-  c.cnpj,
-  c.razao_social,
-  c.id_consorcio_stu,
-  c.id_consorcio_jae,
-  c.id_processo
-FROM consorcio c
-LEFT JOIN
-  {{ ref("aux_consorcio_modo") }} AS cm
-USING (id_consorcio)
-LEFT JOIN
-  {{ ref("modos") }} AS m
-ON
-  m.id_modo = cm.id_modo
-  AND cm.fonte_id_modo = m.fonte
+with
+    stu as (
+        select
+            perm_autor as id_consorcio,
+            cnpj,
+            processo,
+            data_registro,
+            razao_social,
+            case
+                {% for id_stu, id_jae in var("ids_consorcios").items() %}
+                    when perm_autor = {{ id_stu }} then {{ id_jae }}
+                {% endfor %}
+            end as cd_consorcio_jae
+        from {{ ref("staging_operadora_empresa") }} as stu
+        where perm_autor in ({{ var("ids_consorcios").keys() | join(", ") }})
+    ),
+    consorcio as (
+        select
+            coalesce(s.id_consorcio, j.cd_consorcio) as id_consorcio,
+            case
+                when s.id_consorcio = '221000050'
+                then "Consórcio BRT"
+                else j.nm_consorcio
+            end as consorcio,
+            s.cnpj,
+            s.razao_social,
+            s.id_consorcio as id_consorcio_stu,
+            j.cd_consorcio as id_consorcio_jae,
+            s.processo as id_processo
+        from {{ ref("staging_consorcio") }} as j
+        full outer join stu as s on j.cd_consorcio = s.cd_consorcio_jae
+    )
+select
+    c.id_consorcio,
+    c.consorcio,
+    m.modo,
+    c.cnpj,
+    c.razao_social,
+    c.id_consorcio_stu,
+    c.id_consorcio_jae,
+    c.id_processo
+from consorcio c
+left join {{ ref("aux_consorcio_modo") }} as cm using (id_consorcio)
+left join
+    {{ ref("modos") }} as m on m.id_modo = cm.id_modo and cm.fonte_id_modo = m.fonte
--- queries/models/cadastro/staging/aux_consorcio_modo.sql
+++ queries/models/cadastro/staging/aux_consorcio_modo.sql
@@ -1,6 +1,3 @@
-{{
-    config(
-        materialized="table"
-    )
-}}
-select * from {{ source("cadastro_staging", "consorcio_modo") }}
+{{ config(materialized="table") }}
+select *
+from {{ source("cadastro_staging", "consorcio_modo") }}
