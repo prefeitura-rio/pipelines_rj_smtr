b6f81afc6939d9ccf403691cfcd91fc7972ae859
Duartejanaina <janainacduarte@gmail.com>
2025-09-24 15:52:19 -0300

    Alterações do full outer

--- queries/models/financeiro_interno/subsidio_faixa_servico_dia_tipo_viagem.sql
+++ queries/models/financeiro_interno/subsidio_faixa_servico_dia_tipo_viagem.sql
@@ -9,11 +9,14 @@ with
     subsidio_faixa_servico_dia_tipo_viagem as (
         select *
         from {{ ref("subsidio_faixa_servico_dia_tipo_viagem_v2") }}
-        where data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}") full outer
-        union all by name
+        where data >= date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
+        --fmt:off
+        full outer union all by name
+        --fmt:on
         select *
         from {{ ref("subsidio_faixa_servico_dia_tipo_viagem_v1") }}
         where data < date("{{ var('DATA_SUBSIDIO_V17_INICIO') }}")
     )
 select *, '{{ invocation_id }}' as id_execucao_dbt
 from subsidio_faixa_servico_dia_tipo_viagem
+    
