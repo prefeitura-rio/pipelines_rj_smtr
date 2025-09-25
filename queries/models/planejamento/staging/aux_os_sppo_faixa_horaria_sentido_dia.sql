38cb2819a23f76f9f8352365518f7fd97b483d4a
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-09-25 17:19:29 -0300

    Corrige materialização da viagem_informada e viagem_validacao (#799)
    * corriige nome da coluna timestamp_gps para datetime_gps
    
    * add filtro data_viagem is not null na partitions_query
    
    * add changelog
    
    * registra flow
    
    * ajusta incremental_filter
    
    * cria indicador_dados_validos
    
    * cria indicador_dados_validos
    
    * add datetime_captura_viagem
    
    * altera descrição datetime_partida
    
    * extrai data do datetime_partida
    
    * atualiza indicador_viagem_sobreposta
    
    * corrige merge changelog
    
    * ajuste changelog
    
    * sqlfmt
    
    * ajustes
    
    * comentários
    
    * correções
    
    * add ordem_servico_faixa_horaria_sentido na aux_os_sppo_faixa_horaria_sentido_dia
    
    * registra flow
    
    * corrige changelog
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- queries/models/planejamento/staging/aux_os_sppo_faixa_horaria_sentido_dia.sql
+++ queries/models/planejamento/staging/aux_os_sppo_faixa_horaria_sentido_dia.sql
@@ -4,16 +4,20 @@
     )
 }}
 
+{% set incremental_filter %}
+    data between date('{{ var("date_range_start") }}') and date('{{ var("date_range_end") }}')
+{% endset %}
+
+{% set calendario = ref("calendario") %}
+{# {% set calendario = "rj-smtr.planejamento.calendario" %} #}
 with
     os as (
         select
             *,
             split(faixa_horaria_inicio, ":") as faixa_horaria_inicio_parts,
             split(faixa_horaria_fim, ":") as faixa_horaria_fim_parts
-        {# from {{ ref("aux_ordem_servico_faixa_horaria") }} #}
-        {# from `rj-smtr.planejamento_staging.aux_ordem_servico_faixa_horaria` #}
+        {# from `rj-smtr.planejamento.ordem_servico_faixa_horaria` #}
         from {{ ref("ordem_servico_faixa_horaria") }}
-    {# from `rj-smtr.planejamento.ordem_servico_faixa_horaria` #}
     ),
     os_tratamento_horario as (
         select
@@ -48,77 +52,48 @@ with
             partidas_volta,
             quilometragem,
             partidas
-        from {{ ref("calendario") }} c
-        {# from `rj-smtr.planejamento.calendario` c #}
+        from {{ calendario }} as c
         join
-            os_tratamento_horario o using (
+            os_tratamento_horario as o using (
                 feed_version, feed_start_date, tipo_dia, tipo_os
             )
+        where {{ incremental_filter }}
     ),
     faixas_agregadas as (
         select
             * except (partidas_ida, partidas_volta, partidas, quilometragem),
             case
-                when
-                    lag(faixa_horaria_inicio) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    = faixa_horaria_inicio
-                then
-                    lag(partidas_ida) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    + partidas_ida
+                when lag(faixa_horaria_inicio) over (win) = faixa_horaria_inicio
+                then lag(partidas_ida) over (win) + partidas_ida
                 else partidas_ida
             end as partidas_ida,
             case
-                when
-                    lag(faixa_horaria_inicio) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    = faixa_horaria_inicio
-                then
-                    lag(partidas_volta) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    + partidas_volta
+                when lag(faixa_horaria_inicio) over (win) = faixa_horaria_inicio
+                then lag(partidas_volta) over (win) + partidas_volta
                 else partidas_volta
             end as partidas_volta,
             case
-                when
-                    lag(faixa_horaria_inicio) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    = faixa_horaria_inicio
-                then
-                    lag(partidas) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    + partidas
+                when lag(faixa_horaria_inicio) over (win) = faixa_horaria_inicio
+                then lag(partidas) over (win) + partidas
                 else partidas
             end as partidas,
             case
-                when
-                    lag(faixa_horaria_inicio) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    = faixa_horaria_inicio
-                then
-                    lag(quilometragem) over (
-                        partition by servico order by data, faixa_horaria_inicio
-                    )
-                    + quilometragem
+                when lag(faixa_horaria_inicio) over (win) = faixa_horaria_inicio
+                then lag(quilometragem) over (win) + quilometragem
                 else quilometragem
-            end as quilometragem,
-            row_number() over (
-                partition by servico, faixa_horaria_inicio order by data desc
-            ) as rn
+            end as quilometragem
         from os_faixa_horaria_dia
+        window win as (partition by servico order by data, faixa_horaria_inicio)
     ),
     os_filtrada as (
         select *
         from faixas_agregadas
-        where rn = 1 and data = extract(date from faixa_horaria_inicio)
+        where data = extract(date from faixa_horaria_inicio)
+        qualify
+            row_number() over (
+                partition by servico, faixa_horaria_inicio order by data desc
+            )
+            = 1
     ),
     os_por_sentido as (
         select
@@ -132,7 +107,7 @@ with
             sentido.codigo as sentido,
             sentido.extensao as extensao,
             sentido.partidas as partidas,
-            quilometragem,  -- TODO: alterar para sentido.extensao * sentido.partidas / 1000 quando subir novo modelo de OS
+            quilometragem,
             faixa_horaria_inicio,
             faixa_horaria_fim
         from
@@ -151,6 +126,69 @@ with
                     )
                 ]
             ) as sentido
+    ),
+    os_faixa_horaria_sentido as (
+        select
+            feed_version,
+            feed_start_date,
+            tipo_dia,
+            tipo_os,
+            servico,
+            consorcio,
+            left(sentido, 1) as sentido,
+            extensao,
+            partidas,
+            quilometragem,
+            split(faixa_horaria_inicio, ":") as faixa_horaria_inicio_parts,
+            split(faixa_horaria_fim, ":") as faixa_horaria_fim_parts
+        {# from `rj-smtr.planejamento.ordem_servico_faixa_horaria_sentido` #}
+        from {{ ref("ordem_servico_faixa_horaria_sentido") }}
+    ),
+    os_faixa_horaria_sentido_tratamento_horario as (
+        select
+            *,
+            make_interval(
+                hour => cast(faixa_horaria_inicio_parts[0] as integer),
+                minute => cast(faixa_horaria_inicio_parts[1] as integer),
+                second => cast(faixa_horaria_inicio_parts[2] as integer)
+            ) as faixa_horario_intervalo_inicio,
+            make_interval(
+                hour => cast(faixa_horaria_fim_parts[0] as integer),
+                minute => cast(faixa_horaria_fim_parts[1] as integer),
+                second => cast(faixa_horaria_fim_parts[2] as integer)
+            ) as faixa_horario_intervalo_fim
+        from os_faixa_horaria_sentido
+    ),
+    os_faixa_horaria_sentido_dia as (
+        select
+            c.data,
+            feed_version,
+            feed_start_date,
+            tipo_dia,
+            tipo_os,
+            servico,
+            consorcio,
+            sentido,
+            extensao,
+            partidas,
+            quilometragem,
+            c.data + o.faixa_horario_intervalo_inicio as faixa_horaria_inicio,
+            c.data + o.faixa_horario_intervalo_fim as faixa_horaria_fim,
+        from {{ calendario }} as c
+        join
+            os_faixa_horaria_sentido_tratamento_horario as o using (
+                feed_version, feed_start_date, tipo_dia, tipo_os
+            )
+        where {{ incremental_filter }}
+    ),
+    os_por_sentido_completa as (
+        select *
+        from os_por_sentido
+
+        union all
+
+        select *
+        from os_faixa_horaria_sentido_dia
     )
 select *
-from os_por_sentido
+from os_por_sentido_completa
