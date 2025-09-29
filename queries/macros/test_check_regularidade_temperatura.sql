1acdbc67367f27c90be45e1f309955c8710f5682
vtr363 <victor.m.rocha.vr@gmail.com>
2025-09-29 17:03:56 -0300

    corrige teste

--- queries/macros/test_check_regularidade_temperatura.sql
+++ queries/macros/test_check_regularidade_temperatura.sql
@@ -123,11 +123,10 @@
                         or data >= date("{{ var('DATA_SUBSIDIO_V19_INICIO') }}")
                     )
                     and (
-                        data < date("{{ var('DATA_SUBSIDIO_V20_INICIO') }}")
-                        or data >= date("{{ var('DATA_SUBSIDIO_V20_INICIO') }}")
-                        and coalesce(indicador_falha_recorrente, false)
-                    )
-                    and (
+                        (
+                            data >= date("{{ var('DATA_SUBSIDIO_V20_INICIO') }}")
+                            and coalesce(indicador_falha_recorrente, false)
+                        )
                         or indicador_temperatura_zero_viagem
                         or not indicador_temperatura_transmitida_viagem
                         or not indicador_temperatura_regular_viagem
