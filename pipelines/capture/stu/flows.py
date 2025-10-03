3b029cf7ac30ce385dc327bc5a0a729a3517c7fc
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-10-03 11:07:02 -0300

    teste aumento de limite

--- pipelines/capture/stu/flows.py
+++ pipelines/capture/stu/flows.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Flows de captura dos dados do STU"""
+from prefect.run_configs import KubernetesRun
+
 from pipelines.capture.stu.constants import constants
 from pipelines.capture.stu.tasks import create_stu_extractor
 from pipelines.capture.templates.flows import create_default_capture_flow
@@ -13,3 +15,12 @@ CAPTURA_STU = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
 )
 set_default_parameters(CAPTURA_STU, {"recapture": True})
+
+CAPTURA_STU.run_config = KubernetesRun(
+    image=smtr_constants.DOCKER_IMAGE.value,
+    labels=[smtr_constants.RJ_SMTR_AGENT_LABEL.value],
+    cpu_limit="1000m",
+    memory_limit="4600Mi",
+    cpu_request="500m",
+    memory_request="1000Mi",
+)
