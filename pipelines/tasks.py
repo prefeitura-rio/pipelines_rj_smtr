99d0d74b11340d5ac2b78b4f2e7cf0c1f090d443
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-10-07 09:20:33 -0300

    .

--- pipelines/tasks.py
+++ pipelines/tasks.py
@@ -13,7 +13,8 @@ from pytz import timezone
 
 from pipelines.constants import constants
 from pipelines.utils.discord import send_discord_message
-from pipelines.utils.prefect import FailedSubFlow, create_subflow_run, wait_subflow_run
+
+# from pipelines.utils.prefect import FailedSubFlow, create_subflow_run, wait_subflow_run
 from pipelines.utils.secret import get_secret
 from pipelines.utils.utils import convert_timezone
 
@@ -203,39 +204,40 @@ def run_subflow(
     if idempotency_key and map_index is not None:
         idempotency_key += f"-{map_index}"
 
-    flow_run_results = []
+    # flow_run_results = []
 
     log(parameters)
     for idx, param_list in enumerate(parameters):
         if not isinstance(param_list, list):
             param_list = [param_list]
+        log(param_list)
 
-        runs_ids = [
-            create_subflow_run(
-                flow_name=flow_name,
-                parameters=params,
-                idempotency_key=idempotency_key + f"-{idx}-{sub_idx}",
-                project_name=project_name,
-                labels=labels,
-            )
-            for sub_idx, params in enumerate(param_list)
-        ]
+        # runs_ids = [
+        #     create_subflow_run(
+        #         flow_name=flow_name,
+        #         parameters=params,
+        #         idempotency_key=idempotency_key + f"-{idx}-{sub_idx}",
+        #         project_name=project_name,
+        #         labels=labels,
+        #     )
+        #     for sub_idx, params in enumerate(param_list)
+        # ]
 
-        for run_id in runs_ids:
-            result = wait_subflow_run(flow_run_id=run_id)
-            flow_run_results.append(result)
+        # for run_id in runs_ids:
+        #     result = wait_subflow_run(flow_run_id=run_id)
+        #     flow_run_results.append(result)
 
-    failed_message = "The following runs failed:"
-    flag_failed_runs = False
-    for res in flow_run_results:
-        if res.state.is_failed():
-            flag_failed_runs = True
-            failed_message += "\n" + constants.FLOW_RUN_URL_PATTERN.value.format(
-                run_id=res.flow_run_id
-            )
+    # failed_message = "The following runs failed:"
+    # flag_failed_runs = False
+    # for res in flow_run_results:
+    #     if res.state.is_failed():
+    #         flag_failed_runs = True
+    #         failed_message += "\n" + constants.FLOW_RUN_URL_PATTERN.value.format(
+    #             run_id=res.flow_run_id
+    #         )
 
-    if flag_failed_runs:
-        raise FailedSubFlow(failed_message)
+    # if flag_failed_runs:
+    #     raise FailedSubFlow(failed_message)
 
 
 @task(trigger=all_finished)
