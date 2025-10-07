e090ed92049a5f1209bb3efdbe32a325b03096d6
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-10-07 07:41:11 -0300

    cria flow captura cct

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -2,6 +2,7 @@
 """
 Imports all flows for every project so we can register all of them.
 """
+from pipelines.capture.cct.flows import *  # noqa
 from pipelines.capture.cittati.flows import *  # noqa
 from pipelines.capture.conecta.flows import *  # noqa
 from pipelines.capture.inmet.flows import *  # noqa
