408d534a939ef71a20f6ce01530cca2329efc4c0
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2025-10-09 10:15:08 -0300

    pr 934

--- pipelines/capture/cct/flows.py
+++ pipelines/capture/cct/flows.py
@@ -7,11 +7,11 @@ from pipelines.capture.templates.flows import create_default_capture_flow
 from pipelines.constants import constants as smtr_constants
 from pipelines.utils.prefect import set_default_parameters
 
-CAPTURA_PAGAMENTO = create_default_capture_flow(
+CAPTURA_PAGAMENTO_CCT = create_default_capture_flow(
     flow_name="cct: pagamento - captura",
     source=constants.PAGAMENTO_SOURCES.value,
     create_extractor_task=create_cct_general_extractor,
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     get_raw_max_retries=0,
 )
-set_default_parameters(CAPTURA_PAGAMENTO, {"recapture": True})
+set_default_parameters(CAPTURA_PAGAMENTO_CCT, {"recapture": True})
