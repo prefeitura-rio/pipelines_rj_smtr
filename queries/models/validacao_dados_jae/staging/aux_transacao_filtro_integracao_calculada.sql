345a6b26b00c547d087c7ef62afc62c59242d38b
adriano <adrianoneto27@gmail.com>
2025-10-09 16:37:45 -0300

    Fix Alias

--- queries/models/validacao_dados_jae/staging/aux_transacao_filtro_integracao_calculada.sql
+++ queries/models/validacao_dados_jae/staging/aux_transacao_filtro_integracao_calculada.sql
@@ -63,7 +63,7 @@ left join
     {{ ref("aux_linha_tarifa") }} l
     on t.id_servico_jae = l.cd_linha
     and t.datetime_transacao >= l.dt_inicio_validade
-    and (l.data_fim_validade is null or t.datetime_transacao < lt.data_fim_validade)
+    and (l.data_fim_validade is null or t.datetime_transacao < l.data_fim_validade)
 where
     tipo_transacao != "Gratuidade" and tipo_transacao_jae != 'Botoeira'
     {% if not flags.FULL_REFRESH %}
