0afc1fed2860f0a705811e62af01b231b3761d37
Duartejanaina <janainacduarte@gmail.com>
2025-10-10 17:47:27 -0300

    comentar bibliotecas não utilizadas

--- pipelines/utils/extractors/gdrive.py
+++ pipelines/utils/extractors/gdrive.py
@@ -4,9 +4,9 @@ import os
 from typing import Optional
 import pandas as pd
 from google.oauth2 import service_account
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 from pipelines.utils.pretreatment import normalize_text
