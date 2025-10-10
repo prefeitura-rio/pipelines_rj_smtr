f336767e6ee64c662f136420b936f9a96ac1ca28
Duartejanaina <janainacduarte@gmail.com>
2025-10-10 17:36:07 -0300

    Alteração no gdrive para pipelines

--- pipelines/utils/extractors/gdrive.py
+++ pipelines/utils/extractors/gdrive.py
@@ -5,6 +5,9 @@ from typing import Optional
 
 import pandas as pd
 from google.oauth2 import service_account
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 from pipelines.utils.pretreatment import normalize_text
@@ -15,11 +18,28 @@ def get_google_sheet_xlsx(
     sheet_name: str,
     filter_expr: Optional[str] = None,
 ) -> pd.DataFrame:
-    credentials = service_account.Credentials.from_service_account_file(
-        filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
-        scopes=["https://www.googleapis.com/auth/drive.readonly"],
-    )
-    drive_service = build("sheets", "v4", credentials=credentials)
+    SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+    # credentials = service_account.Credentials.from_service_account_file(
+    #     filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
+    #     scopes=["https://www.googleapis.com/auth/drive.readonly"],
+    # )
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("cliente_oauth_gdrive.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    drive_service = build("sheets", "v4", credentials=creds)
 
     file = (
         drive_service.spreadsheets()
