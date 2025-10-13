a031a7ed4aaa9c98b4a25aa9684c5a993a40cc25
Duartejanaina <janainacduarte@gmail.com>
2025-10-13 09:41:24 -0300

    sqlfmt

--- queries/macros/test_consistencia_indicadores_temperatura.sql
+++ queries/macros/test_consistencia_indicadores_temperatura.sql
@@ -188,83 +188,91 @@
                                 (
                                     i.ano_fabricacao <= 2019
                                     or data
-                                    >= date('{{ var(' data_subsidio_v19_inicio ') }}') and
-                                    i.indicador_regularidade_ar_condicionado_viagem is null
+                                    >= date('{{ var(' data_subsidio_v19_inicio ') }}')
+                                    and i.indicador_regularidade_ar_condicionado_viagem
+                                    is null
                                 )
-                               
+
+                            )
+                            then
+                                'Veículo dentro do período de aplicação da Resolução (Art. 2º-E)'
+                                end as inconsistencia_condicao_veiculo,
+
+                                case
+                                    when
+                                        p.test_percentual_temperatura_nula_descartada
+                                        = 100
+                                        and i.indicador_regularidade_ar_condicionado_viagem
+                                        is not null
+                                    then
+                                        'Indicador_regularidade_ar_condicionado_viagem deveria ser nulo, 100% das temperaturas foram nulas'
+                                end as inconsistencia_temperatura_nula
+                            from indicadores i
+                            left join percentuais p using (data, id_viagem)
+                        ),
+
+                        falha_recorrente as (
+                            select
+                                v.data,
+                                v.id_veiculo,
+                                v.ano_fabricacao,
+                                v.quantidade_dia_falha_operacional,
+                                i.indicador_falha_recorrente,
+                                i.id_viagem
+                            from
+                                `rj-smtr.monitoramento.veiculo_regularidade_temperatura_dia` v
+                            left join indicadores i using (data, id_veiculo)
+                        ),
+
+                        analise_falha_recorrente as (
+                            select
+                                data,
+                                id_viagem,
+                                id_veiculo,
+                                ano_fabricacao,
+                                quantidade_dia_falha_operacional,
+                                indicador_falha_recorrente,
+                                case
+                                    when
+                                        quantidade_dia_falha_operacional < 6
+                                        and indicador_falha_recorrente = true
+                                    then 'Falha recorrente incorreta'
+                                end as falha_recorrente_inconsistencia
+                            from falha_recorrente
                         )
-                    then
-                         'Veículo dentro do período de aplicação da Resolução (Art. 2º-E)'
-                end as inconsistencia_condicao_veiculo,
 
-                case
-                    when
-                        p.test_percentual_temperatura_nula_descartada = 100
-                        and i.indicador_regularidade_ar_condicionado_viagem is not null
-                    then
-                        'Indicador_regularidade_ar_condicionado_viagem deveria ser nulo, 100% das temperaturas foram nulas'
-                end as inconsistencia_temperatura_nula
-            from indicadores i
-            left join percentuais p using (data, id_viagem)
-        ),
-
-        falha_recorrente as (
-            select
-                v.data,
-                v.id_veiculo,
-                v.ano_fabricacao,
-                v.quantidade_dia_falha_operacional,
-                i.indicador_falha_recorrente,
-                i.id_viagem
-            from `rj-smtr.monitoramento.veiculo_regularidade_temperatura_dia` v
-            left join indicadores i using (data, id_veiculo)
-        ),
-
-        analise_falha_recorrente as (
-            select
-                data,
-                id_viagem,
-                id_veiculo,
-                ano_fabricacao,
-                quantidade_dia_falha_operacional,
-                indicador_falha_recorrente,
-                case
-                    when
-                        quantidade_dia_falha_operacional < 6
-                        and indicador_falha_recorrente = true
-                    then 'Falha recorrente incorreta'
-                end as falha_recorrente_inconsistencia
-            from falha_recorrente
-        )
-
-    select
-        i.data,
-        i.id_viagem,
-        tt.temperatura_nao_transmitida,
-        tt.todas_temperaturas_transmitidas_iguais_zero,
-        vv.todas_temperaturas_zero,
-        dd.inconsistencia_descarte_pos,
-        iz.inconsistencia_temperatura_zero,
-        cv.inconsistencia_condicao_veiculo,
-        cv.inconsistencia_temperatura_nula,
-        fr.falha_recorrente_inconsistencia
-    from indicadores i
-    left join indicador_temperatura_transmitida_viagem tt using (data, id_viagem)
-    left join indicador_temperatura_variacao_viagem vv using (data, id_viagem)
-    left join
-        indicador_temperatura_pos_tratamento_descartada_viagem dd using (
-            data, id_viagem
-        )
-    left join indicador_temperatura_zero_descartada iz using (data, id_viagem)
-    left join condicao_veiculo cv using (data, id_viagem)
-    left join analise_falha_recorrente fr using (data, id_viagem)
-    where
-        tt.temperatura_nao_transmitida is not null
-        or tt.todas_temperaturas_transmitidas_iguais_zero is not null
-        or vv.todas_temperaturas_zero is not null
-        or dd.inconsistencia_descarte_pos is not null
-        or iz.inconsistencia_temperatura_zero is not null
-        or cv.inconsistencia_condicao_veiculo is not null
-        or cv.inconsistencia_temperatura_nula is not null
-        or fr.falha_recorrente_inconsistencia is not null
+                    select
+                        i.data,
+                        i.id_viagem,
+                        tt.temperatura_nao_transmitida,
+                        tt.todas_temperaturas_transmitidas_iguais_zero,
+                        vv.todas_temperaturas_zero,
+                        dd.inconsistencia_descarte_pos,
+                        iz.inconsistencia_temperatura_zero,
+                        cv.inconsistencia_condicao_veiculo,
+                        cv.inconsistencia_temperatura_nula,
+                        fr.falha_recorrente_inconsistencia
+                    from indicadores i
+                    left join
+                        indicador_temperatura_transmitida_viagem tt using (
+                            data, id_viagem
+                        )
+                    left join
+                        indicador_temperatura_variacao_viagem vv using (data, id_viagem)
+                    left join
+                        indicador_temperatura_pos_tratamento_descartada_viagem dd
+                        using (data, id_viagem)
+                    left join
+                        indicador_temperatura_zero_descartada iz using (data, id_viagem)
+                    left join condicao_veiculo cv using (data, id_viagem)
+                    left join analise_falha_recorrente fr using (data, id_viagem)
+                    where
+                        tt.temperatura_nao_transmitida is not null
+                        or tt.todas_temperaturas_transmitidas_iguais_zero is not null
+                        or vv.todas_temperaturas_zero is not null
+                        or dd.inconsistencia_descarte_pos is not null
+                        or iz.inconsistencia_temperatura_zero is not null
+                        or cv.inconsistencia_condicao_veiculo is not null
+                        or cv.inconsistencia_temperatura_nula is not null
+                        or fr.falha_recorrente_inconsistencia is not null
 {%- endtest %}
