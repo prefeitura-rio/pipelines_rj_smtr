4d67cf70f109ca1e9c14bf7bee1d9d4c39217cb7
Duartejanaina <janainacduarte@gmail.com>
2025-10-13 17:32:29 -0300

    Alterações no gdrive para rodar local

--- pipelines/utils/extractors/gdrive.py
+++ pipelines/utils/extractors/gdrive.py
@@ -2,16 +2,14 @@
 """Module to get data from Google Drive"""
 import os
 from typing import Optional
-
 import pandas as pd
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-
+from pipelines.utils.prefect import flow_is_running_local
 from pipelines.utils.pretreatment import normalize_text
-
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 
 def get_google_sheet_xlsx(
@@ -20,26 +18,25 @@ def get_google_sheet_xlsx(
     filter_expr: Optional[str] = None,
 ) -> pd.DataFrame:
     SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
-    creds = service_account.Credentials.from_service_account_file(
-        filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
-        scopes=SCOPES,
-    )
-    # creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    # if os.path.exists("token.json"):
-    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file("cliente_oauth_gdrive.json", SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #         # Save the credentials for the next run
-    #     with open("token.json", "w") as token:
-    #         token.write(creds.to_json())
+    
+    creds = None
+    if flow_is_running_local():
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file("cliente_oauth_gdrive.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+    else:      
+        creds = service_account.Credentials.from_service_account_file(
+            filename=os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
+            scopes=SCOPES)  
     drive_service = build("sheets", "v4", credentials=creds)
 
     file = (
