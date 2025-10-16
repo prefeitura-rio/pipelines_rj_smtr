32ea8bfd10a1939c001090bcbada7793a12914fd
vtr363 <victor.m.rocha.vr@gmail.com>
2025-10-16 10:27:45 -0300

    remove if source.exists

--- pipelines/capture/veiculo_fiscalizacao/tasks.py
+++ pipelines/capture/veiculo_fiscalizacao/tasks.py
@@ -15,16 +15,11 @@ from pipelines.utils.gcp.bigquery import SourceTable
     max_retries=smtr_constants.MAX_RETRIES.value,
     retry_delay=timedelta(seconds=smtr_constants.RETRY_DELAY.value),
 )
-def create_veiculo_fiscalizacao_lacre_extractor(
-    source: SourceTable,
-    timestamp: datetime,
-):
+def create_veiculo_fiscalizacao_lacre_extractor():
     """Cria a extração da planilha de controle de lacre dos veículos"""
 
-    if source.exists():
-
-        return partial(
-            get_google_sheet_xlsx,
-            spread_sheet_id=constants.VEICULO_LACRE_SHEET_ID.value,
-            sheet_name=constants.VEICULO_LACRE_SHEET_NAME.value,
-        )
+    return partial(
+        get_google_sheet_xlsx,
+        spread_sheet_id=constants.VEICULO_LACRE_SHEET_ID.value,
+        sheet_name=constants.VEICULO_LACRE_SHEET_NAME.value,
+    )
