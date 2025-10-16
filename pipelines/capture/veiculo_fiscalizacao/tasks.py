a5409516855b36a3233d4ccf1770af1d8c2f6213
vtr363 <victor.m.rocha.vr@gmail.com>
2025-10-16 10:44:53 -0300

    corrige create_veiculo_fiscalizacao_lacre_extractor

--- pipelines/capture/veiculo_fiscalizacao/tasks.py
+++ pipelines/capture/veiculo_fiscalizacao/tasks.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Tasks de captura dos dados de fiscalização de veiculos"""
-from datetime import timedelta
+from datetime import datetime, timedelta
 from functools import partial
 
 from prefect import task
@@ -8,13 +8,17 @@ from prefect import task
 from pipelines.capture.veiculo_fiscalizacao.constants import constants
 from pipelines.constants import constants as smtr_constants
 from pipelines.utils.extractors.gdrive import get_google_sheet_xlsx
+from pipelines.utils.gcp.bigquery import SourceTable
 
 
 @task(
     max_retries=smtr_constants.MAX_RETRIES.value,
     retry_delay=timedelta(seconds=smtr_constants.RETRY_DELAY.value),
 )
-def create_veiculo_fiscalizacao_lacre_extractor():
+def create_veiculo_fiscalizacao_lacre_extractor(
+    source: SourceTable,
+    timestamp: datetime,
+):
     """Cria a extração da planilha de controle de lacre dos veículos"""
 
     return partial(
