bcc244347fcccd368c6b2cf75f899b85a3a8b105
Guilherme Botelho <guilhermehcb@hotmail.com>
2025-10-24 11:00:06 -0300

    remove not_null__data_ordem__transacao

--- pipelines/migration/projeto_subsidio_sppo/flows.py
+++ pipelines/migration/projeto_subsidio_sppo/flows.py
@@ -316,7 +316,7 @@ with Flow(
             SUBSIDIO_SPPO_DATA_QUALITY_PRE = run_dbt(
                 resource="test",
                 dataset_id=constants.SUBSIDIO_SPPO_PRE_TEST.value,
-                exclude="dashboard_subsidio_sppo_v2 teto_viagens__viagens_remuneradas",
+                exclude="dashboard_subsidio_sppo_v2 teto_viagens__viagens_remuneradas not_null__data_ordem__transacao",  # noqa
                 _vars=dbt_vars,
                 upstream_tasks=[timestamps],
             )
@@ -630,7 +630,7 @@ with Flow(
         SUBSIDIO_SPPO_DATA_QUALITY_PRE = run_dbt(
             resource="test",
             dataset_id=constants.SUBSIDIO_SPPO_PRE_TEST.value,
-            exclude="dashboard_subsidio_sppo_v2 teto_viagens__viagens_remuneradas",
+            exclude="dashboard_subsidio_sppo_v2 teto_viagens__viagens_remuneradas not_null__data_ordem__transacao",  # noqa
             _vars=dbt_vars,
         ).set_upstream(task=send_discord_message)
 
