4e0b14e7fb2ac6402f03d4665dcd90703b42eebf
Duartejanaina <janainacduarte@gmail.com>
2025-10-30 16:05:03 -0300

    Cria o modelo area_planejamento

---
+++ queries/models/br_rj_riodejaneiro_geo/area_planejamento.sql
@@ -0,0 +1,11 @@
+{{
+    config(
+        materialized="view",
+    )
+}}
+
+select 
+  area_planejamento ,
+  ST_UNION_AGG(ST_GEOGFROMTEXT(geometry)) geometria
+from `rj-smtr.br_rj_riodejaneiro_geo.bairros`
+group by area_planejamento
