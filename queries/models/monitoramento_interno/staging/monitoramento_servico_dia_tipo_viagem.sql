6f165f042ea9ab09b390e735d77aa25eff41a06d
Duartejanaina <janainacduarte@gmail.com>
2025-11-03 15:59:28 -0300

    Move modelos de monitoramento para monitoramento interno

---
+++ queries/models/monitoramento_interno/staging/monitoramento_servico_dia_tipo_viagem.sql
@@ -0,0 +1,22 @@
+{% if var("start_date") >= var("DATA_SUBSIDIO_V9_INICIO") %} {{ config(enabled=false) }}
+{% else %} {{ config(materialized="ephemeral") }}
+{% endif %}
+
+select
+    data,
+    tipo_dia,
+    consorcio,
+    servico,
+    tipo_viagem,
+    indicador_ar_condicionado,
+    viagens,
+    km_apurada
+from {{ ref("sumario_servico_tipo_viagem_dia") }}
+-- `rj-smtr.dashboard_subsidio_sppo.sumario_servico_tipo_viagem_dia`
+where
+    data < date("{{ var(" data_subsidio_v9_inicio ") }}")
+    {% if is_incremental() %}
+        and data between date("{{ var(" start_date ") }}") and date_add(
+            date("{{ var(" end_date ") }}"), interval 1 day
+        )
+    {% endif %}
