449cbbd46fc0aa433aedf82469cea6fbe71a577e
Duartejanaina <janainacduarte@gmail.com>
2025-11-03 17:01:17 -0300

    Alterações nas datas

--- queries/models/monitoramento_interno/monitoramento_sumario_servico_dia_tipo_viagem_historico.sql
+++ queries/models/monitoramento_interno/monitoramento_sumario_servico_dia_tipo_viagem_historico.sql
@@ -7,14 +7,14 @@
         labels={"dashboard": "yes"},
     )
 }}
-{% if var("start_date") < var("DATA_SUBSIDIO_V9_INICIO") %}
+{% if var("start_date") < var('DATA_SUBSIDIO_V9_INICIO') %}
     select *
     from {{ ref("monitoramento_servico_dia_tipo_viagem") }}
-    where data < date("{{ var(" data_subsidio_v9_inicio ") }}")
+    where data < date("{{ var('DATA_SUBSIDIO_V9_INICIO')) }}")
     union all
 {% endif %}
 select *
 from {{ ref("monitoramento_servico_dia_tipo_viagem_v2") }}
 where
-    data >= date("{{ var(" data_subsidio_v9_inicio ") }}")
+    data >= date("{{ var('DATA_SUBSIDIO_V9_INICIO') }}")
     and tipo_viagem != "Sem viagem apurada"
