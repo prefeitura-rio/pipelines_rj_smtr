5ea7fafa0ea571975ad6071fe93cb198cad26dab
Duartejanaina <janainacduarte@gmail.com>
2025-11-03 15:07:25 -0300

    Revert "sqlfmt"
    This reverts commit 2d1ec18c60fd239a293cf5cd303f6c7fe3de9761.

--- queries/models/monitoramento_interno/staging/monitoramento_servico_dia_tipo_viagem.sql
+++ queries/models/monitoramento_interno/staging/monitoramento_servico_dia_tipo_viagem.sql
@@ -1,5 +1,7 @@
-{% if var("start_date") >= var("DATA_SUBSIDIO_V9_INICIO") %} {{ config(enabled=false) }}
-{% else %} {{ config(materialized="ephemeral") }}
+{% if var("start_date") >= var("DATA_SUBSIDIO_V9_INICIO") %}
+{{ config(enabled=false) }}
+{% else %}
+{{ config(materialized="ephemeral") }}
 {% endif %}
 
 select
@@ -14,9 +16,9 @@ select
 from {{ ref("sumario_servico_tipo_viagem_dia") }}
 -- `rj-smtr.dashboard_subsidio_sppo.sumario_servico_tipo_viagem_dia`
 where
-    data < date("{{ var(" data_subsidio_v9_inicio ") }}")
+    data < date("{{ var("DATA_SUBSIDIO_V9_INICIO") }}")
     {% if is_incremental() %}
-        and data between date("{{ var(" start_date ") }}") and date_add(
-            date("{{ var(" end_date ") }}"), interval 1 day
+        and data between date("{{ var("start_date") }}") and date_add(
+            date("{{ var("end_date") }}"), interval 1 day
         )
     {% endif %}
