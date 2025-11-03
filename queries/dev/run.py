38c77c0f53a1725cd0fba82103503d452192fe14
Duartejanaina <janainacduarte@gmail.com>
2025-11-03 14:54:48 -0300

    acrescenta pr ao CHANGELOG

--- queries/dev/run.py
+++ queries/dev/run.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 # import os
 
-from queries.dev.utils import run_dbt_model
+from queries.dev.utils import run_dbt_model, run_dbt_tests, run_dbt_selector
 
 # Veja os parâmetros disponíveis da função run_dbt_model em util.py
 
-run_dbt_model(
-    dataset_id="example",
-    table_id="my_first_dbt_model",
-)
+inicio = "2025-10-21"
+fim = "2025-10-21"
+
+# run_dbt_selector(
+#     selector_name="monitoramento_temperatura", _vars={"start_date": inicio, "end_date": fim},
+#     flags = "--target dev --defer --state target-base --favor-state")
+
+run_dbt_tests(
+    dataset_id="temperatura",
+      _vars={"date_range_start": inicio, "date_range_end": fim}, 
+    flags = "--target dev --defer --state target-base ")
