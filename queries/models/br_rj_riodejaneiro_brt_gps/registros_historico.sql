1e9b367e5a19e8fc4c2c9739380e96c3b925fe46
Duartejanaina <janainacduarte@gmail.com>
2025-11-03 13:29:09 -0300

    sqlfmt

--- queries/models/br_rj_riodejaneiro_brt_gps/registros_historico.sql
+++ queries/models/br_rj_riodejaneiro_brt_gps/registros_historico.sql
@@ -4,18 +4,22 @@
     )
 }}
 
-SELECT 
-SAFE_CAST(codigo AS STRING) codigo,
-SAFE_CAST(placa AS STRING) placa,
-SAFE_CAST(linha AS STRING) linha,
-SAFE_CAST(latitude AS FLOAT64) latitude,
-SAFE_CAST(longitude AS FLOAT64) longitude,
-SAFE_CAST(DATETIME(TIMESTAMP(timestamp_gps), "America/Sao_Paulo") AS DATETIME) timestamp_gps,
-SAFE_CAST(velocidade AS INT64) velocidade,
-SAFE_CAST(id_migracao_trajeto AS STRING) id_migracao_trajeto,
-SAFE_CAST(sentido AS STRING) sentido,
-SAFE_CAST(trajeto AS STRING) trajeto,
-SAFE_CAST(DATETIME(TIMESTAMP(timestamp_captura), "America/Sao_Paulo") AS DATETIME) timestamp_captura,
-SAFE_CAST(data AS DATE) data,
-SAFE_CAST(hora AS INT64) hora
+select
+    safe_cast(codigo as string) codigo,
+    safe_cast(placa as string) placa,
+    safe_cast(linha as string) linha,
+    safe_cast(latitude as float64) latitude,
+    safe_cast(longitude as float64) longitude,
+    safe_cast(
+        datetime(timestamp(timestamp_gps), "America/Sao_Paulo") as datetime
+    ) timestamp_gps,
+    safe_cast(velocidade as int64) velocidade,
+    safe_cast(id_migracao_trajeto as string) id_migracao_trajeto,
+    safe_cast(sentido as string) sentido,
+    safe_cast(trajeto as string) trajeto,
+    safe_cast(
+        datetime(timestamp(timestamp_captura), "America/Sao_Paulo") as datetime
+    ) timestamp_captura,
+    safe_cast(data as date) data,
+    safe_cast(hora as int64) hora
 from {{ source("br_rj_riodejaneiro_brt_gps_staging", "registros_historico") }} as t
