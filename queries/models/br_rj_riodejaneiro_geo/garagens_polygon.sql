e49e0cab5c752bfd342d109fa64b6b8f98a10608
Duartejanaina <janainacduarte@gmail.com>
2025-11-03 19:50:54 -0300

    sqlfmt

--- queries/models/br_rj_riodejaneiro_geo/garagens_polygon.sql
+++ queries/models/br_rj_riodejaneiro_geo/garagens_polygon.sql
@@ -1,9 +1,4 @@
-{{
-    config(
-        materialized="view",
-        alias = "garagem_polygon"
-    )
-}}
+{{ config(materialized="view", alias="garagem_polygon") }}
 
-select WKT, NM_MUNICIP, CD_GEOCMU
+select wkt, nm_municip, cd_geocmu
 from {{ source("br_rj_riodejaneiro_geo", "garagens_polygon") }}
