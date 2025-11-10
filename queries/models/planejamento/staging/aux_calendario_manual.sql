924da8766ccf869be285d4375b1d888351145c8b
Duartejanaina <janainacduarte@gmail.com>
2025-11-10 19:26:15 -0300

    Altera changelog e sqlfmt

--- queries/models/planejamento/staging/aux_calendario_manual.sql
+++ queries/models/planejamento/staging/aux_calendario_manual.sql
@@ -40,7 +40,7 @@ with
                 then "Ponto Facultativo"  -- LEI Nº 8.881, DE 14 DE ABRIL DE 2025 / MTR-PRO-2025/16278
                 when data = date(2025, 07, 07)
                 then "Ponto Facultativo"  -- LEI Nº 8.881, DE 14 DE ABRIL DE 2025 / MTR-PRO-2025/16278
-                when data = date(2025,10,20)
+                when data = date(2025, 10, 20)
                 then "Ponto Facultativo"  -- MTR-MEM-2025/02734 - Indicação de tipo dia "Ponto Facultativo" para o feriado do dia do comerciário
             end as tipo_dia,
             case
