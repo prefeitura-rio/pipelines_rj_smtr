d2bbe789bb7e395fb8c2f560515ab1b0db0ccb32
adriano <adrianoneto27@gmail.com>
2025-11-10 16:29:03 -0300

    Adiciona nova source

--- queries/models/br_rj_riodejaneiro_transporte_staging/estacoes_e_terminais_brt.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        materialized="table",
-    )
-}}
-
-select *
-from `rj-smtr-staging.br_rj_riodejaneiro_transporte_staging.estacoes_e_terminais_brt`
--- queries/models/br_rj_riodejaneiro_transporte_staging/terminais_onibus_coordenadas.sql
+++
@@ -1,8 +0,0 @@
-{{
-    config(
-        materialized="table",
-    )
-}}
-
-select *
-from `rj-smtr-staging.br_rj_riodejaneiro_transporte_staging.terminais_onibus_coordenadas`
