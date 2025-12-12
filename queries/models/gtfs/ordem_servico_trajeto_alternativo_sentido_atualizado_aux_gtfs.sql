13f44d79394dea067872215e243e998227f3675c
vtr363 <victor.m.rocha.vr@gmail.com>
2025-12-12 14:54:57 -0300

    corrige variavel

--- queries/models/gtfs/ordem_servico_trajeto_alternativo_sentido_atualizado_aux_gtfs.sql
+++ queries/models/gtfs/ordem_servico_trajeto_alternativo_sentido_atualizado_aux_gtfs.sql
@@ -14,7 +14,7 @@ select *
 from {{ ref("ordem_servico_trajeto_alternativo_sentido_atualizado_aux_gtfs_v2") }}
 where
     feed_start_date between date("{{ var('DATA_GTFS_V4_INICIO') }}") and date(
-        "{{ var('DATA_GTFS_v5_INICIO') }}"
+        "{{ var('DATA_GTFS_V5_INICIO') }}"
     )
 --fmt:off
 full outer union all by name
