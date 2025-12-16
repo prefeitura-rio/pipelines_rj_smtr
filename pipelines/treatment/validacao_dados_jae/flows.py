06e327bdeefcbf67aad3f021463e36f4729ec105
Rafael Carvalho Pinheiro <74972217+pixuimpou@users.noreply.github.com>
2025-12-16 15:39:50 -0300

    Bilhetagem - Ajusta incremental e adiciona coluna `classificacao_integracao_nao_realizada` na tabela `integracao_nao_realizada` (#1110)
    * duplicacao integracao calculada / classificacao integracao_nao_realizada
    
    * pr 1110
    
    * fix materialized
    
    * fix
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pipelines/treatment/validacao_dados_jae/flows.py
+++ pipelines/treatment/validacao_dados_jae/flows.py
@@ -15,6 +15,7 @@ VALIDACAO_DADOS_JAE_MATERIALIZACAO = create_default_materialization_flow(
     flow_name="validacao_dados_jae - materializacao",
     selector=constants.VALIDACAO_DADOS_JAE_SELECTOR.value,
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
+    post_tests=constants.VALIDACAO_DADOS_JAE_DAILY_TEST.value,
     wait=[
         bilhetagem_constants.TRANSACAO_SELECTOR.value,
         bilhetagem_constants.INTEGRACAO_SELECTOR.value,
