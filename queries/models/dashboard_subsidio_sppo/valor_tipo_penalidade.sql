98f947cc2fc7c35e0aceb1448370d41d84227043
Duartejanaina <janainacduarte@gmail.com>
2026-01-14 15:38:54 -0300

    sqlfmt

--- queries/models/dashboard_subsidio_sppo/valor_tipo_penalidade.sql
+++ queries/models/dashboard_subsidio_sppo/valor_tipo_penalidade.sql
@@ -4,14 +4,14 @@
     )
 }}
 
-select 
-data_inicio,
-data_fim,
-perc_km_inferior,
-perc_km_superior,
-tipo_penalidade, 
-valor,
-datetime_ultima_atualizacao,
-versao,
-id_execucao_dbt
+select
+    data_inicio,
+    data_fim,
+    perc_km_inferior,
+    perc_km_superior,
+    tipo_penalidade,
+    valor,
+    datetime_ultima_atualizacao,
+    versao,
+    id_execucao_dbt
 from {{ ref("staging_valor_tipo_penalidade") }}
