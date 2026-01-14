fc51e16820190a73d25ce4d7ff8d47611388177c
Duartejanaina <janainacduarte@gmail.com>
2026-01-14 15:27:27 -0300

    Adiciona colunas de controle e ajusta conforme ontologia `valor_tipo_penalidade` e `staging_valor_tipo_penalidade`

--- queries/models/dashboard_subsidio_sppo/valor_tipo_penalidade.sql
+++ queries/models/dashboard_subsidio_sppo/valor_tipo_penalidade.sql
@@ -4,5 +4,14 @@
     )
 }}
 
-select perc_km_inferior, perc_km_superior, tipo_penalidade, valor, data_inicio, data_fim
+select 
+data_inicio,
+data_fim,
+perc_km_inferior,
+perc_km_superior,
+tipo_penalidade, 
+valor,
+datetime_ultima_atualizacao,
+versao,
+id_execucao_dbt
 from {{ ref("staging_valor_tipo_penalidade") }}
--- queries/models/subsidio/staging/staging_valor_tipo_penalidade.sql
+++ queries/models/subsidio/staging/staging_valor_tipo_penalidade.sql
@@ -6,10 +6,13 @@
 }}
 
 select
+    safe_cast(data_inicio as date) as data_inicio,
+    safe_cast(data_fim as date) as data_fim,
     safe_cast(perc_km_inferior as integer) as perc_km_inferior,
     safe_cast(perc_km_superior as integer) as perc_km_superior,
     nullif(safe_cast(tipo_penalidade as string), '') as tipo_penalidade,
     safe_cast(nullif(trim(valor), '') as numeric) as valor,
-    safe_cast(data_inicio as date) as data_inicio,
-    safe_cast(data_fim as date) as data_fim
+    current_datetime("America/Sao_Paulo") as datetime_ultima_atualizacao,
+    "{{ var('version') }}" as versao,
+    '{{ invocation_id }}' as id_execucao_dbt
 from {{ source("subsidio_staging", "valor_tipo_penalidade") }}
