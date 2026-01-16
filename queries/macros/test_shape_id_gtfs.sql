62d181cf182b24158e89424bec4b89d034c45227
Duartejanaina <janainacduarte@gmail.com>
2026-01-16 12:14:46 -0300

    sqlfmt

--- queries/macros/test_shape_id_gtfs.sql
+++ queries/macros/test_shape_id_gtfs.sql
@@ -1,12 +1,8 @@
 {% test test_shape_id_gtfs(model, column_name) %}
 
-select distinct 
-    t.feed_start_date,
-    t.shape_id
-from {{ ref("trips_gtfs") }} t
-left join {{ ref("shapes_gtfs") }} s
-  using (feed_start_date, shape_id)
-where s.shape_id is null
-  and t.feed_start_date = '{{ var("data_versao_gtfs") }}'
+    select distinct t.feed_start_date, t.shape_id
+    from {{ ref("trips_gtfs") }} t
+    left join {{ ref("shapes_gtfs") }} s using (feed_start_date, shape_id)
+    where s.shape_id is null and t.feed_start_date = '{{ var("data_versao_gtfs") }}'
 
 {% endtest %}
