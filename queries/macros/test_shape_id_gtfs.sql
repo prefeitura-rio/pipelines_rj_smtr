103d1be32c9d424facb12844b61adabd30e67120
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-01-19 15:29:51 -0300

    refactor: update test_shape_id_gtfs to select trip_short_name

--- queries/macros/test_shape_id_gtfs.sql
+++ queries/macros/test_shape_id_gtfs.sql
@@ -1,9 +1,6 @@
-{% test test_shape_id_gtfs(model, column_name) %}--fmt:off
-    select distinct--fmt:on
-        t.feed_start_date, t.shape_id
-    from {{ ref("trips_gtfs") }} t
-    left join {{ ref("shapes_gtfs") }} s using (feed_start_date, shape_id)
-    where
-        s.shape_id is null
-        and t.feed_start_date = '{{ var("data_versao_gtfs") }}'--fmt:off
+{% test test_shape_id_gtfs(model, column_name) %}
+    select distinct t.trip_short_name
+    from {{ model }} t
+    left join {{ ref("shapes_gtfs") }} s using (feed_start_date, {{ column_name }})
+    where s.shape_id is null and t.feed_start_date = '{{ var("data_versao_gtfs") }}'
 {% endtest %}
