00e3450007711f488518fff3f8e0aeaf978a276b
adriano <adrianoneto27@gmail.com>
2026-01-23 16:18:37 -0300

    Adiciona filtro de partição em tabelas grandes

--- queries/models/bilhetagem/transacao.sql
+++ queries/models/bilhetagem/transacao.sql
@@ -3,6 +3,7 @@
         materialized="incremental",
         partition_by={"field": "data", "data_type": "date", "granularity": "day"},
         incremental_strategy="insert_overwrite",
+        require_partition_filter=true,
     )
 }}
 
