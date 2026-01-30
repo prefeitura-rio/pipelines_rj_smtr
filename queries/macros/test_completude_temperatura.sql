46b6e0497d35cadf6a4b592676b112183d131169
Duartejanaina <janainacduarte@gmail.com>
2026-01-30 17:48:33 -0300

    sqlfmt

--- queries/macros/test_completude_temperatura.sql
+++ queries/macros/test_completude_temperatura.sql
@@ -8,17 +8,16 @@
                         date('{{ var("date_range_end") }}'),
                         date('{{ var("date_range_start") }}'),
                         day
-                    ) = 1,
+                    )
+                    = 1,
                     date_sub(date('{{ var("date_range_end") }}'), interval 1 day),
                     date('{{ var("date_range_end") }}')
                 ) as data_fim
         ),
         datas_esperadas as (
             select data
-            from limite_datas,
-            unnest(
-                generate_date_array(data_inicio, data_fim)
-            ) as data
+            from
+                limite_datas, unnest(generate_date_array(data_inicio, data_fim)) as data
         ),
         validation as (
             select data, count(distinct hora) as qtd
