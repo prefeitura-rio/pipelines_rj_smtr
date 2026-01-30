895bf779d6afe518d1ef17d39c1687e0f27e74df
Duartejanaina <janainacduarte@gmail.com>
2026-01-30 19:04:59 -0300

    Atualiza lógica do teste

--- queries/macros/test_completude_temperatura.sql
+++ queries/macros/test_completude_temperatura.sql
@@ -1,28 +1,34 @@
 {% test test_completude_temperatura(model) %}
+    {% set data_fim_ajustada %}
+        if(
+            date_diff(
+                date('{{ var("date_range_end") }}'),
+                date('{{ var("date_range_start") }}'),
+                day
+            ) = 1,
+            date_sub(date('{{ var("date_range_end") }}'), interval 1 day),
+            date('{{ var("date_range_end") }}')
+        )
+    {% endset %}
+
     with
-        limite_datas as (
-            select
-                date('{{ var("date_range_start") }}') as data_inicio,
-                if(
-                    date_diff(
-                        date('{{ var("date_range_end") }}'),
-                        date('{{ var("date_range_start") }}'),
-                        day
-                    )
-                    = 1,
-                    date_sub(date('{{ var("date_range_end") }}'), interval 1 day),
-                    date('{{ var("date_range_end") }}')
-                ) as data_fim
-        ),
         datas_esperadas as (
             select data
             from
-                limite_datas, unnest(generate_date_array(data_inicio, data_fim)) as data
+                unnest(
+                    generate_date_array(
+                        date('{{ var("date_range_start") }}'), {{ data_fim_ajustada }}
+                    )
+                ) as data
         ),
         validation as (
             select data, count(distinct hora) as qtd
             from {{ model }}
-            where temperatura is not null
+            where
+                temperatura is not null
+                and data between date(
+                    '{{ var("date_range_start") }}'
+                ) and {{ data_fim_ajustada }}
             group by data
         )
     select e.data, v.qtd
