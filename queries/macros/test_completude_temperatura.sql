f07272ae8dfa696ceecf40b1b918759fa2aaf271
Duartejanaina <janainacduarte@gmail.com>
2026-01-30 17:37:26 -0300

    Ajusta lógica de data no teste de completude de temperatura para considerar corretamente intervalos de um dia

--- queries/macros/test_completude_temperatura.sql
+++ queries/macros/test_completude_temperatura.sql
@@ -1,14 +1,24 @@
 {% test test_completude_temperatura(model) %}
     with
+        limite_datas as (
+            select
+                date('{{ var("date_range_start") }}') as data_inicio,
+                if(
+                    date_diff(
+                        date('{{ var("date_range_end") }}'),
+                        date('{{ var("date_range_start") }}'),
+                        day
+                    ) = 1,
+                    date_sub(date('{{ var("date_range_end") }}'), interval 1 day),
+                    date('{{ var("date_range_end") }}')
+                ) as data_fim
+        ),
         datas_esperadas as (
             select data
-            from
-                unnest(
-                    generate_date_array(
-                        date('{{ var("date_range_start") }}'),
-                        date('{{ var("date_range_end") }}')
-                    )
-                ) as data
+            from limite_datas,
+            unnest(
+                generate_date_array(data_inicio, data_fim)
+            ) as data
         ),
         validation as (
             select data, count(distinct hora) as qtd
