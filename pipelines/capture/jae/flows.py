6b7baaaa48d5e9285b11e88e1adb437945103ded
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2026-03-02 10:34:46 -0300

    Remove schedules do flow `CAPTURA_TRANSACAO`

--- pipelines/capture/jae/flows.py
+++ pipelines/capture/jae/flows.py
@@ -51,6 +51,7 @@ CAPTURA_TRANSACAO = create_default_capture_flow(
     agent_label=smtr_constants.RJ_SMTR_AGENT_LABEL.value,
     recapture_schedule_cron=create_hourly_cron(),
     get_raw_max_retries=0,
+    generate_schedule=False,
 )
 
 CAPTURA_TRANSACAO_RIOCARD = create_default_capture_flow(
