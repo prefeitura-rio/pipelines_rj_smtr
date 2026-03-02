f7e0d36c9c90b62d3978e369fb2ea4eaa59dc11e
vtr363 <victor.m.rocha.vr@gmail.com>
2026-03-02 17:59:47 -0300

    merge

--- queries/models/veiculo/staging/sppo_registro_agente_verao_staging.sql
+++ queries/models/veiculo/staging/sppo_registro_agente_verao_staging.sql
@@ -1,24 +1,27 @@
 {{ config(materialized="view", alias="sppo_registro_agente_verao") }}
 
-select
-    safe_cast(parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) as date) as data,
-    safe_cast(
-        parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) as datetime
-    ) as datetime_registro,
-    -- fmt: off
-    sha256(
-        parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) || "_" || safe_cast(email as string)
-    ) as id_registro,
-    -- fmt: on
-    safe_cast(json_value(content, '$.id_veiculo') as string) as id_veiculo,
-    safe_cast(json_value(content, '$.servico') as string) as servico,
-    safe_cast(json_value(content, '$.link_foto') as string) as link_foto,
-    safe_cast(json_value(content, '$.validacao') as bool) as validacao,
-    date(data) as data_captura,
-    safe_cast(
-        datetime(
-            timestamp_trunc(timestamp(timestamp_captura), second), "America/Sao_Paulo"
-        ) as datetime
-    ) as datetime_captura,
-    "{{ var('version') }}" as versao
-from {{ source("veiculo_staging", "sppo_registro_agente_verao") }}
+SELECT
+  safe_cast(parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) AS date)
+    AS data,
+  safe_cast(
+    parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro) AS datetime
+  ) AS datetime_registro,
+  -- fmt: off
+  sha256(
+    parse_datetime("%d/%m/%Y %H:%M:%S", datetime_registro)
+    || "_"
+    || safe_cast(email AS string)
+  ) AS id_registro,
+  -- fmt: on
+  safe_cast(json_value(content, '$.id_veiculo') AS string) AS id_veiculo,
+  safe_cast(json_value(content, '$.servico') AS string) AS servico,
+  safe_cast(json_value(content, '$.link_foto') AS string) AS link_foto,
+  safe_cast(json_value(content, '$.validacao') AS bool) AS validacao,
+  date(data) AS data_captura,
+  safe_cast(
+    datetime(
+      timestamp_trunc(timestamp(timestamp_captura), second), "America/Sao_Paulo"
+    ) AS datetime
+  ) AS datetime_captura,
+  "{{ var('version') }}" AS versao
+FROM {{ source("veiculo_staging", "sppo_registro_agente_verao") }}
