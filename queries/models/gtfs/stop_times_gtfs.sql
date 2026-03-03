981112edadc0bb534e08f253cc67a623b3c95d10
Guilherme Botelho <guilhermehcb@hotmail.com>
2026-03-03 18:14:42 -0300

    fix: cast arrival_time as interval to handle hours > 24

--- queries/models/gtfs/stop_times_gtfs.sql
+++ queries/models/gtfs/stop_times_gtfs.sql
@@ -1,43 +1,62 @@
-{{config(
-    partition_by = { 'field' :'feed_start_date',
-    'data_type' :'date',
-    'granularity': 'day' },
-    unique_key = ['trip_id', 'feed_start_date'],
-    alias = 'stop_times'
-)}}
+{{
+    config(
+        partition_by={
+            "field": "feed_start_date",
+            "data_type": "date",
+            "granularity": "day",
+        },
+        unique_key=["trip_id", "feed_start_date"],
+        alias="stop_times",
+    )
+}}
 
 {% if execute and is_incremental() %}
-  {% set last_feed_version = get_last_feed_start_date(var("data_versao_gtfs")) %}
+    {% set last_feed_version = get_last_feed_start_date(var("data_versao_gtfs")) %}
 {% endif %}
 
-SELECT
-    fi.feed_version,
-    SAFE_CAST(st.data_versao AS DATE) as feed_start_date,
-    fi.feed_end_date,
-    SAFE_CAST(st.trip_id AS STRING) trip_id,
-    SAFE_CAST(JSON_VALUE(st.content, '$.arrival_time') AS STRING) arrival_time,
-    SAFE_CAST(JSON_VALUE(st.content, '$.departure_time') AS DATETIME) departure_time,
-    SAFE_CAST(JSON_VALUE(st.content, '$.stop_id') AS STRING) stop_id,
-    SAFE_CAST(st.stop_sequence AS INT64) stop_sequence,
-    SAFE_CAST(JSON_VALUE(st.content, '$.stop_headsign') AS STRING) stop_headsign,
-    SAFE_CAST(JSON_VALUE(st.content, '$.pickup_type') AS STRING) pickup_type,
-    SAFE_CAST(JSON_VALUE(st.content, '$.drop_off_type') AS STRING) drop_off_type,
-    SAFE_CAST(JSON_VALUE(st.content, '$.continuous_pickup') AS STRING) continuous_pickup,
-    SAFE_CAST(JSON_VALUE(st.content, '$.continuous_drop_off') AS STRING) continuous_drop_off,
-    SAFE_CAST(JSON_VALUE(st.content, '$.shape_dist_traveled') AS FLOAT64) shape_dist_traveled,
-    SAFE_CAST(JSON_VALUE(st.content, '$.timepoint') AS STRING) timepoint,
-    '{{ var("version") }}' AS versao_modelo
-FROM
-    {{ source(
-        'br_rj_riodejaneiro_gtfs_staging',
-        'stop_times'
-    ) }} st
-JOIN
-    {{ ref('feed_info_gtfs') }} fi
-ON
-    st.data_versao = CAST(fi.feed_start_date AS STRING)
-{% if is_incremental() -%}
-    WHERE
-        st.data_versao IN ('{{ last_feed_version }}', '{{ var("data_versao_gtfs") }}')
-        AND fi.feed_start_date IN ('{{ last_feed_version }}', '{{ var("data_versao_gtfs") }}')
-{%- endif %}
+with
+    stop_times_raw as (
+        select
+            st.*,
+            fi.feed_version,
+            fi.feed_end_date,
+            split(json_value(st.content, '$.arrival_time'), ":") as arrival_time_parts,
+        from {{ source("br_rj_riodejaneiro_gtfs_staging", "stop_times") }} st
+        join
+            {{ ref("feed_info_gtfs") }} fi
+            on st.data_versao = cast(fi.feed_start_date as string)
+        {% if is_incremental() -%}
+            where
+                st.data_versao
+                in ('{{ last_feed_version }}', '{{ var("data_versao_gtfs") }}')
+                and fi.feed_start_date
+                in ('{{ last_feed_version }}', '{{ var("data_versao_gtfs") }}')
+        {%- endif %}
+    )
+
+select
+    feed_version,
+    safe_cast(data_versao as date) as feed_start_date,
+    feed_end_date,
+    safe_cast(trip_id as string) trip_id,
+    make_interval(
+        hour => cast(arrival_time_parts[0] as integer),
+        minute => cast(arrival_time_parts[1] as integer),
+        second => cast(arrival_time_parts[2] as integer)
+    ) as arrival_time,
+    safe_cast(json_value(content, '$.departure_time') as datetime) departure_time,
+    safe_cast(json_value(content, '$.stop_id') as string) stop_id,
+    safe_cast(stop_sequence as int64) stop_sequence,
+    safe_cast(json_value(content, '$.stop_headsign') as string) stop_headsign,
+    safe_cast(json_value(content, '$.pickup_type') as string) pickup_type,
+    safe_cast(json_value(content, '$.drop_off_type') as string) drop_off_type,
+    safe_cast(json_value(content, '$.continuous_pickup') as string) continuous_pickup,
+    safe_cast(
+        json_value(content, '$.continuous_drop_off') as string
+    ) continuous_drop_off,
+    safe_cast(
+        json_value(content, '$.shape_dist_traveled') as float64
+    ) shape_dist_traveled,
+    safe_cast(json_value(content, '$.timepoint') as string) timepoint,
+    '{{ var("version") }}' as versao_modelo
+from stop_times_raw
