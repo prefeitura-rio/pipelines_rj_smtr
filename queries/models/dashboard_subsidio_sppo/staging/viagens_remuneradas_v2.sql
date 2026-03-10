a3b7f615ee4b117b6d90f79f51d02f7ea820b019
Duartejanaina <janainacduarte@gmail.com>
2026-03-10 18:17:01 -0300

    Atualiza modelo `viagens_remuneradas` devido a excepcionalidade acima do limite em fev q2

--- queries/models/dashboard_subsidio_sppo/staging/viagens_remuneradas_v2.sql
+++ queries/models/dashboard_subsidio_sppo/staging/viagens_remuneradas_v2.sql
@@ -174,6 +174,41 @@ select
     ),
     case
         when
+            (v.data between date('2026-02-16') and date('2026-02-28'))
+            and v.servico in (
+                "104",
+                "107",
+                "161",
+                "167",
+                "169",
+                "232",
+                "361",
+                "409",
+                "410",
+                "416",
+                "435",
+                "461",
+                "473",
+                "552",
+                "583",
+                "584",
+                "805",
+                "SP805",
+                "LECD127",
+                "LECD128",
+                "LECD129",
+                "LECD130",
+                "LECD131",
+                "LECD132",
+                "LECD133",
+                "LECD134",
+                "LECD135",
+                "LECD136",
+                "LECD137",
+                "LECD138"
+            )  -- Processo n° 000300.005989/2026-19
+        then true
+        when
             (v.data between date('2026-01-16') and date('2026-02-15'))
             and v.servico in (
                 "104",
