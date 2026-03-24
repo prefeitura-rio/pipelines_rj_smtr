a2b858bbba3c60293e1631a26857c4aab1230ede
Duartejanaina <janainacduarte@gmail.com>
2026-03-24 15:44:02 -0300

    Ajuste na lógica do teste

--- queries/macros/test_check_regularidade_temperatura.sql
+++ queries/macros/test_check_regularidade_temperatura.sql
@@ -94,13 +94,13 @@
                     {# Quando deveria `indicador_regularidade_ar_condicionado_viagem` ser TRUE, mas não é #}
                     (
                         (
-                            vt.ano_fabricacao <= 2019
-                            or vt.data >= date('{{ var("DATA_SUBSIDIO_V19_INICIO") }}')
+                            ano_fabricacao <= 2019
+                            or data >= date('{{ var("DATA_SUBSIDIO_V19_INICIO") }}')
                         )
-                        and not vt.indicador_temperatura_nula_viagem
+                        and not indicador_temperatura_nula_viagem
                         or (
-                            vt.data >= date('{{ var("DATA_SUBSIDIO_V22_INICIO") }}')
-                            and coalesce(vr.indicador_falha_recorrente, false)
+                            data >= date('{{ var("DATA_SUBSIDIO_V22_INICIO") }}')
+                            and coalesce(indicador_falha_recorrente, false)
                         )
                     )
                     and indicador_ar_condicionado
@@ -133,30 +133,31 @@
                     {# Quando `indicador_regularidade_ar_condicionado_viagem` deveria ser FALSE, mas não é #}
                     (
                         (
-                            vt.ano_fabricacao <= 2019
-                            or vt.data >= date('{{ var("DATA_SUBSIDIO_V19_INICIO") }}')
+                            ano_fabricacao <= 2019
+                            or data >= date('{{ var("DATA_SUBSIDIO_V19_INICIO") }}')
                         )
-                        and not vt.indicador_temperatura_nula_viagem
-                        or (
-                            vt.data >= date('{{ var("DATA_SUBSIDIO_V22_INICIO") }}')
-                            and coalesce(vr.indicador_falha_recorrente, false)
-                        )
-                    )
-                )
-                and (
-                    (
-                        data >= date("{{ var('DATA_SUBSIDIO_V20_INICIO') }}")
-                        and coalesce(indicador_falha_recorrente, false)
-                    )
-                    or indicador_temperatura_zero_viagem
-                    or (
-                        not indicador_temperatura_transmitida_viagem
                         and not indicador_temperatura_nula_viagem
+                        or (
+                            data >= date('{{ var("DATA_SUBSIDIO_V22_INICIO") }}')
+                            and coalesce(indicador_falha_recorrente, false)
+                        )
                     )
-                    or not indicador_temperatura_regular_viagem
-                )
-                and coalesce(indicador_regularidade_ar_condicionado_viagem, false)
 
+                    and (
+                        (
+                            data >= date("{{ var('DATA_SUBSIDIO_V20_INICIO') }}")
+                            and coalesce(indicador_falha_recorrente, false)
+                        )
+                        or indicador_temperatura_zero_viagem
+                        or (
+                            not indicador_temperatura_transmitida_viagem
+                            and not indicador_temperatura_nula_viagem
+                        )
+                        or not indicador_temperatura_regular_viagem
+                    )
+                    and coalesce(indicador_regularidade_ar_condicionado_viagem, false)
+
+                )
         ),
         teste_3_falha as (
             select
