8ef87d4a184d555fe319cc11fc9cd90a25f737d1
Janaína Duarte <89601202+Duartejanaina@users.noreply.github.com>
2026-03-24 14:13:28 -0300

    Apply suggestions from code review
    Co-authored-by: Guilherme Botelho <guilhermehcb@hotmail.com>

--- queries/models/subsidio/viagem_regularidade_temperatura.sql
+++ queries/models/subsidio/viagem_regularidade_temperatura.sql
@@ -12,13 +12,15 @@
 
 {% set condicao_veiculo %}
 (
-    (vt.ano_fabricacao <= 2019 
-     or vt.data >= date('{{ var("DATA_SUBSIDIO_V19_INICIO") }}')
+    (
+        vt.ano_fabricacao <= 2019
+        or vt.data >= date('{{ var("DATA_SUBSIDIO_V19_INICIO") }}')
     )
-    and
-    not vt.indicador_temperatura_nula_viagem
-    or (vt.data >= date('{{ var("DATA_SUBSIDIO_V22_INICIO") }}')
-    and coalesce(vr.indicador_falha_recorrente, false))
+    and not vt.indicador_temperatura_nula_viagem
+    or (
+        vt.data >= date('{{ var("DATA_SUBSIDIO_V22_INICIO") }}')
+        and coalesce(vr.indicador_falha_recorrente, false)
+    )
 )
 {% endset %}
 
