a9262b881263c49eaf16082d0df2a3e22cced456
Duartejanaina <janainacduarte@gmail.com>
2026-03-29 15:07:22 -0300

    reprocessamento dev

--- queries/models/monitoramento/aux_veiculo_dia_consolidada.sql
+++ queries/models/monitoramento/aux_veiculo_dia_consolidada.sql
@@ -5,7 +5,7 @@
 }}
 
 select data, id_veiculo, placa, null as ano_fabricacao, tecnologia, status, indicadores
-from {{ ref("sppo_veiculo_dia") }}
+from rj-smtr.veiculo.sppo_veiculo_dia
 where data < date("{{ var('DATA_SUBSIDIO_V15_INICIO') }}")
 
 union all
@@ -18,5 +18,5 @@ select
     tecnologia,
     if(tipo_veiculo like '%ROD%', "Não licenciado", status) as status,
     indicadores
-from {{ ref("veiculo_dia") }}
+from rj-smtr.monitoramento.veiculo_dia
 where modo is null or modo = 'ONIBUS'
