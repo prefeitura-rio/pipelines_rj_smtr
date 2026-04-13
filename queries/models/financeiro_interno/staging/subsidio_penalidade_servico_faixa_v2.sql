cb03a4b4cd7a29a89615eb3932a5b5d619832042
Duartejanaina <janainacduarte@gmail.com>
2026-04-13 13:37:29 -0300

    ALteração penalidade

--- queries/models/financeiro_interno/staging/subsidio_penalidade_servico_faixa_v2.sql
+++ queries/models/financeiro_interno/staging/subsidio_penalidade_servico_faixa_v2.sql
@@ -185,7 +185,6 @@ select
             )
             or (s.servico in ("133", "607", "711") and s.data = "2026-03-19")
             or (s.servico in ("133", "607") and s.data = "2026-03-20")
-            or (s)
         then 0  -- Processo SEI_000301.005390_2026_67
         else safe_cast(coalesce(pe.valor_penalidade, 0) as numeric)
     end as valor_penalidade,
